a311f07f124fed9a3ef9967c23d2f7387895ff52
enoch85 <enoch85@gmail.com>
2016-06-07 21:17:28 +0200

    add info about IPv6

--- static/nextcloud.sh
+++ static/nextcloud.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+WANIP4=$(dig +short myip.opendns.com @resolver1.opendns.com)
+WANIP6=$(curl -s https://6.ifcfg.me/)
 ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 clear
 figlet -f small Nextcloud
-echo "           https://nextcloud.com/"
+echo "           https://www.nextcloud.com"
 echo
 echo
 echo
-echo "WAN IP: $WANIP"
+echo "WAN IPv4: $WANIP4"
+echo "WAN IPv6: $WANIP6"
 echo "LAN IP: $ADDRESS"
 echo
 exit 0
