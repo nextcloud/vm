e7f08025bd852a479501ae296c45d4b0af6c2a22
enoch85 <enoch85@gmail.com>
2016-06-13 21:58:32 +0200

    add more php7.0 dependencies

--- testing/nextcloud_install_testing.sh
+++ testing/nextcloud_install_testing.sh
@@ -222,15 +222,21 @@ service apache2 restart
 # Install PHP 7.0
 apt-get update
 apt-get install -y \
-        php \
-	php-mcrypt \
-	php-pear \
-	php-ldap \
-        php-smbclient \
-	php-gd \
-	php-curl \
-	php-mb \
-	php-zip
+        libapache2-mod-php7.0 \
+	php7.0-common \
+	php7.0-mysql \
+	php7.0-intl \
+	php7.0-mcrypt \
+	php7.0-ldap \
+	php7.0-imap \
+	php7.0-cli \
+	php7.0-gd \
+	php7.0-pgsql \
+	php7.0-json \
+	php7.0-sqlite3 \
+	php7.0-curl \
+	php7.0-xml \
+	php7.0-zip
 
 # Install Unzip
 apt-get install unzip -y
