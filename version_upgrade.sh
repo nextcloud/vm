40132f0a794f8b36f8f664d51b4f791fbec18287
Ezra <ezraholm50@gmail.com>
2016-08-05 11:17:48 +0200

    Update version_upgrade.sh

--- version_upgrade.sh
+++ version_upgrade.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Tech and Me, ©2016 - www.techandme.se
+#
+# Check if root
+        if [ "$(whoami)" != "root" ]; then
+        echo
+        echo -e "\e[31mSorry, you are not root.\n\e[0mYou must type: \e[36msudo \e[0mbash $SCRIPTS/version_upgrade.sh"
+        echo
+        exit 1
+fi
+
+exit 0
