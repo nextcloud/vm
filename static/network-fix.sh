c8a4a42efeced59091772c0db5109a1a08002763
Ezra <ezraholm50@gmail.com>
2016-08-09 19:54:16 +0200

    Update network-fix.sh

--- static/network-fix.sh
+++ static/network-fix.sh
@@ -2,7 +2,7 @@
 IFACE=$(lshw -c network | grep "en" | awk '{print $3}')
 
 ifdown -a
-sed -i 's|eth0|$IFACE|' /etc/network/interfaces
+sed -i "s|eth0|$IFACE|" /etc/network/interfaces
 ifup -a
 
 exit 0
