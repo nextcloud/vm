b134ba953d8afb2da9f898e802b3f56e5f89f05c
Ezra <ezraholm50@gmail.com>
2016-08-09 10:52:52 +0200

    Update techandtool.sh

--- static/techandtool.sh
+++ static/techandtool.sh
@@ -798,7 +798,140 @@ EOF
 ##################### External USB 3.62
 
 do_external_usb() {
-	sleep 1
+	HDUUID=$(blkid -o value -s UUID /dev/sda2)
+	SPUUID=$(blkid -o value -s UUID /dev/sda1)
+	device='/dev/sda'
+	SDCARD='/dev/mmcblk0'
+	ROOT_PROFILE='/root/.profile'
+
+	whiptail --yesno "The SDCARD is used now, do you want to use an HD/SSD? (preferred, also better when powered externally)" 20 60 1
+	whiptail --yesno "Verify that you only have 1 externally powered USB Harddrive/SSD plugged in" 20 60 1
+
+        # Format and create partition
+
+# Disable swap if it is setup before
+if 		[ -f /swapfile ];
+	then
+      		swapoff -a
+      		rm /swapfile
+      		sed -i 's|/swapfile none swap defaults 0 0|#/swapfile none swap defaults 0 0|g' /etc/fstab
+      	else
+      		sleep 1
+fi
+
+# Step 1
+fdisk $device << EOF
+wipefs
+EOF
+
+fdisk $device << EOF
+o
+n
+p
+1
+
++4000M
+w
+EOF
+
+fdisk $device << EOF
+n
+p
+2
+
+
+w
+EOF
+sync
+partprobe
+
+# Swap
+mkswap -L PI_SWAP /dev/sda1 # format as swap
+swapon /dev/sda1 # announce to system
+echo "UUID=$SPUUID none swap sw 0 0" >> /etc/fstab
+sync
+partprobe
+
+# Set cmdline.txt
+mount /dev/mmcblk0p1 /mnt
+sed -i 's|root=/dev/mmcblk0p2|root=$HDUUID|g' /mnt/cmdline.txt
+umount /mnt
+
+# External HD
+sed -i 's|/dev/mmcblk0p2|UUID=$HDUUID|g' /etc/fstab # change ROOT device so the system will know which one to use as ROOT
+echo -ne '\n' | sudo mke2fs -t ext4 -b 4096 -L 'PI_ROOT' /dev/sda2 # make ext4 partition to hold ROOT
+mount /dev/sda2 /mnt
+
+    {
+    i=1
+    while read -r line; do
+        i=$(( $i + 1 ))
+        echo $i
+    done < <(rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /mnt)
+    } | whiptail --title "Progress" --gauge "Please wait while moving SD content to HD" 6 60 0
+
+umount /mnt
+
+# Remove SD card ROOT partition
+#fdisk /dev/mmcblk0 << EOF
+#d
+#2
+#w
+#EOF
+#sync
+#partprobe
+
+
+
+reboot
+
+else
+        # Want to do this later? 
+echo -e "\e[32m"
+echo    "+--------------------------------------------------------------------+"
+echo    "| Ok, you have chosen to not use an external HD/SSD                  |"
+echo    "| This is not only a performance limitation but it could             |"
+echo    "| Also mean that your Micro SDCard could die soon because            |"
+echo    "| Of the intense writing to the SDCard.                              |"
+echo    "|                                                                    |"
+echo    "| If you do want to use a Harddisk run:                              |"
+echo    "|                         sudo bash /var/scripts/external_usb.sh     |"
+echo    "+--------------------------------------------------------------------+"
+echo
+read -p "Press any key to continue..." -n1 -s
+echo -e "\e[0m"
+echo
+
+# Resize sd card
+fdisk $SDCARD << EOF
+d
+2
+w
+EOF
+sync
+
+fdisk $SDCARD << EOF
+n
+p
+2
+
+
+w
+EOF
+sync
+partprobe
+
+# Install swapfile of 2 GB
+if 		[ -f /swapfile ];
+	then
+      		sleep 1
+      	else
+      		fallocate -l 2048M /swapfile # create swapfile and set size
+      		chmod 600 /swapfile # give it the right permissions
+      		mkswap /swapfile # format as swap
+      		swapon /swapfile # announce to system
+      		echo "/swapfile none swap defaults 0 0" >> /etc/fstab # let the system know what file to use as swap after reboot
+fi
 }
 
 ##################### RPI-update 3.63
