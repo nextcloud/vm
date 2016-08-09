50137647cab12c43e018120aa329d622d79a6636
Ezra <ezraholm50@gmail.com>
2016-08-09 19:50:42 +0200

    Update network-fix.sh

--- static/network-fix.sh
+++ static/network-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-IFACE=$(lshw -c network | grep "logical name" | awk '{print $3}')
+IFACE=$(lshw -c network | grep "en" | awk '{print $3}')
 
 ifdown -a
 sed -i 's|eth0|$IFACE|' /etc/network/interfaces
