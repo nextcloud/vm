cb1092dc3eaf1ce3b7261e5311fffd41dd5550d7
Daniel Hansson <daniel@techandme.se>
2017-01-03 19:16:31 +0100

    Get cert for the office domain, not the main domian

--- static/collabora.sh
+++ static/collabora.sh
@@ -109,9 +109,9 @@ else
 
   # SSL configuration, you may want to take the easy route instead and use Lets Encrypt!
   SSLEngine on
-  SSLCertificateChainFile $CERTFILES/$CLEANDOMAIN/chain.pem
-  SSLCertificateFile $CERTFILES/$CLEANDOMAIN/cert.pem
-  SSLCertificateKeyFile $CERTFILES/$CLEANDOMAIN/privkey.pem
+  SSLCertificateChainFile $CERTFILES/$EDITORDOMAIN/chain.pem
+  SSLCertificateFile $CERTFILES/$EDITORDOMAIN/cert.pem
+  SSLCertificateKeyFile $CERTFILES/$EDITORDOMAIN/privkey.pem
   SSLProtocol             all -SSLv2 -SSLv3
   SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
   SSLHonorCipherOrder     on
@@ -171,7 +171,7 @@ sudo service apache2 stop
 cd /etc
 git clone https://github.com/certbot/certbot.git
 cd /etc/certbot
-./letsencrypt-auto certonly --agree-tos --standalone -d $CLEANDOMAIN
+./letsencrypt-auto certonly --agree-tos --standalone -d $EDITORDOMAIN
 # Check if $certfiles exists
 if [ -d "$HTTPS_CONF" ]
 then
