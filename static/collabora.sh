5b29384ad1e64b91b20c9c2fef1032b58b379dd5
Daniel Hansson <daniel@techandme.se>
2017-01-12 22:24:52 +0100

    [WIP] Improve the Collabora script (#79)
    * Get cert for the office domain, not the main domian
    
    * Update on CODE 2.0 (#87)
    
    Check this https://www.collaboraoffice.com/community-en/code-2-0-updates-2/
    Settings has changed.

--- static/collabora.sh
+++ static/collabora.sh
@@ -109,16 +109,16 @@ else
 
   # SSL configuration, you may want to take the easy route instead and use Lets Encrypt!
   SSLEngine on
-  SSLCertificateChainFile $CERTFILES/$CLEANDOMAIN/chain.pem
-  SSLCertificateFile $CERTFILES/$CLEANDOMAIN/cert.pem
-  SSLCertificateKeyFile $CERTFILES/$CLEANDOMAIN/privkey.pem
+  SSLCertificateChainFile $CERTFILES/$EDITORDOMAIN/chain.pem
+  SSLCertificateFile $CERTFILES/$EDITORDOMAIN/cert.pem
+  SSLCertificateKeyFile $CERTFILES/$EDITORDOMAIN/privkey.pem
   SSLProtocol             all -SSLv2 -SSLv3
   SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
   SSLHonorCipherOrder     on
 
   # Encoded slashes need to be allowed
-  AllowEncodedSlashes On
-
+  AllowEncodedSlashes NoDecode
+  
   # Container uses a unique non-signed certificate
   SSLProxyEngine On
   SSLProxyVerify None
@@ -138,7 +138,7 @@ else
   ProxyPassReverse    /hosting/discovery https://127.0.0.1:9980/hosting/discovery
 
   # Main websocket
-  ProxyPassMatch   "/lool/(.*)/ws$"      wss://127.0.0.1:9980/lool/$1/ws
+  ProxyPassMatch "/lool/(.*)/ws$" wss://127.0.0.1:9980/lool/$1/ws nocanon
   
   # Admin Console websocket
   ProxyPass   /lool/adminws wss://127.0.0.1:9980/lool/adminws
@@ -171,7 +171,7 @@ sudo service apache2 stop
 cd /etc
 git clone https://github.com/certbot/certbot.git
 cd /etc/certbot
-./letsencrypt-auto certonly --agree-tos --standalone -d $CLEANDOMAIN
+./letsencrypt-auto certonly --agree-tos --standalone -d $EDITORDOMAIN
 # Check if $certfiles exists
 if [ -d "$HTTPS_CONF" ]
 then
