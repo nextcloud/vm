da9f99b0f10b0c13430bdd53b69011029d107ef1
Daniel Hansson <daniel@techandme.se>
2017-02-04 16:20:11 +0100

    test cloud domain without slashes

--- static/collabora.sh
+++ static/collabora.sh
@@ -65,7 +65,7 @@ fi
 
 # Check if Nextcloud is installed
 echo "Checking if Nextcloud is installed..."
-curl -s https://$NCDOMAIN/status.php | grep -q 'installed":true'
+curl -s https://$(echo $NCDOMAIN | tr -d '\\')/status.php | grep -q 'installed":true'
 if [ $? -eq 0 ]
 then
     sleep 1
