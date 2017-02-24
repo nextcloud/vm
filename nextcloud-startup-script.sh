4118ecddadf277d2f042fc4d03c5e1a7a952947a
Daniel Hansson <daniel@techandme.se>
2017-02-24 02:18:27 +0100

    adding info

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -518,9 +518,12 @@ function ask_yes_or_no() {
 
 if [[ "no" == $(ask_yes_or_no "Do you want to change keyboard layout?") ]]
 then
+echo "Not chaning keyboard layout..."
 sleep 1
+clear
 else
 dpkg-reconfigure keyboard-configuration
+clear
 fi
 
 # Pretty URLs
