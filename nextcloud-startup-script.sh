6222f73ec0b86744ffc3d14f6fb8d4c2739d8612
Daniel Hansson <daniel@techandme.se>
2017-02-24 02:15:02 +0100

    make the function more failsafe

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -446,7 +446,7 @@ then
     echo -e "in your router later. It's included in this guide:"
     echo -e "https://www.techandme.se/open-port-80-443/ (step 1 - 5)"
     echo -e "\e[32m"
-    echo "We'll now setup the VM static IP"
+    read -p "Press any key to set static IP..." -n1 -s
     sleep 2
     echo -e "\e[0m"
     ifdown $IFACE
@@ -508,11 +508,20 @@ clear
 
 # Set keyboard layout
 echo "Current keyboard layout is $(localectl status | grep "Layout" | awk '{print $3}')"
-read -t 10 -r -p "$1 Do you want to change your keyboard layout? ([y]es or [N]o) (10s): "
-      case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-      y|yes) echo "Configuring keyboard layout" ;;
-      *)     echo "No changes to keyboard layout" ;;
-      esac
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+if [[ "no" == $(ask_yes_or_no "Do you want to change keyboard layout?") ]]
+then
+sleep 1
+else
+dpkg-reconfigure keyboard-configuration
+fi
 
 # Pretty URLs
 echo "Setting RewriteBase to "/" in config.php..."
