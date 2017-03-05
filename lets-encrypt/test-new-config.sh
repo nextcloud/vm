c2c18072b34d5a34425ff2f5ef57d9942c850d22
Daniel Hansson <daniel@techandme.se>
2017-03-06 00:40:07 +0100

    set domain name as hostname

--- lets-encrypt/test-new-config.sh
+++ lets-encrypt/test-new-config.sh
@@ -29,6 +29,14 @@ then
     read -p "Press any key to continue... " -n1 -s
     echo -e "\e[0m"
     crontab -u root -l | { cat; echo "@weekly $SCRIPTS/letsencryptrenew.sh"; } | crontab -u root -
+
+# Set hostname and ServerName
+FQDOMAIN=$(grep -r -m 1 ServerName /etc/apache2/sites-enabled/* | awk '{print $3; exit}')
+echo "Setting hostname to $FQDOMAIN..."
+sudo sh -c "echo 'ServerName $FQDOMAIN' >> /etc/apache2/apache2.conf"
+sudo hostnamectl set-hostname $FQDOMAIN
+service apache2 restart
+
 DATE='$(date +%Y-%m-%d_%H:%M)'
 IF='if [[ $? -eq 0 ]]'
 cat << CRONTAB > "$SCRIPTS/letsencryptrenew.sh"
@@ -70,29 +78,6 @@ else
     read -p "Press any key to continue... " -n1 -s
     echo -e "\e[0m"
     exit 1
-fi
-
-# Update Config
-if [ -f $SCRIPTS/update-config.php ]
-then
-    rm $SCRIPTS/update-config.php
-    wget -q $STATIC/update-config.php -P $SCRIPTS
-else
-    wget -q $STATIC/update-config.php -P $SCRIPTS
-fi
-
-# Sets trusted domain in config.php
-if [ -f $SCRIPTS/trusted.sh ]
-then
-    rm $SCRIPTS/trusted.sh
-    wget -q $STATIC/trusted.sh -P $SCRIPTS
-    bash $SCRIPTS/trusted.sh
-    rm $SCRIPTS/update-config.php
-else
-    wget -q $STATIC/trusted.sh -P $SCRIPTS
-    bash $SCRIPTS/trusted.sh
-    rm $SCRIPTS/trusted.sh
-    rm $SCRIPTS/update-config.php
 fi
 
 exit
