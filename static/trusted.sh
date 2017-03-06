f501dada159ef0c9e8f5612ee46205fe1970e7f3
Daniel Hansson <daniel@techandme.se>
2017-03-06 20:54:06 +0100

    change to fqdn for overwrite cli

--- static/trusted.sh
+++ static/trusted.sh
@@ -6,7 +6,7 @@ SCRIPTS=/var/scripts
 
 # Change config.php
 php $SCRIPTS/update-config.php $NCPATH/config/config.php 'trusted_domains[]' localhost ${ADDRESS[@]} $(hostname) $(hostname --fqdn) 2>&1 >/dev/null
-php $SCRIPTS/update-config.php $NCPATH/config/config.php overwrite.cli.url https://$ADDRESS/ 2>&1 >/dev/null
+php $SCRIPTS/update-config.php $NCPATH/config/config.php overwrite.cli.url https://$(hostname --fqdn)/ 2>&1 >/dev/null
 
 # Change .htaccess accordingly
 sed -i "s|RewriteBase /nextcloud|RewriteBase /|g" $NCPATH/.htaccess
