0a99082cd3fbf278c11d3f6aabc634a994155fc8
Daniel Hansson <daniel@techandme.se>
2017-03-09 16:48:31 +0100

    sudo is not needed

--- lets-encrypt/activate-ssl.sh
+++ lets-encrypt/activate-ssl.sh
@@ -209,7 +209,7 @@ then
     letsencrypt --version
 else
     echo "Installing letsencrypt..."
-    sudo add-apt-repository ppa:certbot/certbot -y
+    add-apt-repository ppa:certbot/certbot -y
     apt update -q2
     apt install letsencrypt -y -q
 fi
