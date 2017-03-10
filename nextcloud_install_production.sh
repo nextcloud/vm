53ff714c606e32a24d69d086bf610e7dd88e78ee
Daniel Hansson <daniel@techandme.se>
2017-03-11 00:18:13 +0100

    let the user choose mirror
    some mirrors are bad

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -208,7 +208,7 @@ else
   apt install python-pip -y
  pip install \
      --upgrade pip \
-     apt-select
+     apt-select -m up-to-date -t 5 -c
  apt-select
  sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup && \
  if [ -f sources.list ]
