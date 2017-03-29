2229248e1b5ea4ad94e338ed87e2c58347300f13
morph027 <morphsen@gmx.com>
2017-03-29 21:09:11 +0200

    shellcheck: static/trusted.sh

--- static/trusted.sh
+++ static/trusted.sh
@@ -5,8 +5,8 @@ ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 SCRIPTS=/var/scripts
 
 # Change config.php
-php $SCRIPTS/update-config.php $NCPATH/config/config.php 'trusted_domains[]' localhost ${ADDRESS[@]} $(hostname) $(hostname --fqdn) 2>&1 >/dev/null
-php $SCRIPTS/update-config.php $NCPATH/config/config.php overwrite.cli.url https://$(hostname --fqdn)/ 2>&1 >/dev/null
+php $SCRIPTS/update-config.php $NCPATH/config/config.php 'trusted_domains[]' localhost "${ADDRESS[@]}" "$(hostname)" "$(hostname --fqdn)" >/dev/null 2>&1
+php $SCRIPTS/update-config.php $NCPATH/config/config.php overwrite.cli.url https://"$(hostname --fqdn)"/ >/dev/null 2>&1
 
 # Change .htaccess accordingly
 sed -i "s|RewriteBase /nextcloud|RewriteBase /|g" $NCPATH/.htaccess
