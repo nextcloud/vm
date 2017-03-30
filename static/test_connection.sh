0475ac94b20dfbc91cb9edcb5272021e426cfe7d
Piet's Host <info@piets-host.de>
2017-03-30 21:39:05 +0200

    use printf instead of echo -e

--- static/test_connection.sh
+++ static/test_connection.sh
@@ -5,7 +5,7 @@ WGET="/usr/bin/wget"
 $WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
 if [ ! -s /tmp/google.idx ]
 then
-     echo -e "\e[31mNot Connected!\e[0m"
+     printf "\e[31mNot Connected!\e[0m\n"
 else
-    echo -e "Connected!"
+    printf "Connected!\n"
 fi
