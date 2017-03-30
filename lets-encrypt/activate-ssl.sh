068211c3ea8d365b6acb241b48ab57c523979120
morph027 <morphsen@gmx.com>
2017-03-30 16:17:48 +0200

    shellcheck: lets-encrypt/activate-ssl.sh

--- lets-encrypt/activate-ssl.sh
+++ lets-encrypt/activate-ssl.sh
@@ -9,10 +9,10 @@ certfiles=/etc/letsencrypt/live
 SCRIPTS=/var/scripts
 
 # Check if root
-if [[ $EUID -ne 0 ]]
+if [[ "$EUID" -ne 0 ]]
 then
     echo
-    echo -e "\e[31mSorry, you are not root.\n\e[0mYou need to type: \e[36msudo \e[0mbash $SCRIPTS/activate-ssl.sh"
+    printf "\e[31mSorry, you are not root.\n\e[0mYou need to type: \e[36msudo \e[0mbash %s/activate-ssl.sh" "$SCRIPTS"
     echo
     exit 1
 fi
@@ -32,10 +32,10 @@ cat << STARTMSG
 |       you run this script!                                    |
 |                                                               |
 |       You also have to open port 443 against this VMs         |
-|       IP address: $ADDRESS - do this in your router.      |
+|       IP address: "$ADDRESS" - do this in your router.      |
 |       Here is a guide: https://goo.gl/Uyuf65                  |
 |                                                               |
-|       This script is located in $SCRIPTS and you          |
+|       This script is located in "$SCRIPTS" and you          |
 |       can run this script after you got a domain.             |
 |                                                               |
 |       Please don't run this script if you don't have          |
@@ -46,57 +46,20 @@ cat << STARTMSG
 
 STARTMSG
 
-function ask_yes_or_no() {
+ask_yes_or_no() {
     read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+    case ${REPLY,,} in
     y|yes) echo "yes" ;;
     *)     echo "no" ;;
 esac
 }
+
 if [[ "no" == $(ask_yes_or_no "Are you sure you want to continue?") ]]
 then
     echo
     echo "OK, but if you want to run this script later, just type: sudo bash $SCRIPTS/activate-ssl.sh"
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
+    read -p $'\n\e\[32mPress any key to continue...\e[0m\n' -n1 -s
 exit
-fi
-
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "no" == $(ask_yes_or_no "Have you forwarded port 443 in your router?") ]]
-then
-    echo
-    echo "OK, but if you want to run this script later, just type: sudo bash $SCRIPTS/activate-ssl.sh"
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
-    exit
-fi
-
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "Do you have a domain that you will use?") ]]
-then
-    sleep 1
-else
-    echo
-    echo "OK, but if you want to run this script later, just type: sudo bash $SCRIPTS/activate-ssl.sh"
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
-    exit
 fi
 
 echo
@@ -110,13 +73,6 @@ ENTERDOMAIN
 echo
 read domain
 
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
 echo
 if [[ "no" == $(ask_yes_or_no "Is this correct? $domain") ]]
     then
@@ -140,35 +96,29 @@ fi
 # Check if 443 is open using nmap, if not notify the user
 echo "Running apt update..."
 apt update -q2
-if [ $(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed") -eq 1 ]
+if [ "$(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed")" == "1" ]
 then
-      echo "nmap is already installed..."
-      clear
+    echo "nmap is already installed..."
+    clear
 else
     apt install nmap -y
 fi
 
-if [ $(nmap -sS -p 443 "$WANIP4" -PN | grep -c "open") -eq 1 ]
+if [ "$(nmap -sS -p 443 "$WANIP4" -PN | grep -c "open")" == "1" ]
 then
-  echo -e "\e[32mPort 443 is open on $WANIP4!\e[0m"
-  apt remove --purge nmap -y
+    apt remove --purge nmap -y
 else
-  echo "Port 443 is not open on $WANIP4. We will do a second try on $domain instead."
-  echo -e "\e[32m"
-  read -p "Press any key to test $domain... " -n1 -s
-  echo -e "\e[0m"
-  if [[ $(nmap -sS -PN -p 443 $domain | grep -m 1 "open" | awk '{print $2}') = open ]]
-  then
-    echo -e "\e[32mPort 443 is open on $domain!\e[0m"
-    apt remove --purge nmap -y
-  else
-    echo "Port 443 is not open on $domain. Please follow this guide to open ports in your router: https://www.techandme.se/open-port-80-443/"
-    echo -e "\e[32m"
-    read -p "Press any key to exit... " -n1 -s
-    echo -e "\e[0m"
-    apt remove --purge nmap -y
-    exit 1
-  fi
+    echo "Port 443 is not open on $WANIP4. We will do a second try on $domain instead."
+    read -p "Press any key to test $domain... " -n1 -s
+    if [[ $(nmap -sS -PN -p 443 "$domain" | grep -m 1 "open" | awk '{print $2}') = open ]]
+    then
+        apt remove --purge nmap -y
+    else
+        echo "Port 443 is not open on $domain. Please follow this guide to open ports in your router: https://www.techandme.se/open-port-80-443/"
+        read -p "Press any key to exit... " -n1 -s
+        apt remove --purge nmap -y
+        exit 1
+    fi
 fi
 
 # Fetch latest version of test-new-config.sh
@@ -185,21 +135,19 @@ fi
 # Check if $domain exists and is reachable
 echo
 echo "Checking if $domain exists and is reachable..."
-if wget -q -T 10 -t 2 --spider $domain; then
-   sleep 1
-elif wget -q -T 10 -t 2 --spider --no-check-certificate https://$domain; then
-   sleep 1
-elif curl -s -k -m 10  $domain; then
-   sleep 1
-elif curl -s -k -m 10 https://$domain > /dev/null ; then
-   sleep 1
+if wget -q -T 10 -t 2 --spider "$domain"; then
+    sleep 1
+elif wget -q -T 10 -t 2 --spider --no-check-certificate "https://$domain"; then
+    sleep 1
+elif curl -s -k -m 10 "$domain"; then
+    sleep 1
+elif curl -s -k -m 10 "https://$domain" -o /dev/null ; then
+    sleep 1
 else
-   echo "Nope, it's not there. You have to create $domain and point"
-   echo "it to this server before you can run this script."
-   echo -e "\e[32m"
-   read -p "Press any key to continue... " -n1 -s
-   echo -e "\e[0m"
-   exit 1
+    echo "Nope, it's not there. You have to create $domain and point"
+    echo "it to this server before you can run this script."
+    read -p $'\n\e\[32mPress any key to continue...\e[0m\n' -n1 -s
+    exit 1
 fi
 
 # Install letsencrypt
@@ -216,21 +164,21 @@ else
 fi
 
 #Fix issue #28
-ssl_conf="/etc/apache2/sites-available/$domain.conf"
+ssl_conf="/etc/apache2/sites-available/"$domain.conf""
 
-# Check if $ssl_conf exists, and if, then delete
-if [ -f $ssl_conf ]
+# Check if "$ssl.conf" exists, and if, then delete
+if [ -f "$ssl_conf" ]
 then
-    rm $ssl_conf
+    rm "$ssl_conf"
 fi
 
 # Change ServerName in apache.conf
 sed -i "s|ServerName $(hostname -s)|ServerName $domain|g" /etc/apache2/apache2.conf
-sudo hostnamectl set-hostname $domain
+sudo hostnamectl set-hostname "$domain"
 service apache2 restart
 
 # Generate nextcloud_ssl_domain.conf
-if [ -f $ssl_conf ]
+if [ -f "$ssl_conf" ]
 then
     echo "Virtual Host exists"
 else
@@ -293,7 +241,7 @@ letsencrypt certonly \
 --rsa-key-size 4096 \
 --renew-by-default \
 --agree-tos \
--d $domain
+-d "$domain"
 
 # Activate Apache again (Disabled during standalone)
 service apache2 start
@@ -303,14 +251,12 @@ service apache2 reload
 if [ -d "$certfiles" ]
 then
     # Activate new config
-    bash $SCRIPTS/test-new-config.sh $domain.conf
+    bash $SCRIPTS/test-new-config.sh "$domain.conf"
     exit 0
 else
-    echo -e "\e[96m"
-    echo -e "It seems like no certs were generated, we do three more tries."
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
+    printf "\e[96m"
+    printf "It seems like no certs were generated, we do three more tries."
+    read -p $'\n\e\[32mPress any key to continue...\e[0m\n' -n1 -s
 fi
 ##### START SECOND TRY
 # Generate certs
@@ -318,40 +264,36 @@ letsencrypt \
 --rsa-key-size 4096 \
 --renew-by-default \
 --agree-tos \
--d $domain
+-d "$domain"
 # Check if $certfiles exists
 if [ -d "$certfiles" ]
 then
     # Activate new config
-    bash $SCRIPTS/test-new-config.sh $domain.conf
+    bash $SCRIPTS/test-new-config.sh "$domain.conf"
     exit 0
 else
-    echo -e "\e[96m"
-    echo -e "It seems like no certs were generated, we do two more tries."
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
+    printf "\e[96m"
+    printf "It seems like no certs were generated, we do two more tries."
+    read -p $'\n\e\[32mPress any key to continue...\e[0m\n' -n1 -s
 fi
 ##### START THIRD TRY
 letsencrypt certonly \
---webroot --w $NCPATH \
+--webroot --w "$NCPATH" \
 --rsa-key-size 4096 \
 --renew-by-default \
 --agree-tos \
--d $domain
+-d "$domain"
 
 # Check if $certfiles exists
 if [ -d "$certfiles" ]
 then
     # Activate new config
-    bash $SCRIPTS/test-new-config.sh $domain.conf
+    bash $SCRIPTS/test-new-config.sh "$domain.conf"
     exit 0
 else
-    echo -e "\e[96m"
-    echo -e "It seems like no certs were generated, we do one more try."
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
+    printf "\e[96m"
+    printf "It seems like no certs were generated, we do one more try."
+    read -p $'\n\e\[32mPress any key to continue...\e[0m\n' -n1 -s
 fi
 #### START FORTH TRY
 # Generate certs
@@ -360,18 +302,17 @@ letsencrypt \
 --rsa-key-size 4096 \
 --renew-by-default \
 --agree-tos \
--d $domain
+-d "$domain"
 
 # Check if $certfiles exists
 if [ -d "$certfiles" ]
 then
 # Activate new config
-    bash $SCRIPTS/test-new-config.sh $domain.conf
+    bash $SCRIPTS/test-new-config.sh "$domain.conf"
     exit 0
 else
-    echo -e "\e[96m"
-    echo -e "Sorry, last try failed as well. :/ "
-    echo -e "\e[0m"
+    printf "\e[96m"
+    printf "Sorry, last try failed as well. :/ "
     cat << ENDMSG
 +------------------------------------------------------------------------+
 | The script is located in $SCRIPTS/activate-ssl.sh                  |
@@ -387,16 +328,14 @@ else
 | The script will now do some cleanup and revert the settings.           |
 +------------------------------------------------------------------------+
 ENDMSG
-    echo -e "\e[32m"
     read -p "Press any key to revert settings and exit... " -n1 -s
-    echo -e "\e[0m"
 
-# Cleanup
-apt remove letsencrypt -y
-apt autoremove -y
-# Change ServerName in apache.conf and hostname
+    # Cleanup
+    apt remove letsencrypt -y
+    apt autoremove -y
+    # Change ServerName in apache.conf and hostname
     sed -i "s|ServerName $domain|ServerName $(hostname -s)|g" /etc/apache2/apache2.conf
-    sudo hostnamectl set-hostname $(hostname -s)
+    sudo hostnamectl set-hostname "$(hostname -s)"
     service apache2 restart
 fi
 clear
