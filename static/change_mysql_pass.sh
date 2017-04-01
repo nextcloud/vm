41fd4b860470f3330aa5c5893ee8470c6e1f7034
morph027 <morphsen@gmx.com>
2017-04-01 20:18:33 +0200

    shellcheck: static/change_mysql_pass.sh

--- static/change_mysql_pass.sh
+++ static/change_mysql_pass.sh
@@ -3,14 +3,14 @@
 # Tech and Me, ©2017 - www.techandme.se
 
 SHUF=$(shuf -i 17-20 -n 1)
-NEWMYSQLPASS=$(cat /dev/urandom | tr -dc "a-zA-Z0-9@#*=" | fold -w $SHUF | head -n 1)
+NEWMYSQLPASS=$(tr -dc "a-zA-Z0-9@#*=" < /dev/urandom | fold -w "$SHUF" | head -n 1)
 PW_FILE=/var/mysql_password.txt
 MYCNF=/root/.my.cnf
 OLDMYSQL=$(cat $PW_FILE)
 
 echo "Generating new MySQL root password..."
 # Change MySQL password
-mysqladmin -u root -p$OLDMYSQL password $NEWMYSQLPASS > /dev/null 2>&1
+mysqladmin -u root -p"$OLDMYSQL" password "$NEWMYSQLPASS" > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
     echo -e "\e[32mYour new MySQL root password is: $NEWMYSQLPASS\e[0m"
