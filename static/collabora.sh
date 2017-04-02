edad9a3abf2154cac89fe5a8ed67ce9ee2186d45
Daniel Hansson <daniel@techandme.se>
2017-04-02 18:19:12 +0200

    same thing for collabora

--- static/collabora.sh
+++ static/collabora.sh
@@ -21,6 +21,7 @@ HTTPS_CONF="/etc/apache2/sites-available/$SUBDOMAIN.conf"
 # Letsencrypt
 LETSENCRYPTPATH=/etc/letsencrypt
 CERTFILES=$LETSENCRYPTPATH/live
+DHPARAMS=""$CERTFILES"/"$NCDOMAIN"/dhparam.pem"
 # WANIP
 WANIP4=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # App
@@ -226,9 +227,11 @@ else
   SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
   SSLCertificateFile $CERTFILES/$SUBDOMAIN/cert.pem
   SSLCertificateKeyFile $CERTFILES/$SUBDOMAIN/privkey.pem
+  SSLOpenSSLConfCmd DHParameters $DHPARAMS
   SSLProtocol             all -SSLv2 -SSLv3
   SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
   SSLHonorCipherOrder     on
+  SSLCompression off
 
   # Encoded slashes need to be allowed
   AllowEncodedSlashes NoDecode
@@ -285,6 +288,8 @@ else
     add-apt-repository ppa:certbot/certbot -y
     apt update -q2
     apt install letsencrypt -y -q
+    apt update -q2
+    apt dist-upgrade -y
 fi
 
 # Stop Apache to aviod port conflicts
@@ -292,9 +297,13 @@ a2dissite 000-default.conf
 sudo service apache2 stop
 
 # Generate certs
-letsencrypt certonly --standalone --agree-tos --rsa-key-size 4096 -d "$SUBDOMAIN"
-if [[ "$?" == "0" ]]
+if letsencrypt certonly --standalone --agree-tos --rsa-key-size 4096 -d "$SUBDOMAIN"
 then
+    # Generate DHparams chifer
+    if [ ! -f $DHPARAMS ]
+    then
+        openssl dhparam -dsaparam -out $DHPARAMS 8192
+    fi
     printf "\e[96m\n"
     printf "Certs are generated!\n"
     printf "\e[0m\n"
