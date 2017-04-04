2e5852d86b44f0a4d25825058500f57af7b98b9d
Daniel Hansson <daniel@techandme.se>
2017-04-04 13:56:20 +0200

    Update setup_secure_permissions_nextcloud.sh

--- static/setup_secure_permissions_nextcloud.sh
+++ static/setup_secure_permissions_nextcloud.sh
@@ -3,38 +3,36 @@
 true
 # shellcheck source=lib.sh
 
-
 . <(curl -sL https://cdn.rawgit.com/morph027/vm/master/lib.sh)
-ncpath='/var/www/nextcloud'
+
 htuser='www-data'
 htgroup='www-data'
 rootuser='root'
-NCDATA='/var/ncdata'
 
 printf "Creating possible missing Directories\n"
-mkdir -p $ncpath/data
-mkdir -p $ncpath/updater
+mkdir -p $NCPATH/data
+mkdir -p $NCPATH/updater
 mkdir -p $NCDATA
 
 printf "chmod Files and Directories\n"
-find ${ncpath}/ -type f -print0 | xargs -0 chmod 0640
-find ${ncpath}/ -type d -print0 | xargs -0 chmod 0750
+find ${NCPATH}/ -type f -print0 | xargs -0 chmod 0640
+find ${NCPATH}/ -type d -print0 | xargs -0 chmod 0750
 
 printf "chown Directories\n"
-chown -R ${rootuser}:${htgroup} ${ncpath}/
-chown -R ${htuser}:${htgroup} ${ncpath}/apps/
-chown -R ${htuser}:${htgroup} ${ncpath}/config/
+chown -R ${rootuser}:${htgroup} ${NCPATH}/
+chown -R ${htuser}:${htgroup} ${NCPATH}/apps/
+chown -R ${htuser}:${htgroup} ${NCPATH}/config/
 chown -R ${htuser}:${htgroup} ${NCDATA}/
-chown -R ${htuser}:${htgroup} ${ncpath}/themes/
-chown -R ${htuser}:${htgroup} ${ncpath}/updater/
+chown -R ${htuser}:${htgroup} ${NCPATH}/themes/
+chown -R ${htuser}:${htgroup} ${NCPATH}/updater/
 
 chmod +x ${ncpath}/occ
 
 printf "chmod/chown .htaccess\n"
-if [ -f ${ncpath}/.htaccess ]
+if [ -f ${NCPATH}/.htaccess ]
 then
     chmod 0644 ${ncpath}/.htaccess
-    chown ${rootuser}:${htgroup} ${ncpath}/.htaccess
+    chown ${rootuser}:${htgroup} ${NCPATH}/.htaccess
 fi
 if [ -f ${NCDATA}/.htaccess ]
 then
