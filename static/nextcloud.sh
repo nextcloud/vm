5f53fc1824f3c6a74bd41f008040d8ac96ea40fb
Daniel Hansson <daniel@techandme.se>
2017-04-09 22:39:30 +0200

    change name of IP6 var

--- static/nextcloud.sh
+++ static/nextcloud.sh
@@ -5,8 +5,8 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-WANIPv6=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
-unset WANIPv6
+LOAD_IP6=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset LOAD_IP6
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
