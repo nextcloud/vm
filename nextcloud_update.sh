3aae3456c0dc9be1d2012d84e32a6cb15999c488
Daniel Hansson <daniel@techandme.se>
2017-05-05 18:38:02 +0200

    don't check certificate

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-NC_UPDATE=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+NC_UPDATE=1 . <(curl -sLk https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 unset NC_UPDATE
 
 # Tech and Me © - 2017, https://www.techandme.se/
