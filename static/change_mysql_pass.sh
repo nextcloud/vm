fc17fd6fb0afacfd078865123bbc11cd2160db2c
Daniel Hansson <daniel@techandme.se>
2017-05-17 18:02:35 +0200

    remove PW_FILE

--- static/change_mysql_pass.sh
+++ static/change_mysql_pass.sh
@@ -14,10 +14,9 @@ DEBUG=0
 debug_mode
 
 # Change MySQL Password
-if mysqladmin -u root -p"$OLDMYSQL" password "$NEWMYSQLPASS" > /dev/null 2>&1
+if mysqladmin -u root -p"$MYSQLMYCNFPASS" password "$NEWMYSQLPASS" > /dev/null 2>&1
 then
     echo -e "${Green}Your new MySQL root password is: $NEWMYSQLPASS${Color_Off}"
-    echo "$NEWMYSQLPASS" > $PW_FILE
     cat << LOGIN > "$MYCNF"
 [client]
 password='$NEWMYSQLPASS'
@@ -26,11 +25,6 @@ LOGIN
     exit 0
 else
     echo "Changing MySQL root password failed."
-    echo "Your old password is: $OLDMYSQL"
-    cat << LOGIN > "$MYCNF"
-[client]
-password='$OLDMYSQLPASS'
-LOGIN
-    chmod 0600 $MYCNF
+    echo "Your old password is: $MYSQLMYCNFPASS"
     exit 1
 fi
