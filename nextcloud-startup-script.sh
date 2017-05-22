a3ca0c4090840bf23a1c3c9bdbeb8618c8373d40
Daniel Hansson <daniel@techandme.se>
2017-05-22 23:00:40 +0200

    change MB

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -385,8 +385,8 @@ service apache2 reload
 VALUE="# php_value upload_max_filesize 513M"
 if ! grep -Fxq "$VALUE" $NCPATH/.htaccess
 then
-    sed -i 's/  php_value upload_max_filesize 511M/# php_value upload_max_filesize 511M/g' "$NCPATH"/.htaccess
-    sed -i 's/  php_value post_max_size 511M/# php_value post_max_size 511M/g' "$NCPATH"/.htaccess
+    sed -i 's/  php_value upload_max_filesize 513M/# php_value upload_max_filesize 511M/g' "$NCPATH"/.htaccess
+    sed -i 's/  php_value post_max_size 513M/# php_value post_max_size 511M/g' "$NCPATH"/.htaccess
     sed -i 's/  php_value memory_limit 512M/# php_value memory_limit 512M/g' "$NCPATH"/.htaccess
 fi
 
