0761c1f6b2398da039d5c53e6f19cb9495fd4153
Daniel Hansson <daniel@techandme.se>
2017-05-25 13:23:42 +0200

    Add Preview to lib

--- lib.sh
+++ lib.sh
@@ -91,6 +91,10 @@ SOLR_DSCONF=/opt/solr-$SOLR_VERSION/server/solr/configsets/data_driven_schema_co
 PASSVER_FILE=passman_$PASSVER.tar.gz
 PASSVER_REPO=https://releases.passman.cc
 SHA256=/tmp/sha256
+# Preview Generator
+[ ! -z "$PREVIEW_INSTALL" ] && PREVER=$(curl -s https://api.github.com/repos/nextcloud/calendar/releases/latest | grep "tag_name" | cut -d\" -f4 | sed -e "s|v||g")
+PREVER_FILE=previewgenerator.tar.gz
+PREVER_REPO=https://github.com/rullzer/previewgenerator/releases/download
 # Calendar
 [ ! -z "$CALENDAR_INSTALL" ] && CALVER=$(curl -s https://api.github.com/repos/nextcloud/calendar/releases/latest | grep "tag_name" | cut -d\" -f4 | sed -e "s|v||g")
 CALVER_FILE=calendar.tar.gz
