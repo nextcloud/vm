79b4f87522facabc9b2af19a0693d375dbd4860b
Daniel Hansson <daniel@techandme.se>
2017-05-30 13:58:54 +0200

    add subdomain settings again

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -19,6 +19,9 @@ then
     exit 1
 fi
 
+# Notification
+whiptail --msgbox "Please before you start, make sure that port 443 is directly forwarded to this machine!" "$WT_HEIGHT" "$WT_WIDTH"
+
 # Get the latest packages
 apt update -q4 & spinner_loading
 
@@ -34,10 +37,55 @@ then
     echo
     echo "If you use the Nextcloud VM you can use the Let's Encrypt script to get SSL and activate your Nextcloud domain."
     echo "When SSL is activated, run these commands from your terminal:"
-    echo "sudo wget $APP/onlyoffice.sh"
-    echo "sudo bash onlyoffice.sh"
+    echo "sudo wget $APP/collabora.sh"
+    echo "sudo bash collabora.sh"
     any_key "Press any key to continue... "
     exit 1
+fi
+
+# Check if $SUBDOMAIN exists and is reachable
+echo
+echo "Checking if $SUBDOMAIN exists and is reachable..."
+if wget -q -T 10 -t 2 --spider "$SUBDOMAIN"; then
+   sleep 0.1
+elif wget -q -T 10 -t 2 --spider --no-check-certificate "https://$SUBDOMAIN"; then
+   sleep 0.1
+elif curl -s -k -m 10 "$SUBDOMAIN"; then
+   sleep 0.1
+elif curl -s -k -m 10 "https://$SUBDOMAIN" -o /dev/null; then
+   sleep 0.1
+else
+   echo "Nope, it's not there. You have to create $SUBDOMAIN and point"
+   echo "it to this server before you can run this script."
+   any_key "Press any key to continue... "
+   exit 1
+fi
+
+# Check if 443 is open using nmap, if not notify the user
+apt update -q4 & spinner_loading
+if [ "$(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed")" == "1" ]
+then
+      echo "nmap is already installed..."
+else
+    apt install nmap -y
+fi
+if [ "$(nmap -sS -p 443 "$WANIP4" | grep -c "open")" == "1" ]
+then
+  printf "${Green}Port 443 is open on $WANIP4!${Color_Off}\n"
+  apt remove --purge nmap -y
+else
+  echo "Port 443 is not open on $WANIP4. We will do a second try on $SUBDOMAIN instead."
+  any_key "Press any key to test $SUBDOMAIN... "
+  if [[ "$(nmap -sS -PN -p 443 "$SUBDOMAIN" | grep -m 1 "open" | awk '{print $2}')" = "open" ]]
+  then
+      printf "${Green}Port 443 is open on $SUBDOMAIN!${Color_Off}\n"
+      apt remove --purge nmap -y
+  else
+      whiptail --msgbox "Port 443 is not open on $SUBDOMAIN. Please follow this guide to open ports in your router: https://www.techandme.se/open-port-80-443/" "$WT_HEIGHT" "$WT_WIDTH"
+      any_key "Press any key to exit... "
+      apt remove --purge nmap -y
+      exit 1
+  fi
 fi
 
 # Install Docker
@@ -98,7 +146,7 @@ fi
 
 # Install Onlyoffice docker
 docker pull onlyoffice/communityserver
-sudo docker run -i -t -d -p 80:80  -p 443:443 \
+sudo docker run -i -t -d -p 127.0.0.1:9980:9980 -e "domain=$SUBDOMAIN" --restart always \
     -v /app/onlyoffice/CommunityServer/data:/var/www/onlyoffice/Data  onlyoffice/communityserver
 
 # Activate SSL
@@ -113,7 +161,6 @@ cp onlyoffice.key /app/onlyoffice/CommunityServer/data/certs/
 cp onlyoffice.crt /app/onlyoffice/CommunityServer/data/certs/
 cp dhparam.pem /app/onlyoffice/CommunityServer/data/certs/
 chmod 400 /app/onlyoffice/CommunityServer/data/certs/onlyoffice.key
-
 
 # Install Apache2
 if [ "$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed")" == "1" ]
@@ -130,8 +177,119 @@ else
 fi
 
 # Enable Apache2 module's
+a2enmod proxy
+a2enmod proxy_wstunnel
+a2enmod proxy_http
 a2enmod ssl
 
+# Create Vhost for Collabora online in Apache2
+if [ ! -f "$HTTPS_CONF" ];
+then
+    cat << HTTPS_CREATE > "$HTTPS_CONF"
+<VirtualHost *:443>
+  ServerName $SUBDOMAIN:443
+  
+  <Directory /var/www>
+  Options -Indexes
+  </Directory>
+
+  # SSL configuration, you may want to take the easy route instead and use Lets Encrypt!
+  SSLEngine on
+  SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
+  SSLCertificateFile $CERTFILES/$SUBDOMAIN/cert.pem
+  SSLCertificateKeyFile $CERTFILES/$SUBDOMAIN/privkey.pem
+  SSLOpenSSLConfCmd DHParameters $DHPARAMS
+  SSLProtocol             all -SSLv2 -SSLv3
+  SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
+  SSLHonorCipherOrder     on
+  SSLCompression off
+
+  # Encoded slashes need to be allowed
+  AllowEncodedSlashes NoDecode
+
+  # Container uses a unique non-signed certificate
+  SSLProxyEngine On
+  SSLProxyVerify None
+  SSLProxyCheckPeerCN Off
+  SSLProxyCheckPeerName Off
+
+  # keep the host
+  ProxyPreserveHost On
+
+  # static html, js, images, etc. served from loolwsd
+  # loleaflet is the client part of LibreOffice Online
+  ProxyPass           /loleaflet https://127.0.0.1:9980/loleaflet retry=0
+  ProxyPassReverse    /loleaflet https://127.0.0.1:9980/loleaflet
+
+  # WOPI discovery URL
+  ProxyPass           /hosting/discovery https://127.0.0.1:9980/hosting/discovery retry=0
+  ProxyPassReverse    /hosting/discovery https://127.0.0.1:9980/hosting/discovery
+
+  # Main websocket
+  ProxyPassMatch "/lool/(.*)/ws$" wss://127.0.0.1:9980/lool/\$1/ws nocanon
+
+  # Admin Console websocket
+  ProxyPass   /lool/adminws wss://127.0.0.1:9980/lool/adminws
+
+  # Download as, Fullscreen presentation and Image upload operations
+  ProxyPass           /lool https://127.0.0.1:9980/lool
+  ProxyPassReverse    /lool https://127.0.0.1:9980/lool
+</VirtualHost>
+HTTPS_CREATE
+
+    if [ -f "$HTTPS_CONF" ];
+    then
+        echo "$HTTPS_CONF was successfully created"
+        sleep 1
+    else
+        echo "Unable to create vhost, exiting..."
+        echo "Please report this issue here $ISSUES"
+        exit 1
+    fi
+fi
+
+# Let's Encrypt
+letsencrypt --version 2> /dev/null
+LE_IS_AVAILABLE=$?
+if [ $LE_IS_AVAILABLE -eq 0 ]
+then
+    letsencrypt --version
+else
+    echo "Installing letsencrypt..."
+    add-apt-repository ppa:certbot/certbot -y
+    apt update -q4 & spinner_loading
+    apt install letsencrypt -y -q
+    apt update -q4 & spinner_loading
+    apt dist-upgrade -y
+fi
+
+# Stop Apache to aviod port conflicts
+a2dissite 000-default.conf
+sudo service apache2 stop
+
+# Generate certs
+if letsencrypt certonly --standalone --agree-tos --rsa-key-size 4096 -d "$SUBDOMAIN"
+then
+    # Generate DHparams chifer
+    if [ ! -f "$DHPARAMS" ]
+    then
+        openssl dhparam -dsaparam -out "$DHPARAMS" 8192
+    fi
+    printf "${ICyan}\n"
+    printf "Certs are generated!\n"
+    printf "${Color_Off}\n"
+    a2ensite "$SUBDOMAIN.conf"
+    service apache2 restart
+# Install Collabora App
+    check_command wget -q "$COLLVER_REPO/$COLLVER/$COLLVER_FILE" -P "$NCPATH/apps"
+    check_command tar -zxf "$NCPATH/apps/$COLLVER_FILE" -C "$NCPATH/apps"
+    cd "$NCPATH/apps" || exit 1
+    rm "$COLLVER_FILE"
+else
+    printf "${ICyan}\nIt seems like no certs were generated, please report this issue here: $ISSUES\n"
+    any_key "Press any key to continue... "
+    service apache2 restart
+fi
 # Enable Onlyoffice
 cd $NCPATH/apps
 git clone https://github.com/ONLYOFFICE/onlyoffice-owncloud.git onlyoffice
