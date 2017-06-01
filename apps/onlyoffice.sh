be16b87d7b9ac835c966290436f5206056b61956
Daniel Hansson <daniel@techandme.se>
2017-06-01 19:19:02 +0200

    minor changes

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -155,7 +155,7 @@ fi
 
 # Install Onlyoffice docker
 docker pull onlyoffice/documentserver
-docker run -i -t -d -p 127.0.0.3:9090:80 -p 127.0.0.3:9091:443 onlyoffice/documentserver
+docker run -i -t -d -p 127.0.0.3:9090:80 -p 127.0.0.3:9091:443 --restart always onlyoffice/documentserver
 
 # Install Apache2
 if [ "$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed")" == "1" ]
@@ -184,39 +184,40 @@ then
 <VirtualHost *:443>
      ServerName $SUBDOMAIN:443
 
-     SSLEngine on
-     ServerSignature On
-     SSLHonorCipherOrder on
+    SSLEngine on
+    ServerSignature On
+    SSLHonorCipherOrder on
 
-  SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
-  SSLCertificateFile $CERTFILES/$SUBDOMAIN/cert.pem
-  SSLCertificateKeyFile $CERTFILES/$SUBDOMAIN/privkey.pem
-  SSLOpenSSLConfCmd DHParameters $DHPARAMS
-  SSLProtocol             all -SSLv2 -SSLv3
-  SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
+    SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
+    SSLCertificateFile $CERTFILES/$SUBDOMAIN/cert.pem
+    SSLCertificateKeyFile $CERTFILES/$SUBDOMAIN/privkey.pem
+    SSLOpenSSLConfCmd DHParameters $DHPARAMS
+    
+    SSLProtocol             all -SSLv2 -SSLv3
+    SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
 
-     LogLevel warn
-     CustomLog ${APACHE_LOG_DIR}/access.log combined
-     ErrorLog ${APACHE_LOG_DIR}/error.log
+    LogLevel warn
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/error.log
 
-# Just in case - see below
-SSLProxyEngine On
-SSLProxyVerify None
-SSLProxyCheckPeerCN Off
-SSLProxyCheckPeerName Off
+    # Just in case - see below
+    SSLProxyEngine On
+    SSLProxyVerify None
+    SSLProxyCheckPeerCN Off
+    SSLProxyCheckPeerName Off
 
-# contra mixed content warnings
-RequestHeader set X-Forwarded-Proto "https"
+    # contra mixed content warnings
+    RequestHeader set X-Forwarded-Proto "https"
 
-# basic proxy settings
-ProxyRequests off
+    # basic proxy settings
+    ProxyRequests off
 
-        ProxyPass / http://127.0.0.3:9090/
-        ProxyPassMatch "/(.*)/websocket"  wss://127.0.0.3:9091/\$1/websocket
+    ProxyPass / http://127.0.0.3:9090/
+    ProxyPassMatch "/(.*)/websocket"  wss://127.0.0.3:9091/\$1/websocket
         
-        <Location />
-                ProxyPassReverse /
-        </Location>
+    <Location />
+        ProxyPassReverse /
+    </Location>
 </VirtualHost>
 HTTPS_CREATE
 
