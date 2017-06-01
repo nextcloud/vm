604e6fe618b7c4e757a885f71c00910e57adc1c3
Daniel Hansson <daniel@techandme.se>
2017-06-01 17:43:23 +0200

    add variables

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -2,7 +2,8 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+COLLABORA_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset COLLABORA_INSTALL
 
 # Tech and Me © - 2017, https://www.techandme.se/
 
