2ec22baed9224a47664ccbd7bf17c839e6c5ff2f
Daniel Hansson <daniel@techandme.se>
2017-06-02 17:58:04 +0200

    update docker before Nextcloud

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -44,6 +44,19 @@ then
     service apache2 restart
 fi
 
+# Update docker images
+if [ "$(docker image inspect onlyoffice/documentserver >/dev/null 2>&1 && echo yes || echo no)" == "yes" ]
+then
+    echo "Updating Docker container for OnlyOffice..."
+    echo "docker pull onlyoffice/documentserver"
+fi
+
+if [ "$(docker image inspect collabora/code >/dev/null 2>&1 && echo yes || echo no)" == "yes" ]
+then
+    echo "Updating Docker container for Collabora..."
+    echo "docker pull collabora/code"
+fi
+
 # Cleanup un-used packages
 apt autoremove -y
 apt autoclean
@@ -271,19 +284,6 @@ bash "$SECURE"
 
 # Repair
 sudo -u www-data php "$NCPATH"/occ maintenance:repair
-
-# Update docker images
-if [ "$(docker image inspect onlyoffice/documentserver >/dev/null 2>&1 && echo yes || echo no)" == "yes" ]
-then
-    echo "Updating Docker container for OnlyOffice..."
-    echo "docker pull onlyoffice/documentserver"
-fi
-
-if [ "$(docker image inspect collabora/code >/dev/null 2>&1 && echo yes || echo no)" == "yes" ]
-then
-    echo "Updating Docker container for Collabora..."
-    echo "docker pull collabora/code"
-fi
 
 CURRENTVERSION_after=$(sudo -u www-data php "$NCPATH"/occ status | grep "versionstring" | awk '{print $3}')
 if [[ "$NCVERSION" == "$CURRENTVERSION_after" ]]
