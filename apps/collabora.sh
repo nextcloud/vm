01825937a31888aaf29115aab4da6a74fb88f56b
Daniel Hansson <daniel@techandme.se>
2017-06-09 15:25:03 +0200

    add RAM check

--- apps/collabora.sh
+++ apps/collabora.sh
@@ -20,6 +20,18 @@ then
     exit 1
 fi
 
+# Test RAM size (2GB min)
+mem_available="$(awk '/MemTotal/{print $2}' /proc/meminfo)"
+if [ "${mem_available}" -lt 2000000 ]
+then
+    echo "Error: 2GB RAM required for Collabora!" >&2
+    echo "Current RAM is: ("$((mem_available/1024))" MiB)" >&2
+    sleep 3
+    exit 1
+else
+  echo "Memory: OK ("$((mem_available/1024))" MiB)"
+fi
+
 # Check if Onlyoffice is running
 if [ -d "$NCPATH"/apps/onlyoffice ]
 then
