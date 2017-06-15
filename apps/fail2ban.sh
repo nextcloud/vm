72632df75274851ea40c8350c662c1f7c134a58a
Daniel Hansson <daniel@techandme.se>
2017-06-16 00:49:24 +0200

    Create fail2ban.sh

---
+++ apps/fail2ban.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# Tech and Me © - 2017, https://www.techandme.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Check if root
+if ! is_root
+then
+    printf "\n${Red}Sorry, you are not root.\n${Color_Off}You must type: ${Cyan}sudo ${Color_Off}bash %s/phpmyadmin_install.sh\n" "$SCRIPTS"
+    sleep 3
+    exit 1
+
+### Local variables ###
+# location of Nextcloud logs
+NCLOG="/var/ncdata/nextcloud.log"     
+# time to ban an IP that exceeded attempts
+BANTIME_=600
+# cooldown time for incorrect passwords
+FINDTIME_=600                                    
+# bad attempts before banning an IP
+MAXRETRY_=4                                      
+DESCRIPTION="Brute force protection for SSH and Nextcloud"
+
+apt update -q4 & spinner_loading
+apt install fail2ban -y
+update-rc.d fail2ban disable
+
+if [ ! -f $NCLOG ]
+then
+    echo "$NCLOG not found"
+    exit 1
+else
+    touch $NCLOG
+    chown www-data:www-data $NCLOG
+fi
+
+# Set values in config.php
+sudo -u www-data php "$NCPATH/occ" config:system:set loglevel --value=2
+sudo -u www-data php "$NCPATH/occ" config:system:set log_type --value=file
+sudo -u www-data php "$NCPATH/occ" config:system:set logfile  --value="$NCLOG"
+sudo -u www-data php "$NCPATH/occ" config:system:set logtimezone  --value="$(cat /etc/timezone)"
+
+# Create nextcloud.conf file
+{
+[Definition]
+failregex = ^.*Login failed: '.*' \(Remote IP: '<HOST>'.*$
+ignoreregex =
+} > /etc/fail2ban/filter.d/nextcloud.conf
+
+# Create jail.local file
+{
+# The DEFAULT allows a global definition of the options. They can be overridden
+# in each jail afterwards.
+[DEFAULT]
+
+# "ignoreip" can be an IP address, a CIDR mask or a DNS host. Fail2ban will not
+# ban a host which matches an address in this list. Several addresses can be
+# defined using space separator.
+ignoreip = 127.0.0.1/8
+
+# "bantime" is the number of seconds that a host is banned.
+bantime  = $BANTIME_
+
+# A host is banned if it has generated "maxretry" during the last "findtime"
+# seconds.
+findtime = $FINDTIME_
+maxretry = $MAXRETRY_
+
+#
+# ACTIONS
+#
+banaction = iptables-multiport
+protocol = tcp
+chain = INPUT
+action_ = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action_mw = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action_mwl = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action = %(action_)s
+
+#
+# SSH
+#
+
+[ssh]
+
+enabled  = true
+port     = ssh
+filter   = sshd
+logpath  = /var/log/auth.log
+maxretry = "$MAXRETRY_"
+
+#
+# HTTP servers
+#
+
+[nextcloud]
+
+enabled  = true
+port     = http,https
+filter   = nextcloud
+logpath  = "$NCLOG"
+maxretry = "$MAXRETRY_"
+} > /etc/fail2ban/jail.local
+
+# Update settings
+check_command update-rc.d fail2ban defaults
+check_command update-rc.d fail2ban enable
+check_command service fail2ban restart
+
+
+
