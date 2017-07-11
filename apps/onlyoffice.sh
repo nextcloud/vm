33e5e3a03ce673b1acdcebd0e40a9f244d7e333e
Daniel Hansson <daniel@techandme.se>
2017-07-11 14:23:16 +0200

    install_if_not

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -183,19 +183,15 @@ fi
 docker pull onlyoffice/documentserver
 docker run -i -t -d -p 127.0.0.3:9090:80 -p 127.0.0.3:9091:443 --restart always onlyoffice/documentserver
 
+# Install apache2 
+install_if_not apache2
+install_if_not () {
 # Install Apache2
-if [ "$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed")" == "1" ]
+if [[ "$(is_this_installed apache2)" != "apache2 is installed, it must be a clean server." ]]
 then
-    sleep 0.1
-else
-    {
-    i=1
-    while read -r line; do
-        ((i++))
-        echo $i
-    done < <(apt install apache2 -y)
-    } | whiptail --title "Progress" --gauge "Please wait while installing Apache2" 6 60 0
+    apt install apache2 -y
 fi
+}
 
 # Enable Apache2 module's
 a2enmod proxy
