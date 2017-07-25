3d05a51abbf48ee27490a97d3e8f428bd8923313
Daniel Hansson <daniel@techandme.se>
2017-07-26 00:50:29 +0200

    move vars (#318)

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -8,9 +8,10 @@ sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/g
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-FIRST_IFACE=1 && CHECK_CURRENT_REPO=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+NCDB=1 && FIRST_IFACE=1 && CHECK_CURRENT_REPO=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 unset FIRST_IFACE
 unset CHECK_CURRENT_REPO
+unset NCDB
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
