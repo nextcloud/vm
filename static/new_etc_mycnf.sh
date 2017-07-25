2f00f45f26143a301a2c5ef30e85987923ba013a
Daniel Hansson <daniel@techandme.se>
2017-07-25 23:02:59 +0200

    Update new_etc_mycnf.sh

--- static/new_etc_mycnf.sh
+++ static/new_etc_mycnf.sh
@@ -1,1 +1,20 @@
-add stuff here
+#!/bin/bash
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+MYCNFPW=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset MYCNFPW
+
+/bin/cat <<WRITENEW >$ETCMYCNF
+text1
+text2
+text3
+text4
+WRITENEW
+
+# Restart MariaDB
+mysqladmin shutdown --force & spinner_loading
+wait
+check_command systemctl restart mariadb & spinner_loading
+
+exit
