8f2e3a71c84d1a3477a6fdec25e5030cc2f8c3f7
Daniel Hansson <daniel@techandme.se>
2017-07-26 03:10:09 +0200

    Update nextcloud_install_production.sh

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -263,13 +263,12 @@ sleep 3
 echo
 
 # Enable UTF8mb4 (4-byte support)
-databases=`mysql -u root -p$MARIADB_PASS -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
-databases=`mysql -u root -p$MARIADB_PASS -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=$(mysql -u root -p"$MARIADB_PASS" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 for db in $databases; do
     if [[ "$db" != "performance_schema" ]] && [[ "$db" != _* ]] && [[ "$db" != "information_schema" ]];
     then
         echo "Changing to UTF8mb4 on: $db"
-        check_command mysql -u root -p$MARIADB_PASS -e "ALTER DATABASE $db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+        check_command mysql -u root -p"$MARIADB_PASS" -e "ALTER DATABASE $db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
     fi
 done
 
