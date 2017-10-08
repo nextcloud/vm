09d041747b6d80534f27d099f0e0ab82b5280c38
Daniel Hansson <daniel@techandme.se>
2017-10-08 10:53:57 +0200

    move ncadmin check to adduser instead

--- static/adduser.sh
+++ static/adduser.sh
@@ -12,16 +12,20 @@ true
 DEBUG=0
 debug_mode
 
-if [[ "no" == $(ask_yes_or_no "Do you want to create a new user?") ]]
+if [[ $UNIXUSER != "ncadmin" ]]
 then
-    echo "Not adding another user..."
-    sleep 1
-else
-    read -r -p "Enter name of the new user: " NEWUSER
-    useradd -m "$NEWUSER" -G sudo
-    while true
-    do
-        sudo passwd "$NEWUSER" && break
-    done
-    sudo -u "$NEWUSER" sudo bash nextcloud_install_production.sh
+
+    if [[ "no" == $(ask_yes_or_no "Do you want to create a new user?") ]]
+    then
+        echo "Not adding another user..."
+        sleep 1
+    else
+        read -r -p "Enter name of the new user: " NEWUSER
+        useradd -m "$NEWUSER" -G sudo
+        while true
+        do
+            sudo passwd "$NEWUSER" && break
+        done
+        sudo -u "$NEWUSER" sudo bash nextcloud_install_production.sh
+    fi
 fi
