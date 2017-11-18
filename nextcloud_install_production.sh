cc0e48ae3741da37a713197cbbc598f233aeca53
Daniel Hansson <daniel@techandme.se>
2017-11-18 19:12:13 +0100

    add curl if not existing

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -5,6 +5,14 @@
 # Prefer IPv4
 sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/gai.conf
 
+# Install curl if not existing
+if [ "$(dpkg-query -W -f='${Status}' "curl" 2>/dev/null | grep -c "ok installed")" == "1" ]
+then
+    echo "curl OK"
+else
+    apt update && apt install curl -y
+fi
+
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
