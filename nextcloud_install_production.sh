1d75d98ee2ff37d1b756e3c38e5813d091c986a5
Daniel Hansson <daniel@techandme.se>
2017-11-19 12:25:57 +0100

    change to existing function instead

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -6,12 +6,7 @@
 sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/gai.conf
 
 # Install curl if not existing
-if [ "$(dpkg-query -W -f='${Status}' "curl" 2>/dev/null | grep -c "ok installed")" == "1" ]
-then
-    echo "curl OK"
-else
-    apt update && apt install curl -y
-fi
+install_if_not curl
 
 # shellcheck disable=2034,2059
 true
