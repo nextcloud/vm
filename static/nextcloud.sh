cc26da3007a08b22c6c69145e99ce3daf9780be0
Daniel Hansson <daniel@techandme.se>
2017-12-17 18:08:42 +0100

    Update nextcloud.sh

--- static/nextcloud.sh
+++ static/nextcloud.sh
@@ -2,23 +2,9 @@
 
 # Tech and Me © - 2017, https://www.techandme.se/
 
-<<<<<<< HEAD
-# shellcheck disable=2034,2059
-true
-# shellcheck source=lib.sh
-LOAD_IP6=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/postgresql/lib.sh)
-unset LOAD_IP6
-
-# Check for errors + debug code and abort if something isn't right
-# 1 = ON
-# 0 = OFF
-DEBUG=0
-debug_mode
-=======
 WANIP6=$(curl -s -k -m 7 https://6.ifcfg.me)
 WANIP4=$(curl -s -m 5 ipinfo.io/ip)
 ADDRESS=$(hostname -I | cut -d ' ' -f 1)
->>>>>>> bb612fe... more msg_box (#429)
 
 clear
 figlet -f small Nextcloud
