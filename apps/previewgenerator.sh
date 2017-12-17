cd9855c26f972edd1bea0f16c48f1ee1bb372a90
Daniel Hansson <daniel@techandme.se>
2017-12-17 16:58:07 +0100

    Update previewgenerator.sh

--- apps/previewgenerator.sh
+++ apps/previewgenerator.sh
@@ -5,8 +5,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-PREVIEW_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
-unset PREVIEW_INSTALL
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -14,21 +13,18 @@ unset PREVIEW_INSTALL
 DEBUG=0
 debug_mode
 
-# Download and install Preview Generator
-if [ ! -d "$NCPATH"/apps/previewgenerator ]
+# Download and install previewgenerator
+if [ ! -d "$NC_APPS_PATH/previewgenerator" ]
 then
-    echo "Installing Preview Generator..."
-    wget -q "$PREVER_REPO/v$PREVER/$PREVER_FILE" -P "$NCPATH/apps"
-    tar -zxf "$NCPATH/apps/$PREVER_FILE" -C "$NCPATH/apps"
-    cd "$NCPATH/apps"
-    rm "$PREVER_FILE"
+    echo "Installing previewgenerator..."
+    check_command sudo -u www-data php "$NCPATH"/occ app:install previewgenerator
 fi
 
-# Enable Preview Generator
-if [ -d "$NCPATH"/apps/previewgenerator ]
+# Enable previewgenerator
+if [ -d "$NC_APPS_PATH/previewgenerator" ]
 then
-    sudo -u www-data php "$NCPATH"/occ app:enable previewgenerator
-    chown -R www-data:www-data $NCPATH/apps
+    check_command sudo -u www-data php "$NCPATH"/occ app:enable previewgenerator
+    chown -R www-data:www-data "$NC_APPS_PATH"
     crontab -u www-data -l | { cat; echo "@daily php -f $NCPATH/occ preview:pre-generate >> /var/log/previewgenerator.log"; } | crontab -u www-data -
     sudo -u www-data php "$NCPATH"/occ preview:generate-all
     touch /var/log/previewgenerator.log
