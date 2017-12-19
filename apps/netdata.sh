f163457ff5ccc32b1f1dea67e2d6a8a740ab59d7
Daniel Hansson <daniel@techandme.se>
2017-12-19 22:22:56 +0100

    Update netdata.sh

--- apps/netdata.sh
+++ apps/netdata.sh
@@ -38,7 +38,7 @@ then
 msg_box "Netdata is now installed and can be accessed from these addresses:
 
 http(s)://$ADDRESS:19999
-http(s):/$(hostname):19999
+http(s)://$(hostname):19999
 
 You can find more configuraotion options in their wiki:
 https://github.com/firehol/netdata/wiki/Configuration"
