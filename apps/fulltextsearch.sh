e3f9f78904ad6a7c51dccaf2f6bd031600109798
Daniel Hansson <github@techandme.se>
2018-01-22 20:06:36 +0100

    WIP new script for Full Text Search
    - https://maximilian-boehm.com/en-gb/blog/create-a-java-keystore-jks-from-let-s-encrypt-certificates-1884000/ (enable SSL) & https://github.com/beshu-tech/readonlyrest-docs/blob/master/elasticsearch.md#encryption

---
+++ apps/fulltextsearch.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Tech and Me © - 2018, https://www.techandme.se/
+
+######################## Change to MASTER before merge ###########################
+############################# Developed for NC 13 ################################
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+FTS_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/nc13-appinstall/lib.sh)
+unset FTS_INSTALL
+
+FTS_VERSION=6.1.1
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+if ! is_root
+then
+    echo "Must be root to run script, in Ubuntu type: sudo -i"
+    exit 1
+fi
+
+# Make sure there is an Nextcloud installation
+if ! [ "$(sudo -u www-data php $NCPATH/occ -V)" ]
+then
+    echo "It seems there is no Nextcloud server installed, please check your installation."
+    exit 1
+fi
+
+# Check if it's a clean install
+if [ -d /usr/share/elasticsearch ]
+then
+    echo
+    echo "It seems like /usr/share/elasticsearch already exists. Have you already run this script?"
+    echo "If yes, revert all the settings and try again, it must be a clean install."
+    exit 1
+fi
+
+echo "Starting to setup Elastic Search & Full Text Search on Nextcloud..."
+
+# Installing requirements
+apt update -q4 & spinner_loading
+check_command apt install openjdk-9-jre -y
+check_command apt install apt-transport-https -y
+
+# Install Elastic
+check_command wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+check_command echo "deb https://artifacts.elastic.co/packages/$FTS_VERSION/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-$FTS_VERSION.list
+apt update -q4 & spinner_loading
+apt install elasticsearch -y
+check_command /etc/init.d/elasticsearch start
+check_command /etc/init.d/elasticsearch stop
+check_command /etc/init.d/elasticsearch restart
+if ! [ $(curl http://127.0.0.1:9200) ]
+then
+msg_box "Installation failed!
+
+Please report this to $ISSUES"
+    exit 1
+fi
+
+# Install ingest-attachment plugin
+if [ -d /usr/share/elasticsearch ]
+then
+    cd /usr/share/elasticsearch
+    check_command bin/elasticsearch-plugin install ingest-attachment
+fi
+
+# Install ReadOnlyREST
+bin/elasticsearch-plugin install file://"$GITHUB_REPO"/"$APPS"/fulltextsearch-files/readonlyrest-"$FTS_VERSION".zip
+# Check with SHA TODO
+
+# Create YML with password TODO
+
+# Add password and user values to FTS GUI TODO
+occ_command "config:app:set --value '1' fullnextsearch app_navigation"
+
+# Get Full Text Search app for nextcloud
+install_and_enable_app fulltextsearch
+chown -R www-data:www-data $NC_APPS_PATH
+check_command occ_command "fulltextsearch:index"
+
