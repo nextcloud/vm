2bfea4eddf25c31ec5e21c472d2dbdbebca62e6d
Daniel Hansson <github@techandme.se>
2018-03-01 23:08:12 +0100

    Update nextant.sh

--- apps/nextant.sh
+++ apps/nextant.sh
@@ -5,7 +5,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-NC_UPDATE=1 NEXTANT_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+NC_UPDATE=1 && NEXTANT_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 unset NEXTANT_INSTALL
 unset NC_UPDATE
 
