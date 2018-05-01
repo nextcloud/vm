c10d3c4552cfefcc5d41590c9626a8aed813af8f
Daniel Hansson <github@techandme.se>
2018-05-01 17:02:23 +0200

    Create format-sdb.sh

---
+++ static/format-sdb.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Tech and Me © - 2018, https://www.techandme.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+LABEL_=ncdata
+MOUNT_=/mnt/ncdata
+format() {
+# umount if mounted
+umount /mnt/* &> /dev/null
+
+# mkdir if not existing
+mkdir -p "$MOUNT_"
+
+# check still not mounted
+for dir in $( ls -d /mnt/* 2>/dev/null ); do
+mountpoint -q $dir && { msg_box "$dir is still mounted"; exit 1; }
+done
+
+# Get the name of the drive
+local NAME=( $( lsblk -l -n | grep -v mmcblk | grep sdb | awk '{ print $1 }' ) )
+[[ ${#NAME[@]} != 2 ]] && { echo "unexpected error"; exit 1; }
+
+
+if lsblk -l -n | grep -v mmcblk | grep sdb | awk '{ print $1 }' > /dev/null
+then
+msg_box "Formatting /dev/${NAME} when you hit OK.
+
+*** WARNING: ALL YOUR DATA WILL BE ERASED! ***"
+    check_command wipefs -a -f /dev/"$NAME"
+    check_command parted /dev/"$NAME" --script -- mklabel gpt
+    check_command parted /dev/"$NAME" --script -- mkpart primary 0% 100%
+    sleep 0.5
+    check_command mkfs.btrfs -q /dev/"${NAME}1" -f -L "$LABEL_"
+else
+msg_box "It seems like /dev/${NAME}1 does not exist.
+This script requires that you mount a second drive to hold the data.
+
+Please shutdown there server and mount a second drive.
+
+If you want help you can buy support in our shop:
+https://shop.techandme.se/index.php/product/premium-support-per-30-minutes/"
+exit 1
+fi
+}
+format
+
+# Remove old mount point in fstab if existing
+if cat /etc/fstab | grep ncdata
+then
+    sed -i 10q /etc/fstab > /dev/null
+fi
+
+# Mount it in fstab
+UUID=$(blkid /dev/sdb1 | awk '{ print $3 }')
+FSTAB="$UUID     $MOUNT_     btrfs   defaults 0       2"
+echo "# ncdata mount" >> /etc/fstab
+echo "$FSTAB" >> /etc/fstab
+check_command mount -a
+
+# Success!
+if cat /etc/fstab | grep "$UUID"
+then
+msg_box "$MOUNT_ mounted successfully in /etc/fstab with this command:
+$FSTAB
+
+The drive is formated as BTRFS and this is the device:
+$(btrfs fi show)"
+fi
