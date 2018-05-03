9b24901a1b0ca8148d5f7e9ee9e7166c3237d8e4
Daniel Hansson <github@techandme.se>
2018-05-03 23:08:26 +0200

    Update ip2.sh

--- static/ip2.sh
+++ static/ip2.sh
@@ -36,6 +36,7 @@ network:
          nameservers:
            addresses: [$DNS1,$DNS2] #name servers
 IPCONFIG
+    netplan apply
 else
 cat <<-IPCONFIGnonvmware > "$INTERFACES"
 network:
@@ -50,6 +51,7 @@ network:
          nameservers:
            addresses: [$DNS1,$DNS2] #name servers
 IPCONFIGnonvmware
+    netplan apply
 fi
 
 exit 0
