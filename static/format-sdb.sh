ea8cf3285565aaa0a291670d57f18667fd09502b
Lorenzo Faleschini <lorenzo.faleschini@nordestsystems.com>
2018-05-06 15:15:40 +0200

    changed ncdata volume to ZFS instead of BTRFS (#525)

--- static/format-sdb.sh
+++ static/format-sdb.sh
@@ -10,8 +10,12 @@ true
 # Check if root
 root_check
 
+# Check if ZFS utils are installed
+install_if_not zfsutils-linux 
+
 LABEL_=ncdata
 MOUNT_=/mnt/$LABEL_
+
 format() {
 # umount if mounted
 umount /mnt/* &> /dev/null
@@ -24,30 +28,37 @@ mkdir -p "$MOUNT_"
 isMounted() { findmnt -rno SOURCE,TARGET "$1" >/dev/null;} #path or device
 isDevMounted() { findmnt -rno SOURCE        "$1" >/dev/null;} #device only
 isPathMounted() { findmnt -rno        TARGET "$1" >/dev/null;} #path   only
+isDevPartOfZFS() { zpool status | grep "$1" >/dev/null;} #device memeber of a zpool
 
 if isPathMounted "/mnt/ncdata";      #Spaces in path names are ok.
 then
 msg_box "/mnt/ncdata is mounted and need to be unmounted before you can run this script."
-exit 1
+    exit 1
 fi
 
 if isDevMounted "/dev/sdb";
 then
 msg_box "/dev/sdb is mounted and need to be unmounted before you can run this script."
-exit 1
+    exit 1
 fi
 
 #Universal:
 if isMounted "/mnt/ncdata";
 then
 msg_box "/mnt/ncdata is mounted and need to be unmounted before you can run this script."
-exit 1
+    exit 1
 fi
 
 if isMounted "/dev/sdb1";
 then
 msg_box "/dev/sdb1 is mounted and need to be unmounted before you can run this script."
-exit 1
+    exit 1
+fi
+
+if isDevPartOfZFS "sdb";
+then
+msg_box "/dev/sdb is a member of a ZFS pool and needs to be removed from any zpool before you can run this script."
+    exit 1
 fi
 
 # Get the name of the drive
@@ -57,7 +68,7 @@ then
 msg_box "It seems like /dev/sdb does not exist.
 This script requires that you mount a second drive to hold the data.
 
-Please shutdown there server and mount a second drive.
+Please shutdown the server and mount a second drive, then start this script again.
 
 If you want help you can buy support in our shop:
 https://shop.techandme.se/index.php/product/premium-support-per-30-minutes/"
@@ -69,16 +80,28 @@ then
 msg_box "Formatting $SDB when you hit OK.
 
 *** WARNING: ALL YOUR DATA WILL BE ERASED! ***"
+    if zpool list | grep "$LABEL_" > /dev/null
+    then
+        check_command zpool destroy "$LABEL_"
+    fi
     check_command wipefs -a -f "$SDB"
-    check_command parted "$SDB" --script -- mklabel gpt
-    check_command parted "$SDB" --script -- mkpart primary 0% 100%
     sleep 0.5
-    check_command mkfs.btrfs -q "$SDB"1 -f -L "$LABEL_"
+    check_command zpool create -f -o ashift=12 "$LABEL_" "$SDB"
+    check_command zpool set failmode=continue "$LABEL_"
+    check_command zfs set mountpoint="$MOUNT_" "$LABEL_"
+    check_command zfs set compression=lz4 "$LABEL_"
+    check_command zfs set sync=standard "$LABEL_"
+    check_command zfs set xattr=sa "$LABEL_"
+    check_command zfs set primarycache=all "$LABEL_"
+    check_command zfs set atime=off "$LABEL_"
+    check_command zfs set recordsize=128k "$LABEL_"
+    check_command zfs set logbias=latency "$LABEL_"
+
 else
 msg_box "It seems like /dev/sdb does not exist.
 This script requires that you mount a second drive to hold the data.
 
-Please shutdown there server and mount a second drive.
+Please shutdown the server and mount a second drive, then start this script again.
 
 If you want help you can buy support in our shop:
 https://shop.techandme.se/index.php/product/premium-support-per-30-minutes/"
@@ -87,49 +110,17 @@ fi
 }
 format
 
-# Remove old mount point in fstab if existing
-if  grep "ncdata" /etc/fstab
-then
-    sed -i 10q /etc/fstab > /dev/null
-fi
-
-# Mount it in fstab
-UUID=$(blkid /dev/sdb1 | awk '{ print $3 }')
-FSTAB="$UUID     $MOUNT_     btrfs   defaults 0       2"
-echo "# ncdata mount" >> /etc/fstab
-echo "$FSTAB" >> /etc/fstab
-check_command mount -a
-
 # Success!
-if grep "$UUID" /etc/fstab
+if grep "$LABEL_" /etc/mtab
 then
-msg_box "$MOUNT_ mounted successfully in /etc/fstab with this command:
-$FSTAB
+msg_box "$MOUNT_ mounted successfully as a ZFS volume.
 
-The drive is formated as BTRFS and this is the device:
-$(btrfs filesystem usage $MOUNT_)"
-fi
+Automatic scrubbing is done montly via a cronjob that you can find here:
+/etc/cron.d/zfsutils-linux
 
-# BTRFS maintenance
-msg_box "This script will now download a set of scripts to maintain the BTRFS mount.
+CURRENT STATUS:
+$(zpool status $LABEL_)
 
-The scripts and instructions can be found here: https://github.com/kdave/btrfsmaintenance"
-
-if [ ! -f /etc/default/btrfsmaintenance ]
-then
-    cd /tmp || exit 1
-    wget -O btrfsmaintenance.zip https://github.com/kdave/btrfsmaintenance/archive/master.zip
-    install_if_not unzip
-    unzip -o /tmp/btrfsmaintenance.zip
-    cd /tmp/btrfsmaintenance-master
-    check_command bash dist-install.sh
-#    check_command sed -i 's|BTRFS_DEFRAG_PATHS=.*|BTRFS_DEFRAG_PATHS="/mnt/ncdata"|g' /etc/default/btrfsmaintenance
-    check_command sed -i 's|BTRFS_DEFRAG_PERIOD=.*|BTRFS_DEFRAG_PERIOD="monthly"|g' /etc/default/btrfsmaintenance
-    check_command sed -i 's|BTRFS_BALANCE_MOUNTPOINTS=.*|BTRFS_BALANCE_MOUNTPOINTS="/mnt/ncdata"|g' /etc/default/btrfsmaintenance
-    check_command sed -i 's|BTRFS_SCRUB_MOUNTPOINTS=.*|BTRFS_SCRUB_MOUNTPOINTS="/mnt/ncdata"|g' /etc/default/btrfsmaintenance
-    check_command sed -i 's|BTRFS_TRIM_MOUNTPOINTS=.*|BTRFS_TRIM_MOUNTPOINTS="auto"|g' /etc/default/btrfsmaintenance
-    check_command bash btrfsmaintenance-refresh-cron.sh
-else
-msg_box "It seems like /etc/default/btrfsmaintenance already exists. Have you already run this script?"
+$(zpool list)"
 fi
 
