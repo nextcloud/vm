3498e6f807c55649d58654b3c6a58920b850b811
Daniel Hansson <github@techandme.se>
2018-05-10 14:29:22 +0200

    change to netplan

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -404,7 +404,7 @@ fi
 # Extra configurations
 whiptail --title "Extra configurations" --checklist --separate-output "Choose what you want to configure\nSelect by pressing the spacebar" "$WT_HEIGHT" "$WT_WIDTH" 4 \
 "Security" "(Add extra security based on this http://goo.gl/gEJHi7)" OFF \
-"Static IP" "(Set static IP in Ubuntu with /etc/network/interfaces)" OFF 2>results
+"Static IP" "(Set static IP in Ubuntu with netplan.io)" OFF 2>results
 
 while read -r -u 9 choice
 do
