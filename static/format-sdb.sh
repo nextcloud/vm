ee7f1ae71b6138c0603e9013b5e7ec2e02ebda27
Karl <zalonic@outlook.com>
2018-06-08 18:26:33 +0100

    amended lib.sh
    amended lib.sh back to nextcloud/vm/master/lib.sh

--- static/format-sdb.sh
+++ static/format-sdb.sh
@@ -5,7 +5,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/zalonic/vm/master/lib.sh)
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
 # Check if root
 root_check
