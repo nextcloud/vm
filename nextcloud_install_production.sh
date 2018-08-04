3d1bc7f82660e718a6a71a1022d90455cbf15c69
Daniel Hansson <github@techandme.se>
2018-08-04 16:26:09 +0200

    Update nextcloud_install_production.sh

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -308,28 +308,28 @@ install_and_enable_app previewgenerator
 if [ -d "$NC_APPS_PATH/previewgenerator" ]
 then
     # Enable all previews
-    occ_command config:system:set enable_previews --value=true --type=boolean"
-    occ_command config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\PNG'"
-    occ_command config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\JPEG'"
-    occ_command config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\GIF'"
-    occ_command config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\BMP'"
-    occ_command config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\XBitmap'"
-    occ_command config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\MarkDown'"
-    occ_command config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\MP3'"
-    occ_command config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\TXT'"
-    occ_command config:system:set enabledPreviewProviders 8 --value='OC\\Preview\\Illustrator'"
-    occ_command config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\Movie'"
-    occ_command config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\MSOffice2003'"
-    occ_command config:system:set enabledPreviewProviders 11 --value='OC\\Preview\\MSOffice2007'"
-    occ_command config:system:set enabledPreviewProviders 12 --value='OC\\Preview\\MSOfficeDoc'"
-    occ_command config:system:set enabledPreviewProviders 13 --value='OC\\Preview\\OpenDocument'"
-    occ_command config:system:set enabledPreviewProviders 14 --value='OC\\Preview\\PDF'"
-    occ_command config:system:set enabledPreviewProviders 15 --value='OC\\Preview\\Photoshop'"
-    occ_command config:system:set enabledPreviewProviders 16 --value='OC\\Preview\\Postscript'"
-    occ_command config:system:set enabledPreviewProviders 17 --value='OC\\Preview\\StarOffice'"
-    occ_command config:system:set enabledPreviewProviders 18 --value='OC\\Preview\\SVG'"
-    occ_command config:system:set enabledPreviewProviders 19 --value='OC\\Preview\\TIFF'"
-    occ_command config:system:set enabledPreviewProviders 20 --value='OC\\Preview\\Font'"
+    occ_command config:system:set enable_previews --value=true --type=boolean
+    occ_command config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\PNG'
+    occ_command config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\JPEG'
+    occ_command config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\GIF'
+    occ_command config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\BMP'
+    occ_command config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\XBitmap'
+    occ_command config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\MarkDown'
+    occ_command config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\MP3'
+    occ_command config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\TXT'
+    occ_command config:system:set enabledPreviewProviders 8 --value='OC\\Preview\\Illustrator'
+    occ_command config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\Movie'
+    occ_command config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\MSOffice2003'
+    occ_command config:system:set enabledPreviewProviders 11 --value='OC\\Preview\\MSOffice2007'
+    occ_command config:system:set enabledPreviewProviders 12 --value='OC\\Preview\\MSOfficeDoc'
+    occ_command config:system:set enabledPreviewProviders 13 --value='OC\\Preview\\OpenDocument'
+    occ_command config:system:set enabledPreviewProviders 14 --value='OC\\Preview\\PDF'
+    occ_command config:system:set enabledPreviewProviders 15 --value='OC\\Preview\\Photoshop'
+    occ_command config:system:set enabledPreviewProviders 16 --value='OC\\Preview\\Postscript'
+    occ_command config:system:set enabledPreviewProviders 17 --value='OC\\Preview\\StarOffice'
+    occ_command config:system:set enabledPreviewProviders 18 --value='OC\\Preview\\SVG'
+    occ_command config:system:set enabledPreviewProviders 19 --value='OC\\Preview\\TIFF'
+    occ_command config:system:set enabledPreviewProviders 20 --value='OC\\Preview\\Font'
     
     # Add crotab
     crontab -u www-data -l | { cat; echo "@daily php -f $NCPATH/occ preview:pre-generate >> /var/log/previewgenerator.log"; } | crontab -u www-data -
