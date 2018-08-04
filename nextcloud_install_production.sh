6198e48b3b15cf52842d4ac412c87661ddd85bf4
Daniel Hansson <github@techandme.se>
2018-08-04 15:15:28 +0200

    some fixes

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -149,7 +149,7 @@ END
 service postgresql restart
 
 # Install Apache
-check_command apt install apache2 libapache2-mod-fcgid -y 
+check_command apt install apache2 -y 
 a2enmod rewrite \
         headers \
         env \
@@ -157,15 +157,12 @@ a2enmod rewrite \
         mime \
         ssl \
         setenvif
-        actions \
-        fastcgi \
-        alias \
         proxy_fcgi
-
+        
 # Install PHP 7.2
 apt update -q4 & spinner_loading
 check_command apt install -y \
-    php-fpm \
+    php7.2-fpm \
     php7.2-intl \
     php7.2-ldap \
     php7.2-imap \
@@ -176,10 +173,45 @@ check_command apt install -y \
     php7.2-zip \
     php7.2-mbstring \
     php7.2-soap \
+    php7.2-smbclient \
+    php7.2-imagick \
+    php7.2-json \
+    php7.2-gmp \
+    php7.2-bz2 \
     php-pear \
-    php-smbclient \
-    php-imagick \
     libmagickcore-6.q16-3-extra
+    
+# Set up a php-fpm pool with a unixsocket
+cat << POOL_CONF > "$PHP_POOL_DIR/nextcloud.conf"
+[NextCloud]
+user = www-data
+group = www-data
+listen = /run/php/php7.2-fpm.nextcloud.sock
+listen.owner = www-data
+listen.group = www-data
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 3
+pm.min_spare_servers = 2
+pm.max_spare_servers = 4
+pm.max_requests = 200
+env[HOSTNAME] = $(hotsname -f)
+env[PATH] = /usr/local/bin:/usr/bin:/bin
+env[TMP] = /tmp
+env[TMPDIR] = /tmp
+env[TEMP] = /tmp
+security.limit_extensions = .php
+php_admin_value [cgi.fix_pathinfo] = 1
+POOL_CONF
+
+# Disable the idling example pool.
+mv $PHP_POOL_DIR/www.conf $PHP_POOL_DIR/www.conf.backup
+    
+# Enable php-fpm
+a2enconf php7.2-fpm
+
+# Restart Webserver
+restart_webserver
     
 # Use TCP-socket (UNIX socket is faster, and TCP gives more overhead)
 # sed -i "s|listen = /run/php/php7.2-fpm.sock|listen = 127.0.0.1:9000|g" $PHP_WWW_CONF
@@ -272,10 +304,39 @@ install_and_enable_app previewgenerator
 # Run the first preview generation and add crontab
 if [ -d "$NC_APPS_PATH/previewgenerator" ]
 then
+    # Enable all previews
+    occ_command config:system:set enable_previews --value=true --type=boolean"
+    occ_command config:system:set enabledPreviewProviders 0 --value='OC\\Preview\\PNG'"
+    occ_command config:system:set enabledPreviewProviders 1 --value='OC\\Preview\\JPEG'"
+    occ_command config:system:set enabledPreviewProviders 2 --value='OC\\Preview\\GIF'"
+    occ_command config:system:set enabledPreviewProviders 3 --value='OC\\Preview\\BMP'"
+    occ_command config:system:set enabledPreviewProviders 4 --value='OC\\Preview\\XBitmap'"
+    occ_command config:system:set enabledPreviewProviders 5 --value='OC\\Preview\\MarkDown'"
+    occ_command config:system:set enabledPreviewProviders 6 --value='OC\\Preview\\MP3'"
+    occ_command config:system:set enabledPreviewProviders 7 --value='OC\\Preview\\TXT'"
+    occ_command config:system:set enabledPreviewProviders 8 --value='OC\\Preview\\Illustrator'"
+    occ_command config:system:set enabledPreviewProviders 9 --value='OC\\Preview\\Movie'"
+    occ_command config:system:set enabledPreviewProviders 10 --value='OC\\Preview\\MSOffice2003'"
+    occ_command config:system:set enabledPreviewProviders 11 --value='OC\\Preview\\MSOffice2007'"
+    occ_command config:system:set enabledPreviewProviders 12 --value='OC\\Preview\\MSOfficeDoc'"
+    occ_command config:system:set enabledPreviewProviders 13 --value='OC\\Preview\\OpenDocument'"
+    occ_command config:system:set enabledPreviewProviders 14 --value='OC\\Preview\\PDF'"
+    occ_command config:system:set enabledPreviewProviders 15 --value='OC\\Preview\\Photoshop'"
+    occ_command config:system:set enabledPreviewProviders 16 --value='OC\\Preview\\Postscript'"
+    occ_command config:system:set enabledPreviewProviders 17 --value='OC\\Preview\\StarOffice'"
+    occ_command config:system:set enabledPreviewProviders 18 --value='OC\\Preview\\SVG'"
+    occ_command config:system:set enabledPreviewProviders 19 --value='OC\\Preview\\TIFF'"
+    occ_command config:system:set enabledPreviewProviders 20 --value='OC\\Preview\\Font'"
+    
+    # Add crotab
     crontab -u www-data -l | { cat; echo "@daily php -f $NCPATH/occ preview:pre-generate >> /var/log/previewgenerator.log"; } | crontab -u www-data -
     occ_command preview:generate-all
     touch /var/log/previewgenerator.log
     chown www-data:www-data /var/log/previewgenerator.log
+    
+    # Install needed dependencies
+    install_if_not ffmpeg
+    install_if_not libreoffice
 fi
 
 # Install issuetemplate
@@ -303,12 +364,8 @@ then
 #    ServerAlias subdomain.example.com
 
 ### SETTINGS ###
-    <FilesMatch \.php$>
-        # 2.4.10+ can proxy to unix socket
-        SetHandler "proxy:unix:/var/run/php/php7.2-fpm.sock|fcgi://localhost/"
- 
-        # Else we can just use a tcp socket:
-        #SetHandler "proxy:fcgi://127.0.0.1:9000"
+    <FilesMatch \\.php$>
+        SetHandler "proxy:unix:/run/php/php7.2-fpm.nextcloud.sock|fcgi://localhost"
     </FilesMatch>
 
     DocumentRoot $NCPATH
@@ -352,12 +409,8 @@ then
 #    ServerAlias subdomain.example.com
 
 ### SETTINGS ###
-    <FilesMatch \.php$>
-        # 2.4.10+ can proxy to unix socket
-        SetHandler "proxy:unix:/var/run/php/php7.2-fpm.sock|fcgi://localhost/"
- 
-        # Else we can just use a tcp socket:
-        #SetHandler "proxy:fcgi://127.0.0.1:9000"
+    <FilesMatch \\.php$>
+        SetHandler "proxy:unix:/run/php/php7.2-fpm.nextcloud.sock|fcgi://localhost"
     </FilesMatch>
 
     DocumentRoot $NCPATH
@@ -421,7 +474,7 @@ HTTP2_ENABLE
 fi
 
 # Restart Apache2 to enable new config
-service apache2 restart
+restart_webserver
 
 whiptail --title "Which apps/programs do you want to install?" --checklist --separate-output "" 10 40 3 \
 "Calendar" "              " on \
