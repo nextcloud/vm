aba0bf68473af51137559935fe546fcb56feae36
Daniel Hansson <github@techandme.se>
2018-08-07 10:16:20 +0200

    tweaks (#604)

--- lib.sh
+++ lib.sh
@@ -206,6 +206,25 @@ do
 done
 }
 
+calculate_max_children() {
+# Calculate max_children depending on RAM
+# Tends to be between 30-50MB
+average_php_memory_requirement=50
+total_memory=$(awk '/MemTotal/ {printf "%d", $2/1024}' /proc/meminfo)
+export PHP_FPM_MAX_CHILDREN=$((total_memory/average_php_memory_requirement))
+
+if [ $PHP_FPM_MAX_CHILDREN -lt 1 ]
+then
+msg_box "It seems like php-fpm max_children are less than 1, which means that php-fpm won't function properly.
+Import this VM again and then raise the amount of RAM with at least 2 GB, and then run this script again.
+
+This script will now exit."
+exit 1
+else
+    echo "PHP-FPM max_children is $PHP_FPM_MAX_CHILDREN"
+fi
+}
+
 test_connection() {
 # Install dnsutils if not existing
 if [ "$(dpkg-query -W -f='${Status}' "dnsutils" 2>/dev/null | grep -c "ok installed")" == "1" ]
