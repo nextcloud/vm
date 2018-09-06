b575b94c1463d7a6ef554a9fe6699eec0984986b
Daniel Hansson <github@techandme.se>
2018-09-06 23:32:20 +0200

    fix https://github.com/nextcloud/vm/issues/654

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -286,6 +286,7 @@ fi
 # Pretty URLs
 echo "Setting RewriteBase to \"/\" in config.php..."
 chown -R www-data:www-data $NCPATH
+sed -i "s|'overwrite.cli.url'.*|'overwrite.cli.url' => 'https://localhost/',|g" $NCPATH/config/config.php
 occ_command config:system:set htaccess.RewriteBase --value="/"
 occ_command maintenance:update:htaccess
 bash $SECURE & spinner_loading
