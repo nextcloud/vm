10c5197a7094df44893e202e4e14006d8e5bce03
Daniel Hansson <github@techandme.se>
2018-09-09 17:24:59 +0200

    restart network

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -14,9 +14,10 @@ true
 is_process_running apt
 is_process_running dpkg
 
-# Change DNS
+# Change DNS system wide
 sed -i "s|#DNS=.*|DNS=9.9.9.9 2620:fe::fe|g" /etc/systemd/resolved.conf
 sed -i "s|#FallbackDNS=.*|FallbackDNS=149.112.112.112 2620:fe::9|g" /etc/systemd/resolved.conf
+check_command systemctl restart network-manager.service
 
 # Install curl if not existing
 if [ "$(dpkg-query -W -f='${Status}' "curl" 2>/dev/null | grep -c "ok installed")" == "1" ]
