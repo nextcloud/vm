989d53135dbb0fad7e321dbb63d556ce28a4a45f
Daniel Hansson <github@techandme.se>
2018-09-09 17:17:00 +0200

    add IPv6 to DNS

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -15,8 +15,8 @@ is_process_running apt
 is_process_running dpkg
 
 # Change DNS
-sed -i "s|#DNS=.*|DNS=9.9.9.9|g" /etc/systemd/resolved.conf
-sed -i "s|#FallbackDNS=.*|FallbackDNS=149.112.112.112|g" /etc/systemd/resolved.conf
+sed -i "s|#DNS=.*|DNS=9.9.9.9 2620:fe::fe|g" /etc/systemd/resolved.conf
+sed -i "s|#FallbackDNS=.*|FallbackDNS=149.112.112.112 2620:fe::9|g" /etc/systemd/resolved.conf
 
 # Install curl if not existing
 if [ "$(dpkg-query -W -f='${Status}' "curl" 2>/dev/null | grep -c "ok installed")" == "1" ]
