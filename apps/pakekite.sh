bd2b890bf7a8214e8b6521c07e4705b2fbee2a38
Daniel Hansson <github@hanssonit.se>
2018-10-22 20:32:55 +0200

    include PageKite as an alternative
    Fixes https://github.com/nextcloud/vm/issues/680

---
+++ apps/pakekite.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2018, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Check if root
+root_check
+
+# Get PageKite
+if ! install_if_not pagekite
+then
+    curl -s https://pagekite.net/pk/ | sudo bash
+fi
+
+# Do the signup
+# pagekite.py [--options] [service] kite-name [+flags]
+pagekite.py --signup
+
+# and so on and so on
