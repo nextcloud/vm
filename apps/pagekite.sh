b8c7666b32741976fe163d1cf31d4b004fab2a20
Daniel Hansson <github@hanssonit.se>
2018-10-23 14:38:48 +0200

    moe options

--- apps/pagekite.sh
+++ apps/pagekite.sh
@@ -2,6 +2,8 @@
 
 # T&M Hansson IT AB © - 2018, https://www.hanssonit.se/
 
+# http://manpages.ubuntu.com/manpages/xenial/man1/pagekite.1.html
+
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
@@ -25,13 +27,17 @@ install_if_not pagekite
 
 # Do the signup
 # pagekite.py [--options] [service] kite-name [+flags]
-pagekite --signup
-pagekite --daemonize
+pagekite --signup --defaults
+pagekite --daemonize --new --sslzlib
 
 # fetch the yourdomain.pagekite.me somehow to set trusted host (nextcloud config.php)
 
 # Create virtual hosts (redirect http --> https
 
 # make TLS- TLS all the way
+
+# Please use the pyOpenSSL wrappers instead.
+
+# Set / in htaccess to avoid /nextcloud/index.php
 
 # and so on and so on 
