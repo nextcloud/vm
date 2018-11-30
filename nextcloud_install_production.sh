5db1b705b14bbd9eda414eb9a028b309b9ebde0f
Georg Großmann <georg.grossmann@fau.de>
2018-11-30 20:10:48 +0100

    Split into second script.

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -104,7 +104,17 @@ install_if_not netplan.io
 install_if_not network-manager
 
 # Format /dev/sdb to host the ncdata
-run_static_script format-sdb
+
+# Let the user decide which disk to use
+msg_box "This VM is designed to run with two disks, one for OS and one for DATA.
+
+You will now get the option to decide which disk you want to use, or run the automatic script that will choose the available disk automatically."
+if [[ "yes" == $(ask_yes_or_no "Do you want to choose disk by yourself?") ]]
+then
+	run_static_script format-chosen
+else
+	run_static_script format-sdb
+fi
 
 # Change DNS system wide
 sed -i "s|#DNS=.*|DNS=9.9.9.9 2620:fe::fe|g" /etc/systemd/resolved.conf
