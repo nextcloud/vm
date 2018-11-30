0779a631c8d7d14219efcd617dafa6e43e45c161
ggeorgg <georg@grossmann-technologies.de>
2018-11-30 17:09:48 +0100

    Move blockfor user input
    Move block for user input to be executed after automatic device detection

--- static/format-sdb.sh
+++ static/format-sdb.sh
@@ -24,41 +24,6 @@ umount /mnt/* &> /dev/null
 # mkdir if not existing
 mkdir -p "$MOUNT_"
 
-# Let the user decide which disk to use
-msg_box "This VM is designed to run with two disks, one for OS and one for DATA.
-
-You will now get the option to decide which disk you want to use, or run the automatic script that will choose the available disk automatically."
-if [[ "yes" == $(ask_yes_or_no "Do you want to choose disk by yourself?") ]]
-then
-msg_box "You will now see a list with available devices. Choose the device where you want to put your nextcloud data. 
-Attention, the selected device will be formatted!"
-AvailableDEVICES=$(lsblk | grep "disk" | awk '{print $1}')
-
-# Save current IFS
-SAVEIFS=$IFS
-# Change IFS to new line. 
-IFS=$'\n'
-# Create Array
-AvailableDEVICES=($AvailableDEVICES)
-# Restore IFS
-IFS=$SAVEIFS
-
-# Ask for user input
-while 
-	lsblk
-	read -r -e -p "Enter the drive for the nextcloud data:" -i "$DEVTYPE" userinput
-	userinput=$(echo "$userinput" | awk '{print $1}')
-	for item in "${AvailableDEVICES[@]}"; do
-		[[ $userinput == "$item" ]]  && devtype_present=1 && DEVTYPE="$userinput"
-	done	
-
-	[[ -z ${devtype_present+x} ]]
-do
-	printf "${BRed}$DEVTYPE is not a valid disk. Please try again.${Color_Off}\n"
-    :
-done
-fi
-
 # Check what Hypervisor disks are available
 SYSVENDOR=$(cat /sys/devices/virtual/dmi/id/sys_vendor)
 if [ "$SYSVENDOR" == "VMware, Inc." ];
@@ -95,6 +60,41 @@ To be able to put the DATA on a second drive formatted as ZFS you need to add a 
 
 This script will now exit. Please mount a second disk and start over."
 exit 1
+fi
+
+# Let the user decide which disk to use
+msg_box "This VM is designed to run with two disks, one for OS and one for DATA.
+
+You will now get the option to decide which disk you want to use, or run the automatic script that will choose the available disk automatically."
+if [[ "yes" == $(ask_yes_or_no "Do you want to choose disk by yourself?") ]]
+then
+msg_box "You will now see a list with available devices. Choose the device where you want to put your nextcloud data. 
+Attention, the selected device will be formatted!"
+AvailableDEVICES=$(lsblk | grep "disk" | awk '{print $1}')
+
+# Save current IFS
+SAVEIFS=$IFS
+# Change IFS to new line. 
+IFS=$'\n'
+# Create Array
+AvailableDEVICES=($AvailableDEVICES)
+# Restore IFS
+IFS=$SAVEIFS
+
+# Ask for user input
+while 
+	lsblk
+	read -r -e -p "Enter the drive for the nextcloud data:" -i "$DEVTYPE" userinput
+	userinput=$(echo "$userinput" | awk '{print $1}')
+	for item in "${AvailableDEVICES[@]}"; do
+		[[ $userinput == "$item" ]]  && devtype_present=1 && DEVTYPE="$userinput"
+	done	
+
+	[[ -z ${devtype_present+x} ]]
+do
+	printf "${BRed}$DEVTYPE is not a valid disk. Please try again.${Color_Off}\n"
+    :
+done
 fi
 
 # Get the name of the drive
