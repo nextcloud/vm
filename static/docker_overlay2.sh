430eb3efb910fd6b8c475e64f9b820c90d1b4305
Daniel Hansson <github@hanssonit.se>
2018-12-02 17:44:12 +0100

    stop docker before export

--- static/docker_overlay2.sh
+++ static/docker_overlay2.sh
@@ -100,6 +100,7 @@ load_images() {
 }
 
 # Save all docker images in one file
+check_command service docker stop
 check_command docker images | sed '1d' | awk '{print $1 " " $2 " " $3}' > $DOCKERBACKUP/mydockersimages.list
 msg_box "The following images will be saved to $DOCKERBACKUP/images
 
