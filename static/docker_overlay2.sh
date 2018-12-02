9cc9ff9ac5a4d3f3c5cd44ea09def444ed3a1283
Daniel Hansson <github@hanssonit.se>
2018-12-02 17:18:21 +0100

    make it backwards compatible

--- static/docker_overlay2.sh
+++ static/docker_overlay2.sh
@@ -16,6 +16,11 @@ debug_mode
 # https://www.techandme.se/changes-to-docker-ce-in-the-nextcloud-vm/
 # Credits to: https://gist.github.com/hydra1983/22b2bed38b4f5f56caa87c830c96378d
 
+if [ ! -d $NCDATA ]
+then
+    DOCKERBACKUP=/var/DOCKERBACKUP
+fi
+
 readonly DB_FILE="$DOCKERBACKUP/images.db"
 readonly IMG_DIR="$DOCKERBACKUP/images"
 
