8a057557e048a13b7016e906a7626d058a63959e
Daniel Hansson <github@hanssonit.se>
2018-12-02 15:43:42 +0100

    add new save and load functions

--- lib.sh
+++ lib.sh
@@ -930,3 +930,88 @@ On_IBlue='\e[0;104m'    # Blue
 On_IPurple='\e[0;105m'  # Purple
 On_ICyan='\e[0;106m'    # Cyan
 On_IWhite='\e[0;107m'   # White
+
+
+############ TEMPORARY ############
+
+# Credits to: https://gist.github.com/hydra1983/22b2bed38b4f5f56caa87c830c96378d
+
+DOCKERBACKUP=$NCDATA/DOCKERBACKUP
+
+readonly DB_FILE="$DOCKERBACKUP/images.db"
+readonly IMG_DIR="$DOCKERBACKUP/images"
+
+save-images() {
+  echo "Create ${DB_FILE}"
+  echo "$(docker images|grep -v 'IMAGE ID'|awk '{printf("%s %s %s\n", $1, $2, $3)}'|column -t)" > "${DB_FILE}"
+  
+  echo "Read ${DB_FILE}"
+  local images
+  while read -r image; do
+     images+=("$image"); 
+  done <<< "$(cat ${DB_FILE})"
+
+  echo "Create ${IMG_DIR}"
+  if [[ ! -d "${IMG_DIR}" ]]; then
+    mkdir "${IMG_DIR}"
+  fi
+  
+  local name tag id
+  for image in "${images[@]}"; do
+    name=$(echo $image|awk '{print $1}')
+    tag=$(echo $image|awk '{print $2}')
+    id=$(echo $image|awk '{print $3}')
+        
+    if [[ "${id}" != "" ]]; then
+      local imgPath="${IMG_DIR}/${id}.dim"
+
+      if [[ ! -f "${imgPath}" ]] ; then
+        echo "[DEBUG] save ${id} ${name}:${tag} to ${imgPath}"
+        (time  docker save -o "${imgPath}" ${name}:${tag}) 2>&1 | grep real
+      else
+        echo "[DEBUG] ${id} ${name}:${tag} already saved"
+      fi
+    fi    
+  done
+}
+
+load-images() {
+  if [[ ! -f "${DB_FILE}" ]]; then
+    echo "No ${DB_FILE} to read"
+    exit 0
+  fi
+
+  if [[ ! -d "${IMG_DIR}" ]]; then
+    echo "No ${IMG_DIR} to load images"
+    exit 0
+  fi
+
+  echo "Read ${DB_FILE}"
+  local images
+  while read -r image; do
+     images+=("$image"); 
+  done <<< "$(cat ${DB_FILE})"
+
+  local name tag id
+  for image in "${images[@]}"; do
+    name=$(echo $image|awk '{print $1}')
+    tag=$(echo $image|awk '{print $2}')
+    id=$(echo $image|awk '{print $3}')
+        
+    if [[ "${id}" != "" ]]; then
+      local imgPath="${IMG_DIR}/${id}.dim"
+
+      if [[ "$(docker images|grep ${id} | grep ${name} | grep ${tag})" == "" ]]; then        
+        if [[ "$(docker images|grep ${id})" == "" ]]; then
+          echo "[DEBUG] load ${id} ${name}:${tag} from ${imgPath}"
+          docker load -i "${imgPath}"
+        else
+          echo "[DEBUG] tag ${id} as ${name}:${tag}"
+          docker tag ${id} ${name}:${tag}
+        fi
+      else
+        echo "[DEBUG] ${id} ${name}:${tag} already loaded"
+      fi
+    fi
+  done
+}
