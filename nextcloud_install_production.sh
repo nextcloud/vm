700045c722824a6f84942ce915697d7888278bff
Daniel Hansson <github@hanssonit.se>
2018-12-15 17:23:36 +0100

    do not install imagick due to security reasons
    Fixes https://github.com/nextcloud/vm/issues/743

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -201,12 +201,12 @@ check_command apt install -y \
     php7.2-mbstring \
     php7.2-soap \
     php7.2-smbclient \
-    php7.2-imagick \
     php7.2-json \
     php7.2-gmp \
     php7.2-bz2 \
     php-pear \
-    libmagickcore-6.q16-3-extra
+    # php7.2-imagick \
+    # libmagickcore-6.q16-3-extra
     
 # Enable php-fpm
 a2enconf php7.2-fpm
