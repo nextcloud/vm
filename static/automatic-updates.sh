3639888a79a1ef739e4089be54b635c63feaf479
Daniel Hansson <github@hanssonit.se>
2019-03-15 10:22:15 +0100

    Create automatic-updates.sh

---
+++ static/automatic-updates.sh
@@ -0,0 +1,19 @@
+	
+  
+  
+msg_box "This option will update your server every week on Sundays at 18:00 (6 PM). 
+The update will run the built in script 'update.sh' which will update both the server packages and Nextcloud itself.
+
+You can read more about it here: https://www.techandme.se/nextcloud-update-is-now-fully-automated/
+Please keep in mind that automatic updates might fail hence it's important to have a proper backup in place if you plan
+to run this option.
+
+In the next step you will be able to choose to proceed or exit."
+ask_yes_no "Do you want to continue?"
+
+
+crontab -u root -l | { cat; echo "0 18 * * SUN $SRIPTS/update.sh.sh"; } | crontab -u root -
+
+
+ask_yes_no "Do you want to reboot your server after every upgrade?
+# Add "reboot" to update.sh"
