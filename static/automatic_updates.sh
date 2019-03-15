53732cdfe2a3d018d7710148136bbaffc0dfea5f
Daniel Hansson <github@hanssonit.se>
2019-03-15 16:43:49 +0100

    travis

--- static/automatic_updates.sh
+++ static/automatic_updates.sh
@@ -19,20 +19,21 @@ debug_mode
 root_check
   
 msg_box "This option will update your server every week on Sundays at 18:00 (6 PM). 
-The update will run the built in script 'update.sh' which will update both the server packages and Nextcloud itself.
+The update will run the built in script '$SCRIPTS/update.sh' which will update both the server packages and Nextcloud itself.
 
 You can read more about it here: https://www.techandme.se/nextcloud-update-is-now-fully-automated/
 Please keep in mind that automatic updates might fail hence it's important to have a proper backup in place if you plan
 to run this option.
 
-You can disable the automatic updates by entering the crontab like this:
+You can disable the automatic updates by entering the crontab file like this:
 'sudo crontab -e -u root'
+Then just put a hash (#) in front of the row that you want to disable
 
 In the next step you will be able to choose to proceed or exit."
 
 if [[ "yes" == $(ask_yes_or_no "Do you want to enable automatic updates?") ]]
 then
-    crontab -u root -l | { cat; echo "0 18 * * SUN $SRIPTS/update.sh"; } | crontab -u root -
+    crontab -u root -l | { cat; echo "0 18 * * SUN $SCRIPTS/update.sh"; } | crontab -u root -
 fi
 
 if [[ "yes" == $(ask_yes_or_no "Do you want to reboot your server after every update at 22:00 (22 PM)") ]]
