2fdddb2ab85d8a38ec97e694198ad2b81444fcb9
Daniel Hansson <github@hanssonit.se>
2019-04-29 12:50:18 +0200

    set versions obligation history

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -327,6 +327,9 @@ occ_command config:system:set log_rotate_size --value="10485760"
 # Set trashbin retention obligation (save it in trahbin for 6 months or delete when space is needed)
 occ_command config:system:set trashbin_retention_obligation --value="auto, 180"
 
+# # Set versions retention obligation (save versions for 12 months or delete when space is needed)
+occ_command config:system:set versions_retention_obligation --value="auto, 365"
+
 # Change simple signup
 if grep -rq "free account" "$NCPATH"/core/templates/layout.public.php
 then
