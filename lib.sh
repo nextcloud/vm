e23e52b35ad99b2b915406abc404936b31b44a29
Daniel Hansson <github@hanssonit.se>
2019-05-09 20:05:13 +0200

    check if A record matches WANIP4

--- lib.sh
+++ lib.sh
@@ -214,18 +214,16 @@ domain_check_200() {
     print_text_in_color "$ICyan" "Checking DNS for ${1} with nslookup..."
     if nslookup "${1}" $DNS1 >/dev/null 2>&1
     then
-        print_text_in_color "$IGreen" "DNS seems correct!"
-        # site_200 "${1}" 
-	# Don't check if the site is reachable since it won'twork if you recently setup the DNS, nslookup is enough here.
-	# Also, it's possible to check with another dns-server like $DNS1 (which probaly would work), but curl isn't built with that in Ubuntu (https://c-ares.haxx.se/)
-    else
-        if ! nslookup "${1}" $DNS1
+        if [[ "$(dig +short $1 @resolver1.opendns.com)" = "$WANIP4" ]]
         then
-            print_text_in_color "$IRed" "DNS lookup failed. Please check your DNS settings! Maybe the domain isn't propagated yet?"
-            return 1
+            print_text_in_color "$IGreen" "DNS seems correct!"
         fi
+    else
+        print_text_in_color "$IRed" "DNS lookup failed. Please check your DNS settings! Maybe the domain isn't propagated$
+        return 1
     fi
 }
+
 
 # A function to fetch a file with curl to a directory
 # 1 = https://example.com
