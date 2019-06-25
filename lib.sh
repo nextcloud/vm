fde481a29c80d41c5b19fbd5779f769d54825d50
Daniel Hansson <github@hanssonit.se>
2019-06-25 19:50:37 +0200

    be more verbose

--- lib.sh
+++ lib.sh
@@ -723,6 +723,7 @@ download_verify_nextcloud_stable() {
 install_if_not gnupg
 rm -f "$HTML/$STABLEVERSION.tar.bz2"
 cd $HTML
+print_text_in_color "$Cyan" "Downloading Nextcloud $STABLEVERSION..."
 curl -fSLO --retry 3 "$NCREPO/$STABLEVERSION.tar.bz2"
 mkdir -p "$GPGDIR"
 curl_to_dir "$NCREPO" "$STABLEVERSION.tar.bz2.asc" "$GPGDIR"
