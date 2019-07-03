316693e12419d0f75ddba46933b0cc9dfc0a0e54
Daniel Hansson <github@hanssonit.se>
2019-07-03 21:29:20 +0200

    mockup fix for allowing to pass the DIG test

--- lib.sh
+++ lib.sh
@@ -231,12 +231,21 @@ domain_check_200() {
     if [[ "$(dig +short "${1}" @resolver1.opendns.com)" = "$WANIP4" ]]
     then
         print_text_in_color "$IGreen" "DNS seems correct when checking with dig!"
-    else
+    elif [[ "$(dig +short "${1}" @resolver1.opendns.com)" != "$WANIP4" ]]
+    then
         print_text_in_color "$IRed" "DNS lookup failed with dig. The external IP ($WANIP4) address of this server is not the same as the A-record."
         print_text_in_color "$IRed" "Please check your DNS settings! Maybe the domain isn't propagated?"
 	print_text_in_color "$ICyan" "Please check https://www.whatsmydns.net/#A/${1} if the IP seems correct."
-        return 1
-    fi
+	any_key "Please check information above and press any key to continue..."
+msg_box "As you noticed your external IP ($WANIP4) didn't match your DNS A-record. This can happen when using DDNS for example.
+
+If you feel brave, or are sure that everything is setup correctly, then you can choose to skip this test in the next step.
+
+You can always contact us for further support if you wish: https://shop.hanssonit.se/product/premium-support-per-30-minutes/"
+	if [[ "no" == $(ask_yes_or_no "Do you feel brave and want to continue?") ]]
+        then
+	    exit
+	fi
 }
 
 # A function to fetch a file with curl to a directory
