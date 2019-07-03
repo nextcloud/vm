76bdf80fcf44abbe449624ccc0c5bffeb5d72eef
Daniel Hansson <github@hanssonit.se>
2019-07-03 22:13:34 +0200

    change wording

--- lib.sh
+++ lib.sh
@@ -722,9 +722,10 @@ fi
 download_verify_nextcloud_stable() {
 while [ -z "$NCVERSION" ]
 do
-    print_text_in_color "$Cyan" "The variable 'NCVERSION' is empty, fetching it again..."
+    print_text_in_color "$ICyan" "Fetcing the latest Nextcloud version..."
     NCVERSION=$(curl -s -m 900 $NCREPO/ | sed --silent 's/.*href="nextcloud-\([^"]\+\).zip.asc".*/\1/p' | sort --version-sort | tail -1)
     STABLEVERSION="nextcloud-$NCVERSION"
+    print_text_in_color "$IGreen" "$NCVERSION"
 done
 install_if_not gnupg
 rm -f "$HTML/$STABLEVERSION.tar.bz2"
