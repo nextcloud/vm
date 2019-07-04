499e743eb525e437cee2c9ad721256729bc2b956
Daniel Hansson <github@hanssonit.se>
2019-07-04 10:13:49 +0200

    remove error handling since it's all done in the function (#864)

--- lets-encrypt/activate-ssl.sh
+++ lets-encrypt/activate-ssl.sh
@@ -82,14 +82,7 @@ done
 # Check if $domain exists and is reachable
 echo
 print_text_in_color "$ICyan" "Checking if $domain exists and is reachable..."
-if domain_check_200 "$domain"
-then
-    sleep 0.1
-else
-msg_box "Nope, it's not there. You have to create $domain and point
-it to this server before you can run this script."
-    exit 1
-fi
+domain_check_200 "$domain"
 
 # Check if port is open with NMAP
 sed -i "s|127.0.1.1.*|127.0.1.1       $domain nextcloud|g" /etc/hosts
