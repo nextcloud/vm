c2e8368b3b1edb75f0fcaaf95b511d33c6f60f9f
Daniel Hansson <github@hanssonit.se>
2019-07-17 11:10:33 +0200

    Create prune_zfs_snaphots.sh

---
+++ static/prune_zfs_snaphots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2019, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+if [ -d /dev/mapper/ ]
+then
+    if [ $(df -h /dev/mapper/*--vg-root | awk '{print $5}' | tail -1 | cut -d "%" -f1) -gt 90 ]
+    then
+        check_command curl_to_dir "https://raw.githubusercontent.com/bahamas10/zfs-prune-snapshots/master/" "zfs-prune-snapshots" "$SCRIPTS"
+        chmod +x "$SCRIPTS"/zfs-prune-snapshots
+
+        print_text_in_color "$ICyan" "Delete snapshots older than X weeks:"
+        read -r weeks
+        cd "$SCRIPTS"
+        ./zfs-prune-snapshots "$weeks"w ncdata
+    fi
+fi
