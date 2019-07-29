cf1c2abad8839024a5a379958694b7817301cdba
Daniel Hansson <github@hanssonit.se>
2019-07-29 13:55:20 +0200

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -483,7 +483,7 @@ fi
 
 restart_webserver() {
 check_command systemctl restart apache2
-if dpkg -l | grep "php$PHPVER-fpm" > /dev/null
+if is_this_installed "php$PHPVER-fpm"
 then
     check_command systemctl restart php"$PHPVER"-fpm.service
 fi
