62fbdd0888c8d8b4401cdfde81c4a6417bf74081
Daniel Hansson <github@hanssonit.se>
2019-10-20 18:35:04 +0200

    Remove ModSecurity
    It produces more issues than it's doing good. https://github.com/nextcloud/vm/issues/961

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -509,7 +509,6 @@ restart_webserver
 # Extra configurations
 whiptail --title "Extra configurations" --checklist --separate-output "Choose what you want to configure\nSelect by pressing the spacebar" "$WT_HEIGHT" "$WT_WIDTH" 4 \
 "Security" "(Add extra security based on this http://goo.gl/gEJHi7)" OFF \
-"ModSecurity" "(Add ModSecurity for Apache2)" OFF \
 "Static IP" "(Set static IP in Ubuntu with netplan.io)" OFF \
 "Automatic updates" "(Automatically update your server every week on Sundays)" OFF 2>results
 
@@ -520,12 +519,7 @@ do
             clear
             run_static_script security
         ;;
-
-        "ModSecurity")
-            clear
-            run_static_script modsecurity
-        ;;
-
+	
         "Static IP")
             clear
             run_static_script static_ip
