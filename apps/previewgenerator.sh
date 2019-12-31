c157c48830fd4584bf2eabae89b364ce1cc011d7
szaimen <42591237+szaimen@users.noreply.github.com>
2019-12-31 12:39:34 +0100

    hopefully fix travis

--- apps/previewgenerator.sh
+++ apps/previewgenerator.sh
@@ -211,8 +211,8 @@ then
     msg_box "In the last step you can define a nextcloud-user for which you want to run the preview-generation. "
     if [[ "yes" == $(ask_yes_or_no "So do you want to choose a nextcloud-user?") ]]
     then
-        nextcloud-user=$(whiptail --inputbox "Enter the nextcloud-user for which you want to run the preview-generation" 10 30 3>&1 1>&2 2>&3)
-        if [[ "yes" == $(ask_yes_or_no "Is this correct? $nextcloud-user") ]]
+        nextcloud_user=$(whiptail --inputbox "Enter the nextcloud-user for which you want to run the preview-generation" 10 30 3>&1 1>&2 2>&3)
+        if [[ "yes" == $(ask_yes_or_no "Is this correct? $nextcloud_user") ]]
         then
             break
         fi
@@ -221,7 +221,7 @@ then
     fi
     
     # Add crontab
-    crontab -u www-data -l | { cat; echo "0 4 * * * php -f $NCPATH/occ preview:pre-generate $nextcloud-user >> /var/log/previewgenerator.log"; } | crontab -u www-data -
+    crontab -u www-data -l | { cat; echo "0 4 * * * php -f $NCPATH/occ preview:pre-generate $nextcloud_user >> /var/log/previewgenerator.log"; } | crontab -u www-data -
     touch /var/log/previewgenerator.log
     chown www-data:www-data /var/log/previewgenerator.log
 
