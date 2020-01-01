1c5f6250337974698b3745178873e76e3697c550
Daniel Hansson <github@hanssonit.se>
2020-01-01 20:52:52 +0100

    travis 3

--- apps/previewgenerator.sh
+++ apps/previewgenerator.sh
@@ -254,7 +254,7 @@ else
     while true
     do
         PREVIEW_USER=$(whiptail --inputbox "Enter the Nextcloud user for which you want to run the preview-generation" "$WT_HEIGHT" "$WT_WIDTH" 3>&1 1>&2 2>&3)
-        if [ -z "$(occ_command user:list | grep $PREVIEW_USER | awk '{print $3}')" ]
+        if [ -z "$(occ_command user:list | grep "$PREVIEW_USER" | awk '{print $3}')" ]
         then
             msg_box "It seems like the user you entered ($PREVIEW_USER) doesn't exist, please try again."
         else
