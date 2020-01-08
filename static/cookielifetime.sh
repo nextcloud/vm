a04526039d4e0360a16b052cfd3547f0d4185667
Daniel Hansson <github@hanssonit.se>
2020-01-08 21:05:22 +0100

    change wording

--- static/cookielifetime.sh
+++ static/cookielifetime.sh
@@ -7,7 +7,7 @@ true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
-print_text_in_color "$ICyan" "Configuring automatic updates..."
+print_text_in_color "$ICyan" "Configuring Cookie Lifetime timeout..."
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
