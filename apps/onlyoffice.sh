2fcfaa9805b780ac59bcd5e17aeb8cad91a85d56
Daniel Hansson <github@hanssonit.se>
2020-01-08 13:28:13 +0100

    final touches

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -51,14 +51,15 @@ fi
 # Check if apache2 evasive-mod is enabled and disable it because of compatibility issues
 if [ "$(apache2ctl -M | grep evasive)" != "" ]
 then
-    msg_box "We noticed that 'mod_evasive' is installed which is the DDOS protection for webservices. It has comptibility issues with OnlyOffice and will get disabled if you continue."
+    msg_box "We noticed that 'mod_evasive' is installed which is the DDOS protection for webservices. It has comptibility issues with OnlyOffice and you can now choose to disable it."
     if [[ "no" == $(ask_yes_or_no "Do you want to disable DDOS protection?")  ]]
     then
-        sleep 0.1
+        print_text_in_color "$Cyan" "Keeping mod_evasive active."
     else
         a2dismod evasive
         a2dismod mod-evasive
         apt purge libapache2-mod-evasive -y
+	systemctl restart apache2
     fi
 fi
 
