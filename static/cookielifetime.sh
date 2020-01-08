30f2ef18df1ae82fcf57a1f404f76eaaf5628784
Daniel Hansson <github@hanssonit.se>
2020-01-08 13:44:27 +0100

    change wording

--- static/cookielifetime.sh
+++ static/cookielifetime.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# T&M Hansson IT AB © - 2019, https://www.hanssonit.se/
+
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+print_text_in_color "$ICyan" "Configuring automatic updates..."
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -14,7 +18,7 @@ debug_mode
 # Check if root
 root_check
 
-COOKIE_LIFETIME=$(whiptail --radiolist  "Configure after what time (in seconds) after every Login every Nextcloud user gets logged out in the Browser\nSelect one with the [ARROW] Keys and the [SPACE] key and confirm by pressing [ENTER]" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+COOKIE_LIFETIME=$(whiptail --radiolist  "Configure the logout time (in seconds) which will forcefully logout the Nextcloud user from the web browser when the timeout is reached.\nSelect one with the [ARROW] keys and select with the [SPACE] key. Confirm by pressing [ENTER]" "$WT_HEIGHT" "$WT_WIDTH" 4 \
 "1800s" "half an hour" ON \
 "7200s" "two hours" OFF \
 "43200s" "half a day" OFF \
@@ -45,14 +49,14 @@ elif [ "$COOKIE_LIFETIME" == "Custom" ]
 then 
     while true
     do
-        COOKIE_LIFETIME=$(whiptail --inputbox "Configure after what time (in seconds) after every Login every Nextcloud user gets logged out in the Browser\nPlease enter the Cookie Lifetime in seconds, so e.g. 1800 for half an hour or 3600 for an hour\nIt is not recommended to set it to less than half an hour!" "$WT_HEIGHT" "$WT_WIDTH" 1800 3>&1 1>&2 2>&3)
+        COOKIE_LIFETIME=$(whiptail --inputbox "Configure the logout time (in seconds) which will forcefully logout the Nextcloud user from the web browser when the timeout is reached.\nPlease enter the Cookie Lifetime in seconds, so e.g. 1800 for half an hour or 3600 for an hour\nIt is not recommended to set it to less than half an hour!" "$WT_HEIGHT" "$WT_WIDTH" 1800 3>&1 1>&2 2>&3)
         COOKIE_LIFETIME=${COOKIE_LIFETIME//[!0-9]/}
         if [ "$COOKIE_LIFETIME" -lt "1800" ]
         then
-            msg_box "It seems like you have chosen a value below half an hour, which is not recommended. So please try again."
+            msg_box "It seems like you have chosen a value below half an hour, which is not recommended.Please try again."
         elif [[ "no" == $(ask_yes_or_no "Is this correct? $COOKIE_LIFETIME seconds")  ]]
         then
-            msg_box "It seems like you weren't satisfied with your setting of ($COOKIE_LIFETIME) seconds. So please try again."
+            msg_box "It seems like you weren't satisfied with your setting of ($COOKIE_LIFETIME) seconds. Please try again."
         else
             occ_command config:system:set remember_login_cookie_lifetime --value="$COOKIE_LIFETIME"
             break
