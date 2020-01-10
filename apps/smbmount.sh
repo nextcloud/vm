080df6df9cc60a97e2b0b039f30a31e75def9d13
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-10 22:32:31 +0100

    check if fstab entrys exist for deletion

--- apps/smbmount.sh
+++ apps/smbmount.sh
@@ -173,6 +173,10 @@ then
     run_app_script smbmount
 elif [ "$SMB_MOUNT" == "delete SMB-Mounts" ]
 then
+    if [ "$(grep /mnt/smbshares /etc/fstab)" == "" ]
+    then
+        msg_box "You haven't created any SMB-Mount. So nothing to delete."
+        run_app_script smbmount
     args=(whiptail --title "delete SMB-Mounts" --checklist --separate-output "This option let you delete SMB-Shares to disconnect and remove network-shares from the host-computer or other machines in the local network.\nChoose what you want to do.\n\nSelect or unselect by pressing the spacebar" "$WT_HEIGHT" "$WT_WIDTH" 4)
     if [ "$(grep /mnt/smbshares/1 /etc/fstab)" != "" ]
     then
