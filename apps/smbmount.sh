d46e131c0e3c915c848469cff352bbd93240c129
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-11 15:33:43 +0100

    put delete shares choice into loop

--- apps/smbmount.sh
+++ apps/smbmount.sh
@@ -186,18 +186,15 @@ then
         run_app_script smbmount
     fi
     args=(whiptail --title "delete SMB-Mounts" --checklist --separate-output "This option let you delete SMB-Shares to disconnect and remove network-shares from the host-computer or other machines in the local network.\nChoose what you want to do.\nSelect or unselect by pressing the spacebar" "$WT_HEIGHT" "$WT_WIDTH" 4)
-    if [ "$(grep /mnt/smbshares/1 /etc/fstab)" != "" ]
-    then
-        args+=("/mnt/smbshares/1" "$(grep /mnt/smbshares/1 /etc/fstab | awk '{print $1}')" OFF)
-    fi
-    if [ "$(grep /mnt/smbshares/2 /etc/fstab)" != "" ]
-    then
-        args+=("/mnt/smbshares/2" "$(grep /mnt/smbshares/2 /etc/fstab | awk '{print $1}')" OFF)
-    fi
-    if [ "$(grep /mnt/smbshares/3 /etc/fstab)" != "" ]
-    then
-        args+=("/mnt/smbshares/3" "$(grep /mnt/smbshares/3 /etc/fstab | awk '{print $1}')" OFF)
-    fi
+    count=1
+    while  [ $count -le 3 ]
+    do
+        if [ "$(grep "/mnt/smbshares/$count" /etc/fstab)" != "" ]
+        then
+            args+=("/mnt/smbshares/$count" "$(grep "/mnt/smbshares/$count" /etc/fstab | awk '{print $1}')" OFF)
+        fi
+        count=$(( $count + 1))
+    done
     selected_options=$("${args[@]}" 3>&1 1>&2 2>&3)
     if [[ $selected_options == *"/mnt/smbshares/1"* ]]
     then
