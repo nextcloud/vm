fc2ba0d83eb4162ebbf1edb862e504e05fd012ea
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-23 17:55:04 +0100

    Create apps.sh

---
+++ static/apps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+mkdir -p "$SCRIPTS"
+
+# Delete, download, run
+run_main_script additional_apps
+
+exit
