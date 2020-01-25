7f77a20a833149596949d7a70db0ae1691f84a00
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-25 21:46:46 +0100

    create nextcloud_configuration.sh

---
+++ static/configuration.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+mkdir -p "$SCRIPTS"
+print_text_in_color "$ICyan" "Running the additional apps script..."
+
+if network_ok
+then
+    # Delete, download, run
+    run_static_script nextcloud_configuration.sh
+fi
+
+exit
