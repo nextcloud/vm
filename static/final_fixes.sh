568b6614362cddbb6648a1de91ab25ae521e244d
Daniel Hansson <github@hanssonit.se>
2020-01-25 18:24:12 +0100

    Update final_fixes.sh

--- static/final_fixes.sh
+++ static/final_fixes.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
 
 # Cleanup 1
 occ_command maintenance:repair
