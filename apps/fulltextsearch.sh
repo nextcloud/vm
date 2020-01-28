05f3de11f8fc80dbfc4e2bd66f94993afd2de5f2
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-28 05:25:10 +0100

    fix detail

--- apps/fulltextsearch.sh
+++ apps/fulltextsearch.sh
@@ -6,7 +6,9 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+NC_UPDATE=1 && ES_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset NC_UPDATE
+unset ES_INSTALL
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -29,10 +31,6 @@ then
 fi
 
 print_text_in_color "$ICyan" "Installing Elastic Search & Full Text Search on Nextcloud..."
-
-NC_UPDATE=1 && ES_INSTALL=1 
-unset NC_UPDATE
-unset ES_INSTALL
 
 # Nextcloud 13 is required.
 lowest_compatible_nc 13
