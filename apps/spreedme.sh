53130118bcbf6ec69030f4053d2bdc0b7f8f8984
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-28 05:26:49 +0100

    fix detail

--- apps/spreedme.sh
+++ apps/spreedme.sh
@@ -5,7 +5,8 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+NC_UPDATE=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset NC_UPDATE
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -38,9 +39,6 @@ then
 fi
 
 print_text_in_color "$ICyan" "(Re-)Installing Spreed.ME..."
-
-NC_UPDATE=1 
-unset NC_UPDATE
 
 # Install if missing
 install_if_not apache2
