cadc9fc02c1e790ef1e6c442fedb24bb33a6ada3
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-28 23:03:08 +0100

    fix travis

--- apps/collabora.sh
+++ apps/collabora.sh
@@ -36,7 +36,7 @@ print_text_in_color "$ICyan" "(Re-)Installing Collabora..."
 ram_check 2 Collabora
 cpu_check 2 Collabora
 
-# Install Collabora
+# shellcheck source=src/lib.sh
 NC_UPDATE=1 && COLLABORA_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 unset NC_UPDATE
 unset COLLABORA_INSTALL
