9123956043f95473fc3ecfc23c53cc9bd633dc24
szaimen <42591237+szaimen@users.noreply.github.com>
2020-01-28 22:42:26 +0100

    fix detail

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -6,7 +6,7 @@
 true
 
 # shellcheck source=lib.sh
-<(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
