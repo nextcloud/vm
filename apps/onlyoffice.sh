54b5526afff864af0b9d874ec9dc81876b922120
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-15 15:39:48 +0100

    move parts to the install section

--- apps/onlyoffice.sh
+++ apps/onlyoffice.sh
@@ -47,37 +47,40 @@ then
         "Reinstall OnlyOffice")
             print_text_in_color "$ICyan" "Reinstalling OnlyOffice..."
             
-            # Check if OnlyOffice or Collabora is previously installed
+            # Check if OnlyOffice is previously installed
             # If yes, then stop and prune the docker container
-            docker_prune_this 'collabora/code'
             docker_prune_this 'onlyoffice/documentserver'
-
-            # remove OnlyOffice-documentserver if activated
-            install_if_not jq
-            if occ_command_no_check app:list --output=json | jq -e '.enabled | .documentserver_community' > /dev/null
-            then
-                msg_box "Removing OnlyOffice-documentserver: $1\nYou will be given the option to abort when you hit OK."
-                any_key "Press any key to continue. Press CTRL+C to abort"
-                occ_command app:remove documentserver_community
-            fi
-
-            # Disable RichDocuments (Collabora App) if activated
-            if [ -d "$NC_APPS_PATH"/richdocuments ]
-            then
-                occ_command app:remove richdocuments
-            fi
-
-            # Disable OnlyOffice (Collabora App) if activated
-            if [ -d "$NC_APPS_PATH"/onlyoffice ]
-            then
-                occ_command app:remove onlyoffice
-            fi
         ;;
         *)
         ;;
     esac
 else
     print_text_in_color "$ICyan" "Installing OnlyOffice..."
+fi
+
+# Check if OnlyOffice or Collabora is previously installed
+# If yes, then stop and prune the docker container
+docker_prune_this 'collabora/code'
+
+# remove OnlyOffice-documentserver if activated
+install_if_not jq
+if occ_command_no_check app:list --output=json | jq -e '.enabled | .documentserver_community' > /dev/null
+then
+    msg_box "Removing OnlyOffice-documentserver: $1\nYou will be given the option to abort when you hit OK."
+    any_key "Press any key to continue. Press CTRL+C to abort"
+    occ_command app:remove documentserver_community
+fi
+
+# Disable RichDocuments (Collabora App) if activated
+if [ -d "$NC_APPS_PATH"/richdocuments ]
+then
+    occ_command app:remove richdocuments
+fi
+
+# Disable OnlyOffice (Collabora App) if activated
+if [ -d "$NC_APPS_PATH"/onlyoffice ]
+then
+    occ_command app:remove onlyoffice
 fi
 
 # Test RAM size (2GB min) + CPUs (min 2)
