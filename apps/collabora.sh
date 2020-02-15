ccf473916f39733390106217500b1ed6c8353dc1
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-15 14:44:16 +0100

    remove onlyoffice-documentserver if installed

--- apps/collabora.sh
+++ apps/collabora.sh
@@ -80,6 +80,21 @@ install_docker
 docker_prune_this 'collabora/code'
 docker_prune_this 'onlyoffice/documentserver'
 
+# remove OnlyOffice-documentserver if activated
+install_if_not jq
+if occ_command_no_check app:list --output=json | jq -e '.enabled | .documentserver_community' > /dev/null
+then
+    msg_box "Removing OnlyOffice-documentserver: $1\nYou will be given the option to abort when you hit OK."
+    any_key "Press any key to continue. Press CTRL+C to abort"
+    occ_command app:remove documentserver_community
+fi
+
+# Disable RichDocuments (Collabora App) if activated
+if [ -d "$NC_APPS_PATH"/richdocuments ]
+then
+    occ_command app:remove richdocuments
+fi
+
 # Disable RichDocuments (Collabora App) if activated
 if [ -d "$NC_APPS_PATH"/richdocuments ]
 then
