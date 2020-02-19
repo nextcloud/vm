c52605a446cc24815873e1c22c143a02f6938042
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-19 21:09:58 +0100

    Update collabora.sh

--- apps/collabora.sh
+++ apps/collabora.sh
@@ -5,11 +5,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-NC_UPDATE=1 && COLLABORA_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
-unset NC_UPDATE
-unset COLLABORA_INSTALL
-
-print_text_in_color "$ICyan" "Installing Collabora..."
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -23,9 +19,75 @@ root_check
 # Nextcloud 13 is required.
 lowest_compatible_nc 13
 
+# Check if collabora is already installed
+print_text_in_color "$ICyan" "Checking if Collabora is already installed..."
+if does_this_docker_exist 'collabora/code'
+then
+    choice=$(whiptail --radiolist "It seems like 'Collabora' is already installed.\nChoose what you want to do.\nSelect by pressing the spacebar and ENTER" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+    "Uninstall Collabora" "" ON \
+    "Reinstall Collabora" "" OFF 3>&1 1>&2 2>&3)
+
+    case "$choice" in
+        "Uninstall Collabora")
+            print_text_in_color "$ICyan" "Uninstalling Collabora..."
+            # Check if Collabora is previously installed
+            # If yes, then stop and prune the docker container
+            docker_prune_this 'collabora/code'
+
+            # Disable RichDocuments (Collabora App) if activated
+            if [ -d "$NC_APPS_PATH"/richdocuments ]
+            then
+                occ_command app:remove richdocuments
+            fi
+            
+            msg_box "Collabora was successfully uninstalled."
+            exit
+        ;;
+        "Reinstall Collabora")
+            print_text_in_color "$ICyan" "Reinstalling Collabora..."
+            
+            # Check if Collabora is previously installed
+            # If yes, then stop and prune the docker container
+            docker_prune_this 'collabora/code'
+            
+            # Disable RichDocuments (Collabora App) if activated
+            if [ -d "$NC_APPS_PATH"/richdocuments ]
+            then
+                occ_command app:remove richdocuments
+            fi
+        ;;
+        *)
+        ;;
+    esac
+else
+    print_text_in_color "$ICyan" "Installing Collabora..."
+fi
+
+# Check if OnlyOffice is previously installed
+# If yes, then stop and prune the docker container
+docker_prune_this 'onlyoffice/documentserver'
+
+# remove OnlyOffice-documentserver if activated
+if occ_command_no_check app:list --output=json | jq -e '.enabled | .documentserver_community' > /dev/null
+then
+    any_key "OnlyOffice will get uninstalled. Press any key to continue. Press CTRL+C to abort"
+    occ_command app:remove documentserver_community
+fi
+
+# Disable OnlyOffice (Collabora App) if activated
+if [ -d "$NC_APPS_PATH"/onlyoffice ]
+then
+    occ_command app:remove onlyoffice
+fi
+
 # Test RAM size (2GB min) + CPUs (min 2)
 ram_check 2 Collabora
 cpu_check 2 Collabora
+
+# shellcheck source=lib.sh
+NC_UPDATE=1 && COLLABORA_INSTALL=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+unset NC_UPDATE
+unset COLLABORA_INSTALL
 
 # Notification
 msg_box "Before you start, please make sure that port 80+443 is directly forwarded to this machine!"
@@ -41,7 +103,6 @@ msg_box "It seems like Nextcloud is not installed or that you don't use https on
 ${NCDOMAIN//\\/}.
 Please install Nextcloud and make sure your domain is reachable, or activate SSL
 on your domain to be able to run this script.
-
 If you use the Nextcloud VM you can use the Let's Encrypt script to get SSL and activate your Nextcloud domain.
 When SSL is activated, run these commands from your terminal:
 sudo curl -sLO $APP/collabora.sh
@@ -59,24 +120,6 @@ check_open_port 443 "$SUBDOMAIN"
 
 # Install Docker
 install_docker
-
-# Check if OnlyOffice or Collabora is previously installed
-# If yes, then stop and prune the docker container
-docker_prune_this 'collabora/code'
-docker_prune_this 'onlyoffice/documentserver'
-occ_command_no_check app:remove documentserver_community
-
-# Disable RichDocuments (Collabora App) if activated
-if [ -d "$NC_APPS_PATH"/richdocuments ]
-then
-    occ_command app:remove richdocuments
-fi
-
-# Disable OnlyOffice (Collabora App) if activated
-if [ -d "$NC_APPS_PATH"/onlyoffice ]
-then
-    occ_command app:remove onlyoffice
-fi
 
 # Install Collabora docker
 docker pull collabora/code:latest
@@ -103,11 +146,9 @@ then
     cat << HTTPS_CREATE > "$HTTPS_CONF"
 <VirtualHost *:443>
   ServerName $SUBDOMAIN:443
-
   <Directory /var/www>
   Options -Indexes
   </Directory>
-
   # SSL configuration, you may want to take the easy route instead and use Lets Encrypt!
   SSLEngine on
   SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
@@ -118,34 +159,26 @@ then
   SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
   SSLHonorCipherOrder     on
   SSLCompression off
-
   # Encoded slashes need to be allowed
   AllowEncodedSlashes NoDecode
-
   # Container uses a unique non-signed certificate
   SSLProxyEngine On
   SSLProxyVerify None
   SSLProxyCheckPeerCN Off
   SSLProxyCheckPeerName Off
-
   # keep the host
   ProxyPreserveHost On
-
   # static html, js, images, etc. served from loolwsd
   # loleaflet is the client part of LibreOffice Online
   ProxyPass           /loleaflet https://127.0.0.1:9980/loleaflet retry=0
   ProxyPassReverse    /loleaflet https://127.0.0.1:9980/loleaflet
-
   # WOPI discovery URL
   ProxyPass           /hosting/discovery https://127.0.0.1:9980/hosting/discovery retry=0
   ProxyPassReverse    /hosting/discovery https://127.0.0.1:9980/hosting/discovery
-
   # Main websocket
   ProxyPassMatch "/lool/(.*)/ws$" wss://127.0.0.1:9980/lool/\$1/ws nocanon
-
   # Admin Console websocket
   ProxyPass   /lool/adminws wss://127.0.0.1:9980/lool/adminws
-
   # Download as, Fullscreen presentation and Image upload operations
   ProxyPass           /lool https://127.0.0.1:9980/lool
   ProxyPassReverse    /lool https://127.0.0.1:9980/lool
