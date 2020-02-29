156f47805abbf6e4c5c5e97b9ed11e522ebc29f2
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-29 01:39:47 +0100

    update setup_secure_permissions_nextcloud to use local lib

--- static/setup_secure_permissions_nextcloud.sh
+++ static/setup_secure_permissions_nextcloud.sh
@@ -2,10 +2,19 @@
 
 # T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
 
+# Use local lib file if existant
+if [ -f /var/scripts/main/lib.sh ]
+then
+# shellcheck disable=2034,2059,2012
+true
+# shellcheck source=lib.sh
+source /var/scripts/main/lib.sh
+else
 # shellcheck disable=2034,2059,2012
 true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+fi
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
