cfec00259d430cc03f3487776c373bd251c3a551
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-29 01:32:35 +0100

    update modsecurity.sh to use local lib

--- static/modsecurity.sh
+++ static/modsecurity.sh
@@ -2,10 +2,19 @@
 
 # T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
 
+# Use local lib file if existant
+if [ -f /var/scripts/main/lib.sh ]
+then
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+source /var/scripts/main/lib.sh
+else
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+fi
 
 print_text_in_color "$ICyan" "Installing ModSecurity..."
 
