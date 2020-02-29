8223019f21f49088ed7e203c114235f95f8bcc9d
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-29 01:45:16 +0100

    update updatenotification.sh to use local lib

--- static/updatenotification.sh
+++ static/updatenotification.sh
@@ -2,11 +2,21 @@
 
 # T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
 
+# Use local lib file if existant
+if [ -f /var/scripts/main/lib.sh ]
+then
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+NC_UPDATE=1 source /var/scripts/main/lib.sh
+unset NC_UPDATE
+else
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
 NC_UPDATE=1 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 unset NC_UPDATE
+fi
 
 print_text_in_color "$ICyan" "Checking for new Nextcloud version..."
 
