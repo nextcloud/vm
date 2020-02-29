5f13f3540b55085a7c2999ef3f57cb2be4f20c1c
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-29 14:53:35 +0100

    fix detail

--- apps/tmbitwarden.sh
+++ apps/tmbitwarden.sh
@@ -40,7 +40,7 @@ fi
 # Check if local script is available.
 if [ -f "$SCRIPTS"/apps/tmbitwarden.sh ]
 then
-    msg_box "It seems like you have chosen the option 'Security' during the startup script and are using all files locally.\nPlease note that continuing will download scripts from github for installing and updating, that will not be checked for integrity."
+    msg_box "It seems like you have chosen the option 'Security' during the startup script and are using all files locally.\nPlease note that continuing will download scripts from github for installing and updating bitwarden, that will not be checked for integrity."
     if [[ "no" == $(ask_yes_or_no "Do you want to install bitwarden anyway?") ]]
     then
         exit
