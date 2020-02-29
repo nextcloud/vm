2bc184a2a86bc93a84add932425810851a3a49fd
szaimen <42591237+szaimen@users.noreply.github.com>
2020-02-29 16:32:32 +0100

    create "$SCRIPTS" if not existant

--- nextcloud_vm.sh
+++ nextcloud_vm.sh
@@ -14,12 +14,12 @@ SCRIPTS=/var/scripts
 # Todo: Verify the state of the downloaded package with a checksum?
 
 # Todo: Extract everything to "$SCRIPTS"
+mkdir -p "$SCRIPTS"
 
 # This is for testing purposes only; should get removed if everything above is ready.
 if [ ! -d "$SCRIPTS"/apps ] && [ ! -d "$SCRIPTS"/main ] && [ ! -d "$SCRIPTS"/lets-encrypt ] && [ ! -d "$SCRIPTS"/static ]
 then
     git clone https://github.com/nextcloud/vm.git "$SCRIPTS"
-    
     # Remove all unnecessary files
     rm -r "$SCRIPTS"/.git
     rm "$SCRIPTS"/LICENSE
