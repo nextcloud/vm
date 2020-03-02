d584650c967bebef2da266505f0a68ad00ba6837
szaimen <42591237+szaimen@users.noreply.github.com>
2020-03-02 20:38:30 +0100

    update automatic_updates... (#1134)
    to disable notifications for new app or software updates
    
    Co-authored-by: Daniel Hansson <github@hanssonit.se>

--- static/automatic_updates.sh
+++ static/automatic_updates.sh
@@ -32,6 +32,7 @@ In the next step you will be able to choose to proceed or exit."
 
 if [[ "yes" == $(ask_yes_or_no "Do you want to enable automatic updates?") ]]
 then
+    occ_command config:app:set updatenotification notify_groups --value="[]"
     touch $VMLOGS/update.log
     crontab -u root -l | { cat; echo "0 $AUT_UPDATES_TIME * * 6 $SCRIPTS/update.sh minor >> $VMLOGS/update.log"; } | crontab -u root -
     if [[ "yes" == $(ask_yes_or_no "Do you want to reboot your server after every update? *recommended*") ]]
