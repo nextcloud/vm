3578ebd388874ae098448396b2e94a09829424a4
enoch85 <github@hanssonit.se>
2020-04-15 21:26:43 +0200

    locales
    Signed-off-by: enoch85 <github@hanssonit.se>

--- static/locales.sh
+++ static/locales.sh
@@ -2,24 +2,22 @@
 
 # T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
 
-
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
 . <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/20.04_testing/lib.sh)
-
 
 # Set locales
 print_text_in_color "$ICyan" "Setting locales..."
 KEYBOARD_LAYOUT=$(localectl status | grep "Layout" | awk '{print $3}')
 if [ "$KEYBOARD_LAYOUT" = "us" ]
 then
-    print_text_in_color "$ICyan" "US locales are set."
+    print_text_in_color "$ICyan" "US locales are already set."
 elif [ "$KEYBOARD_LAYOUT" = "se" ]
-then 
+then
     sudo locale-gen "sv_SE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
 elif [ "$KEYBOARD_LAYOUT" = "de" ]
-then 
+then
     sudo locale-gen "de_DE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
 elif [ "$KEYBOARD_LAYOUT" = "us" ]
 then
