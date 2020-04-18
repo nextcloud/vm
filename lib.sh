eec8cbba154ff13d453c6cb85d8505124c248140
szaimen <szaimen@e.mail.de>
2020-04-18 03:14:06 +0200

    fix detail

--- lib.sh
+++ lib.sh
@@ -709,7 +709,7 @@ calc_wt_size() {
 
 # example: is_app_enabled documentserver_community
 is_app_enabled() {
-if occ_command app:list | sed 'Disabled:/Q' | grep -q "$1"
+if occ_command app:list | sed '/Disabled:/Q' | grep -q "$1"
 then
     return 0
 else
