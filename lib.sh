3650e73e0204886754cb0219e526c51d78cad035
szaimen <szaimen@e.mail.de>
2020-04-25 09:11:02 +0200

    update notify_admin_gui (#1193)

--- lib.sh
+++ lib.sh
@@ -1183,7 +1183,7 @@ fi
 print_text_in_color "$ICyan" "Posting notification to users that are admins, this might take a while..."
 occ_command_no_check user:list | sed 's|^  - ||g' | sed 's|:.*||' | while read -r admin
 do
-    if occ_command_no_check user:info "$admin" | cut -d "-" -f2 | grep -q " admin"
+    if occ_command_no_check user:info "$admin" | cut -d "-" -f2 | grep -xq " admin"
     then
         print_text_in_color "$IGreen" "Posting '$1' to: $admin"
         occ_command_no_check notification:generate -l "$2" "$admin" "$1"
