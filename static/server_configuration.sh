3d7e1869c3c4c437f0a6978875645245020a185b
Daniel Hansson <github@hanssonit.se>
2020-05-07 23:18:04 +0200

    cleanup

--- static/server_configuration.sh
+++ static/server_configuration.sh
@@ -59,6 +59,10 @@ https://www.techandme.se/open-port-80-443/"
             print_text_in_color "$ICyan" "OK, but if you want to run it later, just type: sudo bash $SCRIPTS/activate-tls.sh"
             any_key "Press any key to continue..."
         fi
+        
+        # Just make sure they are gone
+        rm -f "$SCRIPTS/test-new-config.sh"
+        rm -f "$SCRIPTS/activate-tls.sh"
         clear
     ;;&
     *)
