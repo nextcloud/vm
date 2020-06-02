4a7cd9c1ae7d539b7b3cd2ac2bb016eddb0fc98b
Daniel Hansson <github@hanssonit.se>
2020-06-02 13:03:41 +0200

    make it more robust

--- apps/talk.sh
+++ apps/talk.sh
@@ -32,7 +32,7 @@ lowest_compatible_nc 13
 
 # Check if adminer is already installed
 print_text_in_color "$ICyan" "Checking if Talk is already installed..."
-if [ -n "$(occ_command_no_check config:app:get spreed turn_servers | sed 's/\[\]//')" ]
+if [ -n "$(occ_command_no_check config:app:get spreed turn_servers | sed 's/\[\]//')" ] || is_this_installed coturn
 then
     choice=$(whiptail --radiolist "It seems like 'Nextcloud Talk' is already installed.\nChoose what you want to do.\nSelect by pressing the spacebar and ENTER" "$WT_HEIGHT" "$WT_WIDTH" 4 \
     "Uninstall Nextcloud Talk" "" OFF \
