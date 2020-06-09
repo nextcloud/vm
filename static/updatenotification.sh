1644843cc2cb16f3f873d4081d0da9dea38da392
szaimen <szaimen@e.mail.de>
2020-06-09 02:46:20 +0200

    improve updatenotification.sh

--- static/updatenotification.sh
+++ static/updatenotification.sh
@@ -19,32 +19,58 @@ debug_mode
 # Check if root
 root_check
 
-REPORTEDNCVERSION=""
+NCMIN=$(curl -s -m 900 $NCREPO/ | sed --silent 's/.*href="nextcloud-\([^"]\+\).zip.asc".*/\1/p' | sort --version-sort | grep "${CURRENTVERSION%%.*}" | tail -1)
+REPORTEDMAJ=""
+REPORTEDMIN=""
 
-if [ "$CURRENTVERSION" == "$NCVERSION" ]
+if [ "$CURRENTVERSION" == "$NCVERSION" ] && [ "$CURRENTVERSION" == "$NCMIN" ]
 then
     print_text_in_color "$IGreen" "You already run the latest version! ($NCVERSION)"
     exit
 fi
 
-if [ "$REPORTEDNCVERSION" == "$NCVERSION" ]
+if [ "$REPORTEDMAJ" == "$NCVERSION" ] && [ "$REPORTEDMIN" == "$NCMIN" ]
 then
-    print_text_in_color "$ICyan" "The notification regarding the new Nextcloud update has been already reported! ($NCVERSION)"
+    print_text_in_color "$ICyan" "The notification regarding the new Nextcloud update has been already reported!"
     exit
 fi
 
-if version_gt "$NCVERSION" "$CURRENTVERSION"
+if [ "$NCVERSION" == "$NCMIN" ] && version_gt "$NCMIN" "$REPORTEDMIN"
 then
-    if crontab -l -u root | grep $SCRIPTS/update.sh
+    sed -i "s|^REPORTEDMAJ.*|REPORTEDMAJ=$NCVERSION|" $SCRIPTS/updatenotification.sh
+    sed -i "s|^REPORTEDMIN.*|REPORTEDMIN=$NCMIN|" $SCRIPTS/updatenotification.sh
+    if crontab -l -u root | grep -q $SCRIPTS/update.sh
     then
         notify_admin_gui \
         "New Nextcloud version!" \
-        "Nextcloud $NCVERSION just became available. Since you are running Automatic Updates at $AUT_UPDATES_TIME:00, you don't need to bother about updating the server manually, as that's already taken care of."
-        sed -i "s|^REPORTEDNCVERSION.*|REPORTEDNCVERSION=$NCVERSION|" $SCRIPTS/updatenotification.sh
+        "Nextcloud $NCVERSION just became available. Since you are running Automatic Updates on Saturdays at $AUT_UPDATES_TIME:00, you don't need to bother about updating the server manually, as that's already taken care of."
     else
         notify_admin_gui \
         "Update available!" \
         "Nextcloud $NCVERSION is available. Please run 'sudo bash /var/scripts/update.sh' from your CLI to update your server."
-        sed -i "s|^REPORTEDNCVERSION.*|REPORTEDNCVERSION=$NCVERSION|" $SCRIPTS/updatenotification.sh
     fi
+    exit
+fi
+
+if version_gt "$NCMIN" "$REPORTEDMIN"
+then
+    sed -i "s|^REPORTEDMIN.*|REPORTEDMIN=$NCMIN|" $SCRIPTS/updatenotification.sh
+    if crontab -l -u root | grep -q $SCRIPTS/update.sh
+    then
+        notify_admin_gui \
+        "New Nextcloud version!" \
+        "Nextcloud $NCMIN just became available. Since you are running Automatic Updates on Saturdays at $AUT_UPDATES_TIME:00, you don't need to bother about updating the server manually, as that's already taken care of."
+    else
+        notify_admin_gui \
+        "Update available!" \
+        "Nextcloud $NCMIN is available. Please run 'sudo bash /var/scripts/update.sh minor' from your CLI to update your server."
+    fi
+fi
+
+if version_gt "$NCVERSION" "$REPORTEDMAJ"
+then
+    sed -i "s|^REPORTEDMAJ.*|REPORTEDMAJ=$NCVERSION|" $SCRIPTS/updatenotification.sh
+    notify_admin_gui \
+    "Update available!" \
+    "Nextcloud $NCVERSION is available. Please run 'sudo bash /var/scripts/update.sh' from your CLI to update your server."
 fi
