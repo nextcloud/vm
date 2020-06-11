e7638bba2af8dc25549429982b3068c0deb47d12
Daniel Hansson <github@hanssonit.se>
2020-06-12 00:04:04 +0200

    add memory

--- lib.sh
+++ lib.sh
@@ -1154,7 +1154,7 @@ home_sme_server() {
 # OLD DISKS: "Samsung SSD 860" || ST5000LM000-2AN1  || ST5000LM015-2E81
 if lshw -c system | grep -q NUC8i3BEH
 then
-    if lshw -c memory | grep -q BLS16G4
+    if lshw -c memory | grep -q BLS16G4 || lshw -c memory | grep -q 18ASF2G72HZ
     then
         if lshw -c disk | grep -q ST2000LM015-2E81 || lshw -c disk | grep -q WDS400
         then
