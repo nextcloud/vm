7a7f3be0fae15beda31a07da384e5b0de8f5e6c6
Daniel Hansson <github@hanssonit.se>
2020-06-28 22:01:14 +0200

    travis

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -418,7 +418,7 @@ if ! is_this_installed php"$PHPVER"-imagick
 then
     notify_admin_gui \
     "Regarding Imagick not being installed" \
-    "As you may have noticed, Imagick is not installed. We care about your security, and here's the reason: https://github.com/nextcloud/server/issues/13>
+    "As you may have noticed, Imagick is not installed. We care about your security, and here's the reason: https://github.com/nextcloud/server/issues/13099."
 fi
 
 # Fixes https://github.com/nextcloud/vm/issues/58
