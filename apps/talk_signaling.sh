702dd20157b58ae3fd5b02fa1cd304a681b90521
Daniel Hansson <github@hanssonit.se>
2020-07-11 09:46:51 +0200

    Create Signaling script (#1)
    initial support for signaling server
    
    Signed-off-by: morph027 <stefan.heitmueller@gmx.com>

---
+++ apps/talk_signaling.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Check if root
+root_check
+
+### TLS?
+
+### Proxy config?
+
+### more?
