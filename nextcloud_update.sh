1fdf40d95a563370c2fd356e5acbe34fe45e63c2
szaimen <szaimen@e.mail.de>
2020-07-20 17:51:41 +0200

    allow automatic backup and restore of app status

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -395,6 +395,15 @@ fi
 
 countdown "Backing up files and upgrading to Nextcloud $NCVERSION in 10 seconds... Press CTRL+C to abort." "10"
 
+# Backup app status
+print_text_in_color "$ICyan" "Getting and backing up the status of apps for later. This can take a while..."
+ENABLED_APPS="$(occ_command app:list | sed '/Enabled/d' | sed '/Disabled/d' | sed 's|^  - ||g' | sed 's|:.*||')"
+declare -Ag APPSTORAGE
+for app in $ENABLED_APPS
+do
+    APPSTORAGE[$app]="$(occ_command config:app:get '$app' enabled)"
+done
+
 # Stop Apache2
 check_command systemctl stop apache2.service
 
@@ -550,7 +559,14 @@ then
 fi
 
 # Recover apps that exists in the backed up apps folder
-run_script STATIC recover_apps
+#run_script STATIC recover_apps
+
+# Restore app status
+print_text_in_color "$ICyan" "Restoring the status of apps. This can take a while..."
+for app in "${!APPSTORAGE[@]}"
+do 
+    occ_command_no_check config:app:set "$app" enabled --value="${APPSTORAGE[$app]}"
+done
 
 # Remove header for Nextcloud 14 (already in .htaccess)
 if [ -f /etc/apache2/sites-available/"$(hostname -f)".conf ]
