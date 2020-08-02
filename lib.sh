4c810cfa70582f5ee2a6a3a9f7888a10c739b669
Daniel Hansson <github@hanssonit.se>
2020-08-02 14:27:30 +0200

    merge master into rpi (#1340)
    Co-authored-by: Kelly Byrd <kellybyrd@users.noreply.github.com>
    Co-authored-by: szaimen <szaimen@e.mail.de>

--- lib.sh
+++ lib.sh
@@ -785,7 +785,7 @@ calc_wt_size() {
 
 # example: is_app_enabled documentserver_community
 is_app_enabled() {
-if occ_command app:list | sed '/Disabled/,$d' | grep -q " $1"
+if occ_command app:list | sed '/Disabled/,$d' | awk '{print$2}' | tr -d ':' | sed '/^$/d' | grep -q "^$1$"
 then
     return 0
 else
