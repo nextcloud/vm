f75c8549c48ce226e37eba9e59309ad228cd97d4
Daniel Hansson <github@hanssonit.se>
2020-08-06 21:27:13 +0200

    change link to login

--- static/index.php
+++ static/index.php
@@ -99,7 +99,7 @@
             The easiest way to get a real TLS certificate is to run the Lets' Encrypt script included on this server.<br>
             Just run 'sudo bash /var/scripts/activate-tls.sh' from your terminal and follow the instructions.
             <h3>
-                <a href="https://docs.hanssonit.se/s/W6fMouPiqQz3_Mog/virtual-machines-vm/d/W6fMquPiqQz3_Moi/nextcloud-vm-first-setup-instructions?currentPageId=W6yn7ePiqQz3_Mpi" target="_blank">Login details</a>
+                <a href="https://docs.hanssonit.se/s/W6fMouPiqQz3_Mog/virtual-machines-vm/d/W6fMquPiqQz3_Moi/nextcloud-vm-first-setup-instructions?currentPageId=W6yo9OPiqQz3_Mpy" target="_blank">Login details</a>
             </h3>
         </div>
 
