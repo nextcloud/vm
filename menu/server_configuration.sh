c565fda54d3ca28c977262adbc74ac45283560a0
enoch85 <github@hanssonit.se>
2020-08-10 17:54:01 +0200

    travis
    Signed-off-by: enoch85 <github@hanssonit.se>

--- menu/server_configuration.sh
+++ menu/server_configuration.sh
@@ -37,7 +37,7 @@ choice=$(whiptail --title "Server configurations" --checklist "Choose what you w
 "Security" "(Add extra security based on this http://goo.gl/gEJHi7)" OFF \
 "Static IP" "(Set static IP in Ubuntu with netplan.io)" OFF \
 "Disk Check" "(Check for S.M.A.R.T errors on your disks every week on Mondays)" OFF \
-"Fail2ban Statuscheck" "Check banned IPs and attempts") OFF \
+"Fail2ban Statuscheck" "(Check status of banned IPs in iptables and Fail2ban)" OFF \
 "Automatic updates" "(Automatically update your server every week on Sundays)" OFF 3>&1 1>&2 2>&3)
 
 case "$choice" in
