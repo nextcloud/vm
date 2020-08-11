dfc946d885b5a26e8b4c8695c4184b411a02055a
szaimen <szaimen@e.mail.de>
2020-08-12 01:49:33 +0200

    update bitwardden to allow subdomains

--- apps/tmbitwarden.sh
+++ apps/tmbitwarden.sh
@@ -41,8 +41,7 @@ print_text_in_color "$ICyan" "Installing Bitwarden password manager..."
 
 msg_box "Bitwarden is a password manager that is seperate from Nextcloud, though we provide this service because it's self hosted and secure.
 
-If you just want to run Bitwarden locally (not connecting your smartphone) then you can use 'localhost' as domain.
-If you on the other hand want to run this on a domain, then please create a DNS record and point it to this server.
+If you on the other hand want to run this on a subdomain, then please create a DNS record and point it to this server.
 In the process of setting up Bitwarden you will be asked to generate an TLS cert with Let's Enrypt so no need to get your own prior to this setup.
 
 The script is based on this documentation: https://help.bitwarden.com/article/install-on-premise/
@@ -51,8 +50,8 @@ It's a good idea to read that before you start this script.
 Please also report any issues regarding this script setup to $ISSUES"
 
 msg_box "The necessary preparations to run expose Bitwarden to the internet are:
-1. The HTTP proxy and HTTPS ports for Bitwarden are 8080 and 8443, please open those ports before running this script.
-2. Please create a DNS record and point that to this server.
+1. Please open port 443 and 80 and point to this server.
+2. Please create a DNS record for your subdomain and point that to this server.
 3. Raise the amount of RAM to this server to at least 3 GB."
 
 if [[ "no" == $(ask_yes_or_no "Have you made the necessary preparations?") ]]
@@ -61,9 +60,22 @@ msg_box "OK, please do the necessary preparations before you run this script and
 
 To run this script again, execute $SCRIPTS/menu.sh and choose Additional Apps --> Bitwarden"
     exit
-else
-    sleep 0.1
 fi
+
+msg_box "ATTENTION!
+
+You will see in the next step a setup which is provided by bitwarden.
+You have to make your choices based on the following instructions, 
+otherwise things can go wrong since we have no influence on those scripts.
+Best is if you take a picture of those instructions so that you remember them later on.
+And please don't worry: we will create a valid SSL certificate afterwards ourselves.
+
+1. Enter your subdomain like e.g. 'bitwarden.example.com'
+2. You will be asked if you want to use 'lets encrypt'; Please type in no: 'n'
+3. Enter your installation ID, which you must get on https://bitwarden.com/host
+4. Enter your installation key, which you will also get there
+5. You will be asked if you have a SSL certificate which you want to use; Please type in no: 'n'
+6. You will be asked if you want to generate a self-signed SSL certificate; Please type in no: 'n'"
 
 # Install Docker
 install_docker
@@ -78,22 +90,125 @@ cd /root
 curl_to_dir "https://raw.githubusercontent.com/bitwarden/core/master/scripts" "bitwarden.sh" "/root"
 chmod +x /root/bitwarden.sh
 check_command ./bitwarden.sh install
-sed -i "s|http_port.*|http_port: 8080|g" /root/bwdata/config.yml
-sed -i "s|https_port.*|https_port: 8443|g" /root/bwdata/config.yml
+sed -i "s|http_port.*|http_port: 5178|g" /root/bwdata/config.yml
+sed -i "s|https_port.*|https_port: 5179|g" /root/bwdata/config.yml
+# Get Subdomain from config.yml and change it to https
+SUBDOMAIN=$(grep ^url /root/bwdata/config.yml)
+SUBDOMAIN=${SUBDOMAIN##*url: http://}
+sed -i "s|^url: .*|url: https://$SUBDOMAIN|g" /root/bwdata/config.yml
+sed -i 's|http://|https://|g' /root/bwdata/env/global.override.env
 check_command ./bitwarden.sh rebuild
 check_command ./bitwarden.sh start
-if check_command ./bitwarden.sh updatedb
-then
-msg_box "Bitwarden was sucessfully installed! Please visit $(grep 'url:' /root/bwdata/config.yml | awk '{print$2}'):8443 to setup your account.
+check_command ./bitwarden.sh updatedb
 
-After the account it setup, please disable user registration by running sudo bash $SCRIPTS/menu.sh and choose:
-Additional Apps --> Bitwarden Registration"
-else
-msg_box "Bitwarden installation failed! We will now remove necessary configs to be able to run this script again"
-    rm -rf /root/bwdata/
+# Produce reverse-proxy config and get lets-encrypt certificate
+
+# Curl the lib another time to get the correct HTTPS_CONF
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check if $SUBDOMAIN exists and is reachable
+print_text_in_color "$ICyan" "Checking if $SUBDOMAIN exists and is reachable..."
+domain_check_200 "$SUBDOMAIN"
+
+# Check open ports with NMAP
+check_open_port 80 "$SUBDOMAIN"
+check_open_port 443 "$SUBDOMAIN"
+
+# Install Apache2
+install_if_not apache2
+
+# Enable Apache2 module's
+a2enmod proxy
+a2enmod proxy_wstunnel
+a2enmod proxy_http
+a2enmod ssl
+
+if [ -f "$HTTPS_CONF" ]
+then
+    a2dissite "$SUBDOMAIN.conf"
+    rm -f "$HTTPS_CONF"
 fi
 
-# Start Apache2
+if [ ! -f "$HTTPS_CONF" ];
+then
+    cat << HTTPS_CREATE > "$HTTPS_CONF"
+<VirtualHost *:443>
+    ServerName $SUBDOMAIN:443
+    SSLEngine on
+    ServerSignature On
+    SSLHonorCipherOrder on
+    SSLCertificateChainFile $CERTFILES/$SUBDOMAIN/chain.pem
+    SSLCertificateFile $CERTFILES/$SUBDOMAIN/cert.pem
+    SSLCertificateKeyFile $CERTFILES/$SUBDOMAIN/privkey.pem
+    SSLOpenSSLConfCmd DHParameters $DHPARAMS_SUB
+    
+    SSLProtocol             all -SSLv2 -SSLv3
+    SSLCipherSuite ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS
+    LogLevel warn
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    # Just in case - see below
+    SSLProxyEngine On
+    SSLProxyVerify None
+    SSLProxyCheckPeerCN Off
+    SSLProxyCheckPeerName Off
+    # contra mixed content warnings
+    RequestHeader set X-Forwarded-Proto "https"
+    # basic proxy settings
+    ProxyRequests off
+    ProxyPassMatch (.*)(\/websocket)$ "ws://127.0.0.1:5178/$1$2"
+    ProxyPass / "http://127.0.0.1:5178/"
+    ProxyPassReverse / "http://127.0.0.1:5178/"
+        
+    <Location />
+        ProxyPassReverse /
+    </Location>
+</VirtualHost>
+HTTPS_CREATE
+
+    if [ -f "$HTTPS_CONF" ];
+    then
+        print_text_in_color "$IGreen" "$HTTPS_CONF was successfully created."
+        sleep 1
+    else
+        print_text_in_color "$IRed" "Unable to create vhost, exiting..."
+        print_text_in_color "$IRed" "Please report this issue here $ISSUES"
+        exit 1
+    fi
+fi
+
+# Install certbot (Let's Encrypt)
+install_certbot
+
+# Generate certs and  auto-configure  if successful
+if generate_cert  "$SUBDOMAIN"
+then
+    # Generate DHparams chifer
+    if [ ! -f "$DHPARAMS_SUB" ]
+    then
+        openssl dhparam -dsaparam -out "$DHPARAMS_SUB" 4096
+    fi
+    print_text_in_color "$IGreen" "Certs are generated!"
+    a2ensite "$SUBDOMAIN.conf"
+    restart_webserver
+else
+    last_fail_tls "$SCRIPTS"/apps/tmbitwarden.sh
+fi
+
+# Add prune command
+{
+echo "#!/bin/bash"
+echo "docker system prune -a --force"
+echo "exit"
+} > "$SCRIPTS/dockerprune.sh"
+chmod a+x "$SCRIPTS/dockerprune.sh"
+crontab -u root -l | { cat; echo "@weekly $SCRIPTS/dockerprune.sh"; } | crontab -u root -
+print_text_in_color "$ICyan" "Docker automatic prune job added."
 check_command systemctl start apache2.service
+
+msg_box "Bitwarden was sucessfully installed! Please visit $SUBDOMAIN to setup your account.
+
+# After the account it setup, please disable user registration by running sudo bash $SCRIPTS/menu.sh and choose:
+# Additional Apps --> Bitwarden Registration"
 
 exit
