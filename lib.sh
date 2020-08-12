a524e779e51bab69a05878a4f7fd19dd940061e5
Daniel Hansson <github@hanssonit.se>
2020-08-12 23:09:22 +0200

    add home

--- lib.sh
+++ lib.sh
@@ -94,6 +94,7 @@ UNIXUSER_PROFILE="/home/$UNIXUSER/.bash_profile"
 ROOT_PROFILE="/root/.bash_profile"
 # User for Bitwarden
 BITWARDEN_USER=bitwarden
+BITWARDEN_HOME=/home/"$BITWARDEN_USER"
 # Database
 SHUF=$(shuf -i 25-29 -n 1)
 PGDB_PASS=$(gen_passwd "$SHUF" "a-zA-Z0-9@#*=")
