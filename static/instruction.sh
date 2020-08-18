f65678b7d9677169a75aec6ba2d0bef001d1e9b2
Daniel Hansson <github@hanssonit.se>
2020-08-18 11:36:44 +0200

    fix the text
    now every line is shown on smaller screens

--- static/instruction.sh
+++ static/instruction.sh
@@ -25,15 +25,13 @@ cat << INST2
 | please use an SSH terminal like Putty. You can download it here:      |
 | https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html        |
 | If you are running Windows 10 1809 or later, you can simply use SSH   |
-| from the command prompt.                                              |
-| Connect like this: ssh ncadmin@local.IP.of.this.server                |
+| from the command prompt. Connect like this:                           |
+| ssh ncadmin@local.IP.of.this.server                                   |
 |                                                                       |
-| You can schedule the Nextcloud update process using a cron job.       |
-| This is done using a script built into this server that automatically |
-| updates Nextcloud, sets secure permissions, and logs the successful   |
-| update to $VMLOGS/update.log                               |
-| Just choose to configure it when asked to do so later in this script. |
-|                                                                       |
+| This server could be made maintenace free by using automatic updates  |
+| with the built in update script. If you want automatic updates on     |
+| a weekly schelude, choose to configure it later during this setup.    |
+|
 |  ###################### T&M Hansson IT - $(date +"%Y") ######################  |
 +-----------------------------------------------------------------------+
 INST2
