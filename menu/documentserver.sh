036a4d80590262362f8385771507d1d56306e813
Daniel Hansson <github@hanssonit.se>
2020-08-20 15:16:44 +0200

    remove the recomendation all togehter
    It might change in the future and one less thing to maintain. The checks are in place anyway.

--- menu/documentserver.sh
+++ menu/documentserver.sh
@@ -17,9 +17,9 @@ debug_mode
 root_check
 
 choice=$(whiptail --title "Which Documentserver do you want to install?" --radiolist "Automatically configure and install the selected Documentserver.\nSelect by pressing the spacebar and ENTER\nYou can view this menu later by running 'sudo bash $SCRIPTS/menu.sh'" "$WT_HEIGHT" "$WT_WIDTH" 4 \
-"Collabora (Docker)" "(Online editing [2GB RAM] - Extra Subdomain required)" OFF \
+"Collabora (Docker)" "(Online editing - Extra Subdomain required)" OFF \
 "Collabora (Integrated)" "(Online editing - No Subdomain required)" OFF \
-"OnlyOffice (Docker)" "(Online editing [2GB RAM] - Extra Subdomain required)" OFF \
+"OnlyOffice (Docker)" "(Online editing - Extra Subdomain required)" OFF \
 "OnlyOffice (Integrated)" "(Online editing - No Subdomain required)" OFF 3>&1 1>&2 2>&3)
 
 case "$choice" in
