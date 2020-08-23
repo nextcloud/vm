9a6f2d1b7127930e61bff6e3408a6fcfc469159e
szaimen <szaimen@e.mail.de>
2020-08-23 11:03:18 +0200

    Update tmbitwarden.sh (#1382)
    Fix #1384
    
    Co-authored-by: Daniel Hansson <github@hanssonit.se>

--- apps/tmbitwarden.sh
+++ apps/tmbitwarden.sh
@@ -235,12 +235,11 @@ then
     ProxyPass / "http://127.0.0.1:5178/"
     ProxyPassReverse / "http://127.0.0.1:5178/"
     # Extra (remote) headers
-    RemoteIPHeader X-Forwarded-For
-    RemoteIPHeader X-Real-IP
-    RemoteIPHeader X-Forwarded-Proto
+    RequestHeader set X-Real-IP %{REMOTE_ADDR}s
     Header set X-XSS-Protection "1; mode=block"
     Header set Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
     Header set X-Content-Type-Options nosniff
+    Header set Content-Security-Policy "frame-ancestors 'self'"
     <Location />
         ProxyPassReverse /
     </Location>
@@ -295,5 +294,11 @@ Some notes to the Bitwarden service:
 to START Bitwarden, simply execute: 'systemctl start bitwarden'
 to STOP Bitwarden, simply execute: 'systemctl stop bitwarden'
 to RESTART Bitwarden, simply execute: 'systemctl restart bitwarden'"
+
+msg_box "In case you want to backup Bitwarden, you should know that the MSSQL files are stored here:
+/var/lib/docker/volumes/docker_mssql_data/_data
+
+This is beacuse we run the database as a Docker container, and not directly on the filesystem - which otherwise would be the default.
+Reason? We found it to be more stable running in a container, several sources in their issue tracker also confirms the same thing."
 
 exit
