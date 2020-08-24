6c13e39a9f399bff196672718ed836f674fd4690
morph027 <stefan.heitmueller@gmx.com>
2020-08-24 19:44:50 +0200

    cleanup, re-order

--- apps/talk_signaling.sh
+++ apps/talk_signaling.sh
@@ -33,7 +33,7 @@ lowest_compatible_nc 19
 
 ####################### TALK (COTURN)
 
-# Check if adminer is already installed
+# Check if talk/spreed is already installed
 print_text_in_color "$ICyan" "Checking if Talk is already installed..."
 if [ -n "$(occ_command_no_check config:app:get spreed turn_servers | sed 's/\[\]//')" ] || is_this_installed coturn
 then
@@ -48,8 +48,8 @@ then
             occ_command_no_check config:app:delete spreed turn_servers
             occ_command_no_check config:app:delete spreed signaling_servers
             occ_command_no_check app:remove spreed
-            rm $TURN_CONF
-            apt-get purge coturn -y
+            rm "$TURN_CONF" "$SIGNALING_SERVER_CONF"
+            apt-get purge coturn nats-server janus nextcloud-spreed-signaling -y
             msg_box "Nextcloud Talk was successfully uninstalled and all settings were resetted."
             exit
         ;;
@@ -59,8 +59,8 @@ then
             occ_command_no_check config:app:delete spreed turn_servers
             occ_command_no_check config:app:delete spreed signaling_servers
             occ_command_no_check app:remove spreed
-            rm $TURN_CONF
-            apt-get purge coturn -y
+            rm "$TURN_CONF" "$SIGNALING_SERVER_CONF"
+            apt-get purge coturn nats-server janus nextcloud-spreed-signaling -y
         ;;
         *)
         ;;
@@ -79,8 +79,8 @@ Please install Nextcloud and make sure your domain is reachable, or activate TLS
 on your domain to be able to run this script.
 If you use the Nextcloud VM you can use the Let's Encrypt script to get TLS and activate your Nextcloud domain.
 When TLS is activated, run these commands from your terminal:
-sudo curl -sLO $APP/talk.sh
-sudo bash talk.sh"
+sudo curl -sLO $APP/talk_signaling.sh
+sudo bash talk_signaling.sh"
     exit 1
 fi
 
@@ -164,8 +164,8 @@ check_command systemctl restart coturn.service
 msg_box "You have to open $TURN_PORT TCP/UDP in your firewall or your TURN/STUN server won't work!
 After you hit OK the script will check for the firewall and eventually exit on failure.
 To run again the setup, after fixing your firewall:
-sudo -sLO $APP/talk.sh
-sudo bash talk.sh"
+sudo -sLO $APP/talk_signaling.sh
+sudo bash talk_signaling.sh"
 
 # Check if the port is open
 check_open_port "$TURN_PORT" "$TURN_DOMAIN"
@@ -173,7 +173,7 @@ check_open_port "$TURN_PORT" "$TURN_DOMAIN"
 # Enable Spreed (Talk)
 STUN_SERVERS_STRING="[\"$TURN_DOMAIN:$TURN_PORT\"]"
 TURN_SERVERS_STRING="[{\"server\":\"$TURN_DOMAIN:$TURN_PORT\",\"secret\":\"$TURN_SECRET\",\"protocols\":\"udp,tcp\"}]"
-SIGNALING_SERVERS_STRING="{\"servers\":[{\"server\":\"https://$HPB_DOMAIN/\",\"verify\":true}],\"secret\":\"$NC_SECRET\"}"
+SIGNALING_SERVERS_STRING="{\"servers\":[{\"server\":\"https://$SUBDOMAIN/\",\"verify\":true}],\"secret\":\"$NC_SECRET\"}"
 if ! is_app_installed spreed
 then
     install_and_enable_app spreed
@@ -210,47 +210,73 @@ domain_check_200 "$SUBDOMAIN"
 check_open_port 80 "$SUBDOMAIN"
 check_open_port 443 "$SUBDOMAIN"
 
-# Check if HPB is already installed
-is_process_running dpkg
-is_process_running apt
-print_text_in_color "$ICyan" "Checking if ${DESCRIPTION} is already installed..."
-if ! is_this_installed nextcloud-spreed-signaling
-then
-    choice=$(whiptail --radiolist "It seems like '${DESCRIPTION}' is already installed.\nChoose what you want to do.\nSelect by pressing the spacebar and ENTER" "$WT_HEIGHT" "$WT_WIDTH" 4 \
-    "Uninstall ${DESCRIPTION}" "" OFF \
-    "Reinstall ${DESCRIPTION}" "" ON 3>&1 1>&2 2>&3)
-    case "$choice" in
-        "Uninstall ${DESCRIPTION}")
-          # TODO: remove nats, janus and signaling-server
-          :
-        ;;
-        "Reinstall ${DESCRIPTION}")
-          # TODO: remove nats, janus and signaling-server
-          :
-        ;;
-        *)
-        ;;
-    esac
-else
-    print_text_in_color "$ICyan" "Installing ${DESCRIPTION}..."
-fi
-
-# Install
-. /etc/lsb-release
-for repo in nats-server nextcloud-spreed-signaling janus
-do
-  # curl_to_dir doesn't fit here as the name of the fetched file and output differs
-  curl -sL -o "/etc/apt/trusted.gpg.d/morph027-${repo}.asc" "https://packaging.gitlab.io/${repo}/gpg.key"
-done
-
-echo "deb [arch=amd64] https://packaging.gitlab.io/nextcloud-spreed-signaling signaling main" > /etc/apt/sources.list.d/morph027-nextcloud-spreed-signaling.list
-echo "deb [arch=amd64] https://packaging.gitlab.io/janus/$DISTRIB_CODENAME $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/morph027-janus.list
+# NATS
+## Installation
+curl -sL -o "/etc/apt/trusted.gpg.d/morph027-nats-server.asc" "https://packaging.gitlab.io/nats-server/gpg.key"
 echo "deb [arch=amd64] https://packaging.gitlab.io/nats-server nats main" > /etc/apt/sources.list.d/morph027-nats-server.list
-
 apt update -q4 & spinner_loading
-check_command apt-get install -y nextcloud-spreed-signaling nats-server janus
+install_if_not nats-server
+## Configuration
+mkdir -p /etc/nats
+install -d -o nats -g nats /etc/nats
+echo "listen: 127.0.0.1:4222" > /etc/nats/nats.conf
+chown nats:nats /etc/nats/nats.conf
+check_command systemctl restart nats-server
+check_command systemctl enable nats-server
+
+# Janus WebRTC Server
+## Installation
+. /etc/lsb-release
+curl -sL -o "/etc/apt/trusted.gpg.d/morph027-janus.asc" "https://packaging.gitlab.io/janus/gpg.key"
+echo "deb [arch=amd64] https://packaging.gitlab.io/janus/$DISTRIB_CODENAME $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/morph027-janus.list
+apt update -q4 & spinner_loading
+install_if_not janus
+## Configuration
+sed -i "s|#turn_rest_api_key\s*=.*|$JANUS_API_KEY|" /etc/janus/janus.jcfg
+sed -i "s|#full_trickle|full_trickle|g" /etc/janus/janus.jcfg
+sed -i 's|#interface.*|interface = "lo"|g' /etc/janus/janus.transport.websockets.jcfg
+sed -i 's|#ws_interface.*|ws_interface = "lo"|g' /etc/janus/janus.transport.websockets.jcfg
 check_command systemctl restart janus
 check_command systemctl enable janus
+
+# HPB
+## Installation
+curl -sL -o "/etc/apt/trusted.gpg.d/morph027-nextcloud-spreed-signaling.asc" "https://packaging.gitlab.io/nextcloud-spreed-signaling/gpg.key"
+echo "deb [arch=amd64] https://packaging.gitlab.io/nextcloud-spreed-signaling signaling main" > /etc/apt/sources.list.d/morph027-nextcloud-spreed-signaling.list
+apt update -q4 & spinner_loading
+install_if_not nextcloud-spreed-signaling
+## Configuration
+if [ ! -f "$SIGNALING_SERVER_CONF" ];
+then
+    cat << SIGNALING_CONF_CREATE > "$SIGNALING_SERVER_CONF"
+[http]
+listen = 127.0.0.1:8081
+[app]
+debug = false
+[sessions]
+hashkey = $(openssl rand -hex 16)
+blockkey = $(openssl rand -hex 16)
+[clients]
+internalsecret = $(openssl rand -hex 16)
+[backend]
+allowed = ${TURN_DOMAIN}
+allowall = false
+secret = ${NC_SECRET}
+timeout = 10
+connectionsperhost = 8
+[nats]
+url = nats://localhost:4222
+[mcu]
+type = janus
+url = ws://127.0.0.1:8188
+[turn]
+apikey = ${JANUS_API_KEY}
+secret = ${TURN_SECRET}
+servers = turn:$TURN_DOMAIN:$TURN_PORT?transport=tcp
+SIGNALING_CONF_CREATE
+fi
+check_command systemctl restart signaling
+check_command systemctl enable signaling
 
 ### PROXY ###
 # https://github.com/strukturag/nextcloud-spreed-signaling#apache
@@ -306,21 +332,18 @@ then
     RequestHeader set X-Forwarded-Proto "https"
     # basic proxy settings
     # Enable proxying Websocket requests to the standalone signaling server.
-    ProxyPass "/standalone-signaling/"  "ws://127.0.0.1:8080/"
+    ProxyPass "/"  "ws://127.0.0.1:8081/"
     RewriteEngine On
     # Websocket connections from the clients.
-    RewriteRule ^/standalone-signaling/spreed$ - [L]
+    RewriteRule ^/spreed$ - [L]
     # Backend connections from Nextcloud.
-    RewriteRule ^/standalone-signaling/api/(.*) http://127.0.0.1:8080/api/$1 [L,P]
+    RewriteRule ^/api/(.*) http://127.0.0.1:8081/api/$1 [L,P]
     # Extra (remote) headers
     RequestHeader set X-Real-IP %{REMOTE_ADDR}s
     Header set X-XSS-Protection "1; mode=block"
     Header set Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
     Header set X-Content-Type-Options nosniff
     Header set Content-Security-Policy "frame-ancestors 'self'"
-    <Location />
-        ProxyPassReverse /
-    </Location>
 </VirtualHost>
 HTTPS_CREATE
 
@@ -355,73 +378,3 @@ else
     last_fail_tls "$SCRIPTS"/apps/talk_signaling.sh
     exit 1
 fi
-
-# Add prune command
-add_dockerprune
-
-# Configuration
-## Janus WebRTC Server
-sed -i "s|turn_rest_api_key|$TURN_SECRET|g" /etc/janus/janus.jcfg
-sed -i "s|#full_trickle|full_trickle|g" /etc/janus/janus.jcfg
-sed -i 's|#interface.*|interface = "lo"|g' /etc/janus/janus.transport.websockets.jcfg
-sed -i 's|#ws_interface.*|ws_interface = "lo"|g' /etc/janus/janus.transport.websockets.jcfg
-check_command systemctl restart janus
-
-nc_secret="$(openssl rand -hex 16)"
-janus_api_key="$(openssl rand -base64 16)"
-
-if [ ! -f "$SIGNALING_SERVER_CONF" ];
-then
-    cat << SIGNALING_CONF_CREATE > "$SIGNALING_SERVER_CONF"
-[http]
-listen = 127.0.0.1:8081
-[app]
-debug = false
-[sessions]
-hashkey = $(openssl rand -hex 16)
-blockkey = $(openssl rand -hex 16)
-[clients]
-internalsecret = $(openssl rand -hex 16)
-[backend]
-allowed = ${TURN_DOMAIN}
-allowall = false
-secret = ${NC_SECRET}
-timeout = 10
-connectionsperhost = 8
-[nats]
-url = nats://localhost:4222
-[mcu]
-type = janus
-url = ws://127.0.0.1:8188
-[turn]
-apikey = ${JANUS_API_KEY}
-secret = ${TURN_SECRET}
-# do we know about the domain and the endpoint in some variable?
-# looks like: turn:example.com:3478?transport=tcp
-servers = ${TURN_SERVER}
-SIGNALING_CONF_CREATE
-fi
-
-sed -i 's,#turn_rest_api_key\s*=.*,turn_rest_api_key = "'"${JANUS_API_KEY}"'",' /etc/janus/janus.jcfg.dpkg-dist
-systemctl restart janus
-# We could just add this in there instead:
-msg_box "Please enter nc secret into your Talk settings: ${NC_SECRET}"
-
-
-
-## NATS server
-mkdir -p /etc/nats
-sudo install -d -o nats -g nats /etc/nats
-sudo -u nats echo "listen: 127.0.0.1:4222" | sudo tee -a /etc/nats/nats.conf
-install_if_not nats-server
-start_if_stopped nats-server
-check_command systemctl enable nats-server
-
-## nextcloud-spreed-signaling server (HPB)
-# TODO: create keys, setup config for janus and hpb (get turn server url from coturn app)
-
-# Install with apt: https://morph027.gitlab.io/blog/nextcloud-spreed-signaling/
-install_if_not nextcloud-spreed-signaling-proxy
-install_if_not nextcloud-spreed-signaling
-cp -f /usr/share/signaling/server.conf /etc/signaling/server.conf
-cp -f /usr/share/signaling/proxy.conf /etc/signaling/proxy.conf
