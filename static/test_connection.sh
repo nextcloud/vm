8103440e019add9308293b0a8d09ea9cff8a0483
Daniel Hansson <github@hanssonit.se>
2020-09-01 20:06:17 +0200

    Update test_connection.sh

--- static/test_connection.sh
+++ static/test_connection.sh
@@ -1,1 +1,14 @@
-# just here for backwards compatibility
+#!/bin/bash
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# This is needed since we removed this from the startup script, or changed name so it can't be downloaded anymore
+msg_box "You are running an outdated release.
+
+You see this message only to make it possible to run the first startup script, but as time goes, more and more will we incompatible.
+
+We urge you to download the latest version as soon as possible: https://github.com/nextcloud/vm/releases"
+
+exit
