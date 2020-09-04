6a14b50dca56496e5bfcceaeaec82b945f9630d7
szaimen <szaimen@e.mail.de>
2020-09-04 18:17:37 +0200

    add the defaultno function to places where it makes sense

--- lib.sh
+++ lib.sh
@@ -320,7 +320,7 @@ msg_box "As you noticed your WAN IP and DNS record doesn't match. This can happe
 If you feel brave, or are sure that everything is setup correctly, then you can choose to skip this test in the next step.
 
 You can always contact us for further support if you wish: https://shop.hanssonit.se/product/premium-support-per-30-minutes/"
-        if ! yesno_box "Do you feel brave and want to continue?"
+        if ! yesno_box_defaultno "Do you feel brave and want to continue?"
             then
             exit
         fi
@@ -652,7 +652,7 @@ If you are 100% sure the port ${1} is open you can now choose to
 continue. There are no guarantees that it will work anyway though,
 since the service depend on that the port ${1} is open and
 accessible from outside your network."
-    if ! yesno_box "Are you 100% sure the port ${1} is open?"
+    if ! yesno_box_defaultno "Are you 100% sure the port ${1} is open?"
     then
         msg_box "Port $1 is not open on either ${WANIP4} or ${2}.\n\nPlease follow this guide to open ports in your router or firewall:\nhttps://www.techandme.se/open-port-80-443/"
         any_key "Press any key to exit..."
