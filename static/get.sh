41fe2df94707b339c39bf3b8a9693f4449e3cc20
szaimen <szaimen@e.mail.de>
2020-09-07 00:51:17 +0200

    fix travis

--- static/get.sh
+++ static/get.sh
@@ -4,7 +4,7 @@ true
 # see https://github.com/koalaman/shellcheck/wiki/Directive
 
 # Cache the lib for half an hour
-if ! [ -f /var/scripts/lib.sh ] || test "`find /var/scripts/lib.sh -mmin +30`"
+if ! [ -f /var/scripts/lib.sh ] || test "$(find /var/scripts/lib.sh -mmin +30)"
 then
     rm -f /var/scripts/lib.sh
     curl -so /var/scripts/lib.sh https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh
