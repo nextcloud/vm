77fcfb3a380e8f2ed085eeae87a73240ddbe8711
Daniel Hansson <github@hanssonit.se>
2020-09-10 00:48:50 +0200

    better?

--- static/fetch_lib.sh
+++ static/fetch_lib.sh
@@ -3,8 +3,6 @@
 true
 # see https://github.com/koalaman/shellcheck/wiki/Directive
 
-
-
 # Functions
 msg_box() {
     [ -n "$2" ] && local SUBTITLE=" - $2"
@@ -20,26 +18,17 @@ print_text_in_color() {
         printf "%b%s%b\n" "$1" "$2" "$Color_Off"
 }
 
-lib_error_message() {
-    msg_box "You don't seem to have an internet connection and the local lib isn't available. Hence you cannot run this script."
-    exit 1
-}
-
-get_lib_curl() {
-    mkdir -p /var/scripts
-    curl -sfL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh -o /var/scripts/lib.sh
-}
-
-# Get the variables and functions from the actual lib file
-if ping github.com -c 2 >/dev/null 2>&1
+mkdir -p /var/scripts
+if ! [ -f /var/scripts/lib.sh ]
 then
-    print_text_in_color "$ICyan" "Updating lib..."
-    get_lib_curl
-else
-    if ! [ -f /var/scripts/lib.sh ]
+    if ! curl -sfL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh -o /var/scripts/lib.sh
     then
-        lib_error_message
+        msg_box "You don't seem to have an internet connection and the local lib isn't available. Hence you cannot run this script."
+        exit 1
     fi
+else
+    print_text_in_color "$ICyan" "Updating lib..."
+    curl -sfL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh -o /var/scripts/lib.sh
 fi
 
 # shellcheck source=lib.sh
