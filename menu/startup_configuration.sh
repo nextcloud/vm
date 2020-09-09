474d56363960d36d406b6bd0d05b9012c9727516
Daniel Hansson <github@hanssonit.se>
2020-09-09 20:10:40 +0200

    move comments

--- menu/startup_configuration.sh
+++ menu/startup_configuration.sh
@@ -59,9 +59,11 @@ case "$choice" in
             sleep 1
             clear
         else
+            # Change layout
             dpkg-reconfigure keyboard-configuration
             setupcon --force
-            run_script ADDONS locales # Set locales
+            # Set locales
+            run_script ADDONS locales
             input_box "Please try out all buttons (e.g: @ # \$ : y n) to find out if the keyboard settings were correctly applied.\nIf the keyboard is still wrong, you will be offered to reboot the server in the next step.\n\nPlease continue by hitting [ENTER]" >/dev/null
             if ! yesno_box_yes "Did the keyboard work as expected??\n\nIf you choose 'No' the server will be rebooted. After the reboot, please login as usual and run this script again."
             then
