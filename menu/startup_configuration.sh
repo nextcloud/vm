728b20423d27ccdfb52cc374ac3768c99d72a4a9
szaimen <szaimen@e.mail.de>
2020-09-09 22:04:46 +0200

    readd some subtitles that were mistakenly removed

--- menu/startup_configuration.sh
+++ menu/startup_configuration.sh
@@ -64,8 +64,8 @@ case "$choice" in
             setupcon --force
             # Set locales
             run_script ADDONS locales
-            input_box "Please try out all buttons (e.g: @ # \$ : y n) to find out if the keyboard settings were correctly applied.\nIf the keyboard is still wrong, you will be offered to reboot the server in the next step.\n\nPlease continue by hitting [ENTER]" >/dev/null
-            if ! yesno_box_yes "Did the keyboard work as expected??\n\nIf you choose 'No' the server will be rebooted. After the reboot, please login as usual and run this script again."
+            input_box "Please try out all buttons (e.g: @ # \$ : y n) to find out if the keyboard settings were correctly applied.\nIf the keyboard is still wrong, you will be offered to reboot the server in the next step.\n\nPlease continue by hitting [ENTER]" "$SUBTITLE" >/dev/null
+            if ! yesno_box_yes "Did the keyboard work as expected??\n\nIf you choose 'No' the server will be rebooted. After the reboot, please login as usual and run this script again." "$SUBTITLE"
             then
                 reboot
             fi
