3268a74b88fa77bf49b1fec1418ffb18463f31e8
Daniel Hansson <github@hanssonit.se>
2020-09-09 23:19:59 +0200

    revert to old way, but make it simpler
    we can improve later, but please make this small steps as it's such a big change.
    
    Tested and works like this:
    
    ```
    #!/bin/bash
    source /home/daniel/fetch_lib.sh
    
    echo "$VMLOGS"
    
    first_iface
    
    yesno_box_yes "Do you want to do this?"
    ```

--- static/fetch_lib.sh
+++ static/fetch_lib.sh
@@ -4,17 +4,42 @@ true
 # see https://github.com/koalaman/shellcheck/wiki/Directive
 
 
-mkdir -p /var/scripts
-if ! [ -f /var/scripts/lib.sh ]
+
+# Functions
+msg_box() {
+    [ -n "$2" ] && local SUBTITLE=" - $2"
+    whiptail --title "$TITLE$SUBTITLE" --msgbox "$1" "$WT_HEIGHT" "$WT_WIDTH"
+}
+
+
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+ICyan='\e[0;96m'        # Cyan
+Color_Off='\e[0m'       # Text Reset
+print_text_in_color() {
+        printf "%b%s%b\n" "$1" "$2" "$Color_Off"
+}
+
+lib_error_message() {
+    msg_box "You don't seem to have an internet connection and the local lib isn't available. Hence you cannot run this script."
+    exit 1
+}
+
+get_lib_curl() {
+    mkdir -p /var/scripts
+    curl -sfL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh -o /var/scripts/lib.sh
+}
+
+# Get the variables and functions from the actual lib file
+if ping github.com -c 2 >/dev/null 2>&1
 then
-    if ! curl -so /var/scripts/lib.sh https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh 
+    print_text_in_color "$ICyan" "Updating lib..."
+    get_lib_curl
+else
+    if ! [ -f /var/scripts/lib.sh ]
     then
-        printf "The local lib isn't available and you don't have internet access. You cannot run this script"
-        exit 1
+        lib_error_message
     fi
-elif test "$(find /var/scripts/lib.sh -mmin +30)"
-then
-    curl -so /var/scripts/lib.sh https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh
 fi
 
 # shellcheck source=lib.sh
