40f0348807ccf262dfd15311d2ffed011e588ad3
Daniel Hansson <github@hanssonit.se>
2020-09-12 07:55:30 +0200

    add menus for Talk and Fail2ban (#1443)
    Signed-off-by: enoch85 <github@hanssonit.se>

---
+++ menu/talk_menu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+SCRIPT_NAME="Talk Menu"
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+# Set the startup switch
+if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
+then
+    STARTUP_SWITCH="ON"
+else
+    STARTUP_SWITCH="OFF"
+fi
+
+choice=$(whiptail --title "$TITLE" --checklist "Automatically install and configure Talk.\n$CHECKLIST_GUIDE\n$MENU_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Talk " "(Install Talk standalone - no subdomain required)" OFF \
+"Talk-Signaling" "(Install Talk + Signaling Server - subdomain required)" OFF 3>&1 1>&2 2>&3)
+
+case "$choice" in
+    *"Talk "*)
+        clear
+        print_text_in_color "$ICyan" "Downloading the Talk script..."
+        run_script APP talk
+    ;;&
+    *"Talk-Signaling"*)
+        clear
+        print_text_in_color "$ICyan" "Downloading the Talk Signaling script..."
+        run_script APP talk_signaling
+    ;;&
+    *)
+    ;;
+esac
+exit
+
