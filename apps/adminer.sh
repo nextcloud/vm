a4b0c97c0262e579773cddcbe0cc63c035f0c368
Daniel Hansson <github@hanssonit.se>
2020-09-13 16:18:58 +0200

    fix adminer bad request and change port

--- apps/adminer.sh
+++ apps/adminer.sh
@@ -55,9 +55,14 @@ curl_to_dir "https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Admine
 ln -s "$ADMINERDIR"/latest.php "$ADMINERDIR"/adminer.php
 
 cat << ADMINER_CREATE > "$ADMINER_CONF"
-Listen 8443
+ <VirtualHost *:80>
+     RewriteEngine On
+     RewriteRule ^(.*)$ https://%{HTTP_HOST}$1:9443 [R=301,L]
+ </VirtualHost>
 
-<VirtualHost *:8443>
+Listen 9443
+
+<VirtualHost *:9443>
     Header add Strict-Transport-Security: "max-age=15768000;includeSubdomains"
     SSLEngine on
     
@@ -99,7 +104,7 @@ The script will exit."
     exit 1
 else
 msg_box "Adminer was sucessfully installed and can be reached here:
-https://$ADDRESS:8443
+https://$ADDRESS:9443
 
 You can download more plugins and get more information here: 
 https://www.adminer.org
