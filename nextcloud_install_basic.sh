121be84b71c80c837d16f22342d75b7eda128446
Daniel Hansson <github@hanssonit.se>
2020-09-16 15:04:51 +0200

    small detail

--- nextcloud_install_basic.sh
+++ nextcloud_install_basic.sh
@@ -573,7 +573,7 @@ download_script STATIC instruction
 download_script STATIC change_db_pass
 download_script STATIC history
 download_script STATIC welcome
-chown $UNIXUSER:UNIXUSER welcome
+chown $UNIXUSER:$UNIXUSER welcome
 download_script NETWORK trusted
 download_script MENU startup_configuration
 
