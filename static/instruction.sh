0a85a3bd1761c25245f281cb9358581cf5b60cc4
Daniel Hansson <github@hanssonit.se>
2020-09-16 14:41:58 +0200

    Update instruction.sh

--- static/instruction.sh
+++ static/instruction.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
@@ -16,22 +17,17 @@ echo -e "|"  "${IGreen}To run the startup script type the sudoer password, then 
 echo -e "|"  "${IGreen}The default sudoer password is: ${BIGreen}nextcloud${IGreen}${Color_Off}                             |"
 cat << INST2
 |                                                                       |
-| You can find the complete install instructions here:                  |
-| Nextcloud VM              = https://bit.ly/2S8eGfS                    |
-| Nextcloud Home/SME Server = https://bit.ly/2k2TNaM                    |
-|                                                                       |
 | To be 100% sure that all the keystrokes work correctly (like @),      |
 | please use an SSH terminal like Putty. You can download it here:      |
 | https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html        |
+|                                                                       |
 | If you are running Windows 10 1809 or later, you can simply use SSH   |
 | from the command prompt. Connect like this:                           |
 | ssh ncadmin@local.IP.of.this.server                                   |
 |                                                                       |
-| This server could be made maintenace free by using automatic updates  |
-| with the built in update script. If you want automatic updates on     |
-| a weekly schelude, choose to configure it later during this setup.    |
+| To choose the defaults during installation, just hit [ENTER].         |
 |                                                                       |
-|  ###################### T&M Hansson IT - $(date +"%Y") ######################  |
+|     ###################### Nextcloud - $(date +"%Y") #####################     |
 +-----------------------------------------------------------------------+
 INST2
 
