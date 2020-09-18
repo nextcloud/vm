910d25cb1da9bb56c779f5c0ca1d3b1f0e25b754
szaimen <szaimen@e.mail.de>
2020-09-18 11:36:29 +0200

    update the lib also if  internet is working

--- static/fetch_lib.sh
+++ static/fetch_lib.sh
@@ -26,7 +26,7 @@ then
         print_text_in_color "$IRed" "You don't seem to have an internet connection and the local lib isn't available. Hence you cannot run this script."
         exit 1
     fi
-elif ! [ -f /var/scripts/nextcloud-startup-script.sh ]
+elif ! [ -f /var/scripts/nextcloud-startup-script.sh ] || [ -f /var/scripts/you-can-not-run-the-startup-script-several-times ]
 then
     print_text_in_color "$ICyan" "Updating lib..."
     curl -sfL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh -o /var/scripts/lib.sh
