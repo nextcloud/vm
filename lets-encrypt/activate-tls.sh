a116de1aacf6c3e9652df9de32b4cd63ec64d35f
Daniel Hansson <github@hanssonit.se>
2020-09-18 12:13:27 +0200

    another typo

--- lets-encrypt/activate-tls.sh
+++ lets-encrypt/activate-tls.sh
@@ -23,7 +23,7 @@ msg_box "Before we begin the installation of your TLS certificate you need to:
 1. Have a domain like: cloud.example.com
 If you want to get a domain at a fair price, please check this out: https://store.binero.se/?lang=en-US
 
-2. Open port 80 and 443 against this VMs IP address: $ADDRESS.
+2. Open port 80 and 443 against this servers IP address: $ADDRESS.
 Here is a guide: https://goo.gl/Uyuf65
 It's also possible to automatically open ports with UPNP, if you have that enabled in your firewall/router.
 
