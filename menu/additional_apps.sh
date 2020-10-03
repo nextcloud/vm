8aef5de360906766a567933420bdca1707109cbf
szaimen <szaimen@e.mail.de>
2020-10-03 20:27:25 +0200

    remove it from additional_apps

--- menu/additional_apps.sh
+++ menu/additional_apps.sh
@@ -34,7 +34,6 @@ $CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
 "Fail2ban  " "(Extra Bruteforce protection)" "$STARTUP_SWITCH" \
 "Adminer" "(PostgreSQL GUI)" OFF \
 "Netdata" "(Real-time server monitoring in Web GUI)" OFF \
-"ClamAV" "(Antivirus for Nextcloud and files)" OFF \
 "BPYTOP" "(Real-time server monitoring in CLI)" OFF \
 "FullTextSearch" "(Elasticsearch for Nextcloud [2GB RAM])" OFF \
 "PreviewGenerator" "(Pre-generate previews)" "$STARTUP_SWITCH" \
@@ -68,11 +67,6 @@ case "$choice" in
         clear
         print_text_in_color "$ICyan" "Downloading the Netdata script..."
         run_script APP netdata
-    ;;&
-    *"ClamAV"*)
-        clear
-        print_text_in_color "$ICyan" "Downloading the ClamAV script..."
-        run_script APP clamav
     ;;&
     *"BPYTOP"*)
         clear
