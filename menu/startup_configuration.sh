c5e1037ecaf30af6283b50ee137c201e88ccd02c
szaimen <szaimen@e.mail.de>
2020-10-04 14:44:31 +0200

    redo the change

--- menu/startup_configuration.sh
+++ menu/startup_configuration.sh
@@ -44,7 +44,7 @@ fi
 # Show a msg_box during the startup script
 if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
 then
-msg_box "Running a server, it's important that certain things are correct.
+    msg_box "Running a server, it's important that certain things are correct.
 In the following menu you will be asked to setup the most basic stuff of your server.
 
 The script is smart, and have already pre-selected the values that you'd want to change based on the current settings."
