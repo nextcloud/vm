1d8127bb693359c57c82263419260ec1d60b4a69
szaimen <szaimen@e.mail.de>
2020-10-11 03:02:02 +0200

    allow to restrict by continent

--- network/geoblock.sh
+++ network/geoblock.sh
@@ -55,48 +55,107 @@ install_if_not libapache2-mod-geoip
 check_command a2enmod geoip rewrite
 check_command systemctl restart apache2
 
-# Get country names
-COUNTRY_NAMES=$(jq .[][].name /usr/share/iso-codes/json/iso_3166-1.json | sed 's|^"||;s|"$||')
-mapfile -t COUNTRY_NAMES <<< "$COUNTRY_NAMES"
-
-# Get country codes
-COUNTRY_CODES=$(jq .[][].alpha_2 /usr/share/iso-codes/json/iso_3166-1.json | sed 's|^"||;s|"$||')
-mapfile -t COUNTRY_CODES <<< "$COUNTRY_CODES"
-
-# Check if both arrays match
-if [ "${#COUNTRY_NAMES[@]}" != "${#COUNTRY_CODES[@]}" ]
-then
-    msg_box "Somethings is wrong. The names length is not equal to the codees length.
-Please report this to $ISSUES"
-fi
-
-# Create checklist
-args=(whiptail --title "$TITLE - $SUBTITLE" --separate-output --checklist \
-"Please select all countries that shall have access to your websites.
-All countries that are not selected will not have access to your websites.
-$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4)
-count=0
-while [ "$count" -lt "${#COUNTRY_NAMES[@]}" ]
-do
-    args+=("${COUNTRY_CODES[$count]}" "${COUNTRY_NAMES[$count]}" OFF)
-    ((count++))
-done
-
-# Let the user choose the countries
-selected_options=$("${args[@]}" 3>&1 1>&2 2>&3)
-if [ -z "$selected_options" ]
+# Restrict to countries and/or continents
+choice=$(whiptail --title "$TITLE"  --checklist \
+"Do you want to restrict to countries and/or continents?
+$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Countries" "" ON \
+"Continents" "" ON 3>&1 1>&2 2>&3)
+if [ -z "$choice" ]
 then
     exit 1
 fi
-mapfile -t selected_options <<< "$selected_options"
+
+# Countries
+if [[ "$choice" = *"Countries"* ]]
+then
+    # Get country names
+    COUNTRY_NAMES=$(jq .[][].name /usr/share/iso-codes/json/iso_3166-1.json | sed 's|^"||;s|"$||')
+    mapfile -t COUNTRY_NAMES <<< "$COUNTRY_NAMES"
+
+    # Get country codes
+    COUNTRY_CODES=$(jq .[][].alpha_2 /usr/share/iso-codes/json/iso_3166-1.json | sed 's|^"||;s|"$||')
+    mapfile -t COUNTRY_CODES <<< "$COUNTRY_CODES"
+
+    # Check if both arrays match
+    if [ "${#COUNTRY_NAMES[@]}" != "${#COUNTRY_CODES[@]}" ]
+    then
+        msg_box "Somethings is wrong. The names length is not equal to the codees length.
+    Please report this to $ISSUES"
+    fi
+
+    # Create checklist
+    args=(whiptail --title "$TITLE - $SUBTITLE" --separate-output --checklist \
+"Please select all countries that shall have access to your websites.
+All countries that are not selected will not have access to your websites \
+if you not choose to activate their continent.
+$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4)
+    count=0
+    while [ "$count" -lt "${#COUNTRY_NAMES[@]}" ]
+    do
+        args+=("${COUNTRY_CODES[$count]}" "${COUNTRY_NAMES[$count]}" OFF)
+        ((count++))
+    done
+
+    # Let the user choose the countries
+    selected_options=$("${args[@]}" 3>&1 1>&2 2>&3)
+    if [ -z "$selected_options" ]
+    then
+        unset selected_options
+    fi
+fi
+
+# Continents
+if [[ "$choice" = *"Continents"* ]]
+then
+    # Restrict to continents
+    choice=$(whiptail --title "$TITLE" --separate-output --checklist \
+"Please choose all continents that shall have access to your websites.
+All countries on not selected continents will not have access to your websites \
+if you haven't explicitely chosen them in the countries menu before.
+$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"AF" "Africa" OFF \
+"AN" "Antarctica" OFF \
+"AS" "Asia" OFF \
+"EU" "Europe" OFF \
+"NA" "North America" OFF \
+"OC" "Oceania" OFF \
+"SA" "South America" OFF 3>&1 1>&2 2>&3)
+    if [ -z "$choice" ]
+    then
+        unset choice
+    fi
+else
+    unset choice
+fi
+
+# Exit if nothing chosen
+if [ -z "$selected_options" ] && [ -z "$choice" ]
+then
+    exit 1
+fi
+
+# Convert to array
+if [ -n "$selected_options" ]
+then
+    mapfile -t selected_options <<< "$selected_options"
+fi
+if [ -n "$choice" ]
+then
+    mapfile -t choice <<< "$choice"
+fi
 
 GEOIP_CONF="#Geoip-block-start - Please don't remove or change this line
 <Location />\n"
+for continent in "${choice[@]}"
+do
+    GEOIP_CONF+="    SetEnvIf GEOIP_CONTINENT_CODE $continent AllowCountryOrContinent\n"
+done
 for country in "${selected_options[@]}"
 do
-    GEOIP_CONF+="    SetEnvIf GEOIP_COUNTRY_CODE $country AllowCountry\n"
+    GEOIP_CONF+="    SetEnvIf GEOIP_COUNTRY_CODE $country AllowCountryOrContinent\n"
 done
-GEOIP_CONF+="    Allow from env=AllowCountry
+GEOIP_CONF+="    Allow from env=AllowCountryOrContinent
     Allow from 127.0.0.1/8
     Allow from 192.168.0.0/16
     Allow from 172.16.0.0/12
