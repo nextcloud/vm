5fd85770c4f36eea9b99937876a2bd5ff86b7f10
Daniel Hansson <github@hanssonit.se>
2020-10-19 00:38:27 +0200

    wording

--- apps/fail2ban.sh
+++ apps/fail2ban.sh
@@ -7,7 +7,7 @@
 true
 SCRIPT_NAME="Fail2ban"
 SCRIPT_EXPLAINER="Fail2ban provides extra Brute Force protextion for Nextcloud.
-It scans Nextcloud's log files and bans IPs that show the malicious \
+It scans Nextcloud's and SSH log files and bans IPs that show malicious \
 signs -- too many password failures, seeking for exploits, etc. 
 Generally Fail2Ban is then used to update firewall rules to \
 reject the IP addresses for a specified amount of time."
