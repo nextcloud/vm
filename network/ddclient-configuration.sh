064e3f1b0f10e34c335376403e71a26bfc17252a
Daniel Hansson <github@hanssonit.se>
2020-10-19 20:19:29 +0200

    change name

--- network/ddclient-configuration.sh
+++ network/ddclient-configuration.sh
@@ -5,7 +5,7 @@
 
 # shellcheck disable=2034,2059
 true
-SCRIPT_NAME="Setup DDclient"
+SCRIPT_NAME="DynDNS with ddclient"
 SCRIPT_EXPLAINER="This script lets you setup DynDNS by using the Linux ddclient software."
 # shellcheck source=lib.sh
 source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
