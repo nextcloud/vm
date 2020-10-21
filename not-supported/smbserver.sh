97cccb9132464cf7a8653b56acac8f2b42c07cf6
szaimen <szaimen@e.mail.de>
2020-10-21 13:24:51 +0200

    fix detail

--- not-supported/smbserver.sh
+++ not-supported/smbserver.sh
@@ -685,13 +685,13 @@ create_share() {
         then
             # Correct the ACL
             chmod -R 770 "$NEWPATH"
-            if [ "$(stat -c %a $NEWPATH)" != "770" ]
+            if [ "$(stat -c %a '$NEWPATH')" != "770" ]
             then
                 msg_box "Something went wrong. Couln't set the correct mod permissions for the location."
                 return 1
             fi
             chown -R "$WEB_USER":"$WEB_GROUP" "$NEWPATH"
-            if [ "$(stat -c %G $NEWPATH)" != "$WEB_GROUP" ] || [ "$(stat -c %U $NEWPATH)" != "$WEB_USER" ]
+            if [ "$(stat -c %G '$NEWPATH')" != "$WEB_GROUP" ] || [ "$(stat -c %U $NEWPATH)" != "$WEB_USER" ]
             then
                 msg_box "Something went wrong. Couln't set the correct own permissions for the location."
                 return 1
