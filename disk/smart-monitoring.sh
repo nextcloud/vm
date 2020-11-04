497b92f663614dc813d139facf2385f0f0fe4ed5
szaimen <szaimen@e.mail.de>
2020-11-04 15:06:45 +0100

    fix detail

--- disk/smart-monitoring.sh
+++ disk/smart-monitoring.sh
@@ -78,17 +78,17 @@ source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercont
 root_check
 if home_sme_server
 then
-    notify_admin_gui "S.M.A.R.T results weekly scan (nvme0n1)" "$(smartctl --all /dev/nvme0n1)"
-    notify_admin_gui "S.M.A.R.T results weekly scan (sda)" "$(smartctl --all /dev/sda)"
+    notify_admin_gui "S.M.A.R.T results weekly scan (nvme0n1)" "\$(smartctl --all /dev/nvme0n1)"
+    notify_admin_gui "S.M.A.R.T results weekly scan (sda)" "\$(smartctl --all /dev/sda)"
 else
     # get all disks into an array
-    disks="$(fdisk -l | grep Disk | grep /dev/sd | awk '{print$2}' | cut -d ":" -f1)"
+    disks="\$(fdisk -l | grep Disk | grep /dev/sd | awk '{print\$2}' | cut -d ":" -f1)"
     # loop over disks in array
-    for disk in $(printf "${disks[@]}")
+    for disk in \$(printf "\${disks[@]}")
     do
-        if [ -n "$disks" ]
+        if [ -n "\$disks" ]
         then
-             notify_admin_gui "S.M.A.R.T results weekly scan ($disk)" "$(smartctl --all $disk)"
+             notify_admin_gui "S.M.A.R.T results weekly scan (\$disk)" "\$(smartctl --all \$disk)"
         fi
     done
 fi
