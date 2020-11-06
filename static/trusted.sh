e4a6fcdcc1814760c4e6b5ef51f8ff610a361a4b
Daniel Hansson <github@hanssonit.se>
2020-11-06 17:15:45 +0100

    temporary fix (remove this script in a few versions)

---
+++ static/trusted.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+# shellcheck disable=2034,2059
+true
+SCRIPT_NAME="Set trusted domain"
+# shellcheck source=lib.sh
+source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# This is needed since we removed this from the startup script, or changed name so it can't be downloaded anymore
+msg_box "You are running an outdated release.
+
+You see this message only to make it possible to run the first startup script, but as time goes, more and more will we incompatible.
+We urge you to download the latest version as soon as possible: https://github.com/nextcloud/vm/releases"
