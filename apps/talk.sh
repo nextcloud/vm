f79c78677e866529f5cbab35a7ddf583f136aa7c
Daniel Hansson <github@hanssonit.se>
2020-11-12 16:16:24 +0100

    Update talk.sh

--- apps/talk.sh
+++ apps/talk.sh
@@ -259,7 +259,7 @@ check_command systemctl enable nats-server
 
 # Janus WebRTC Server
 ## Installation
-. /etc/lsb-release
+source /etc/lsb-release
 curl -sL -o "/etc/apt/trusted.gpg.d/morph027-janus.asc" "https://packaging.gitlab.io/janus/gpg.key"
 echo "deb [arch=amd64] https://packaging.gitlab.io/janus/$DISTRIB_CODENAME $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/morph027-janus.list
 apt update -q4 & spinner_loading
