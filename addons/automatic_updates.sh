b26478cbfc9c31992b40a65a7d8de8a3a0c79884
szaimen <szaimen@e.mail.de>
2020-11-18 22:49:10 +0100

    Address review
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- addons/automatic_updates.sh
+++ addons/automatic_updates.sh
@@ -4,13 +4,13 @@
 
 true
 SCRIPT_NAME="Automatic Updates"
+# shellcheck source=lib.sh
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 SCRIPT_EXPLAINER="This option will update your server every week on Saturdays at $AUT_UPDATES_TIME:00.
 The update will run the built in script '$SCRIPTS/update.sh' which will update both the server packages and Nextcloud itself.\n
 You can read more about it here: https://www.techandme.se/nextcloud-update-is-now-fully-automated/
 Please keep in mind that automatic updates might fail hence it's \
 important to have a proper backup in place if you plan to run this option."
-# shellcheck source=lib.sh
-source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
