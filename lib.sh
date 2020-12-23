db7ad08c51b323a6701442cbf1e9a873508f538b
szaimen <szaimen@e.mail.de>
2020-12-23 14:50:59 +0100

    fix not showing the repo address in some edge cases
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- lib.sh
+++ lib.sh
@@ -39,7 +39,7 @@ SYSVENDOR=$(cat /sys/devices/virtual/dmi/id/sys_vendor)
 # Network
 IFACE=$(ip r | grep "default via" | awk '{print $5}')
 IFACE2=$(ip -o link show | awk '{print $2,$9}' | grep 'UP' | cut -d ':' -f 1)
-REPO=$(grep deb-src /etc/apt/sources.list | grep http | awk '{print $3}' | head -1)
+REPO=$(grep "^deb " /etc/apt/sources.list | grep http | awk '{print $2}' | head -1)
 ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 # WANIP4=$(dig +short myip.opendns.com @resolver1.opendns.com) # as an alternative
 WANIP4=$(curl -s -k -m 5 https://ipv4bot.whatismyipaddress.com)
