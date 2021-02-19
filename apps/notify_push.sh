9fbc347669679d31e8827a65a3437203e854fab5
szaimen <szaimen@e.mail.de>
2021-02-20 00:58:36 +0100

    uncomment the todo lines for now again
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- apps/notify_push.sh
+++ apps/notify_push.sh
@@ -93,10 +93,10 @@ else
 fi
 
 # Workaround as long as the app isn't installable via the appstore
-# rm -f "$SCRIPTS/notify_push.tar.gz" # TODO: remove me
-#wget https://github.com/nextcloud/notify_push/releases/download/v0.1.0-beta1/notify_push.tar.gz -P "$SCRIPTS" # TODO: remove me
-#tar -xvzf "$SCRIPTS/notify_push.tar.gz" -C "$NC_APPS_PATH" # TODO: remove me
-#chown -R www-data:www-data "$NC_APPS_PATH/notify_push" # TODO: remove me
+rm -f "$SCRIPTS/notify_push.tar.gz" # TODO: remove me
+wget https://github.com/nextcloud/notify_push/releases/download/v0.1.0-beta1/notify_push.tar.gz -P "$SCRIPTS" # TODO: remove me
+tar -xvzf "$SCRIPTS/notify_push.tar.gz" -C "$NC_APPS_PATH" # TODO: remove me
+chown -R www-data:www-data "$NC_APPS_PATH/notify_push" # TODO: remove me
 
 # Install the app
 install_and_enable_app notify_push
