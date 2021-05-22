5c46ef354751a9e0962e5aa697c2d4978ae6daa3
szaimen <szaimen@e.mail.de>
2021-05-23 00:11:24 +0200

    update mimetype list
    Sometimes the mimetype list gets updated and as far as I know doesn't Nextcloud get those automatically so they need to be refreshed manually.
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -993,6 +993,9 @@ Thank you for using T&M Hansson IT's updater!"
     echo "NEXTCLOUD UPDATE success-$(date +"%Y%m%d")" >> "$VMLOGS"/updates/update.log
     # Remove logs from last year to save space
     rm -f "$VMLOGS"/updates/update-"$(date --date='1 year ago' +%Y)"*
+    # Update mimtype list
+    nextcloud_occ maintenance:mimetype:update-js
+    nextcloud_occ maintenance:mimetype:update-db
     if [ -n "$SNAPSHOT_EXISTS" ]
     then
         check_command lvrename /dev/ubuntu-vg/NcVM-snapshot-pending /dev/ubuntu-vg/NcVM-snapshot
