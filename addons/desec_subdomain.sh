d3154c676513e8368f14cd1a4000036a0b00ecd7
Daniel Hansson <github@hanssonit.se>
2021-05-24 02:27:00 +0200

    intendent

--- addons/desec_subdomain.sh
+++ addons/desec_subdomain.sh
@@ -143,7 +143,7 @@ then
     if generate_desec_cert "$FINAL_SUBDOMAIN"
     then
         msg_box "Congrats! You should now be able to use $FINAL_SUBDOMAIN for setting up Talk, Collabora, OnlyOffice and other apps in Nextcloud.
-    
+
 Please remember to add the port number to the domain, if you chose a custom one, like this: $FINAL_SUBDOMAIN:portnumber"
     fi
 fi
