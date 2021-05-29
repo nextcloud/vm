69e0f5f398ca3b8eb785c8895d97cec23bc2494d
szaimen <szaimen@e.mail.de>
2021-05-29 22:55:32 +0200

    Make it work
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- apps/onlyoffice_integrated.sh
+++ apps/onlyoffice_integrated.sh
@@ -61,6 +61,64 @@ remove_from_trusted_domains() {
         fi
     done
 }
+remove_collabora_docker() {
+    # Check if Collabora is previously installed
+    # If yes, then stop and prune the docker container
+    docker_prune_this 'collabora/code'
+    # Revoke LE
+    SUBDOMAIN=$(input_box_flow "Please enter the subdomain you are using for Collabora, e.g: office.yourdomain.com")
+    if [ -f "$CERTFILES/$SUBDOMAIN/cert.pem" ]
+    then
+        yes no | certbot revoke --cert-path "$CERTFILES/$SUBDOMAIN/cert.pem"
+        REMOVE_OLD="$(find "$LETSENCRYPTPATH/" -name "$SUBDOMAIN*")"
+        for remove in $REMOVE_OLD
+            do rm -rf "$remove"
+        done
+    fi
+    # Remove Apache2 config
+    if [ -f "$SITES_AVAILABLE/$SUBDOMAIN.conf" ]
+    then
+        a2dissite "$SUBDOMAIN".conf
+        restart_webserver
+        rm -f "$SITES_AVAILABLE/$SUBDOMAIN.conf"
+    fi
+    # Disable RichDocuments (Collabora App) if activated
+    if is_app_installed richdocuments
+    then
+        nextcloud_occ app:remove richdocuments
+    fi
+    # Remove trusted domain
+    remove_from_trusted_domains "$SUBDOMAIN"
+}
+remove_onlyoffice_docker() {
+    # Check if Onlyoffice is previously installed
+    # If yes, then stop and prune the docker container
+    docker_prune_this 'onlyoffice/documentserver'
+    # Revoke LE
+    SUBDOMAIN=$(input_box_flow "Please enter the subdomain you are using for OnlyOffice, e.g: office.yourdomain.com")
+    if [ -f "$CERTFILES/$SUBDOMAIN/cert.pem" ]
+    then
+        yes no | certbot revoke --cert-path "$CERTFILES/$SUBDOMAIN/cert.pem"
+        REMOVE_OLD="$(find "$LETSENCRYPTPATH/" -name "$SUBDOMAIN*")"
+        for remove in $REMOVE_OLD
+            do rm -rf "$remove"
+        done
+    fi
+    # Remove Apache2 config
+    if [ -f "$SITES_AVAILABLE/$SUBDOMAIN.conf" ]
+    then
+        a2dissite "$SUBDOMAIN".conf
+        restart_webserver
+        rm -f "$SITES_AVAILABLE/$SUBDOMAIN.conf"
+    fi
+    # Disable onlyoffice if activated
+    if is_app_installed onlyoffice
+    then
+        nextcloud_occ app:remove onlyoffice
+    fi
+    # Remove trusted domain
+    remove_from_trusted_domains "$SUBDOMAIN"
+}
 
 # Check if Collabora is installed using the new method
 if ! is_app_installed documentserver_community
