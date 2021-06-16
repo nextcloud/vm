7f33ebd883702caccb1b83849d10b04b8068dfc1
szaimen <szaimen@e.mail.de>
2021-06-16 21:50:56 +0200

    Add Monitor Link shares script to the not-supported folder (#2025)
    Signed-off-by: szaimen <szaimen@e.mail.de>
    
    Co-authored-by: Daniel Hansson <github@hanssonit.se>

--- network/geoblock.sh
+++ network/geoblock.sh
@@ -32,10 +32,6 @@ else
     # Removal
     find /var/scripts -type f -regex \
 "$SCRIPTS/202[0-9]-[01][0-9]-Maxmind-Country-IPv[46]\.dat" -delete
-    if is_this_installed jq
-    then
-        apt purge jq -y
-    fi
     if is_this_installed libapache2-mod-geoip
     then
         a2dismod geoip
