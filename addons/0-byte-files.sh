a4380accc6b74cf08b5fc06fbc8b06d77b8617de
Daniel Hansson <github@hanssonit.se>
2021-06-17 10:59:10 +0200

    add some info

--- addons/0-byte-files.sh
+++ addons/0-byte-files.sh
@@ -11,14 +11,26 @@ cd "$NCDATA"
 
 emSub="Warning: Nextcloud contains 0-Byte files!"
 
+# Info
+msg_box "We will now scan $NCDATA for files that are 0-Byte.
+
+A 0-Byte file means that it's empty and probably corrupted/not usable. If you see files that are of importance in this list, you should report it immediately to $ISSUES.
+
+The scan may take very long time depending on the speed of your disks, and the amount of files."
+
+countdown "The scan starts in 3 seconds..." "3"
+
+print_text_in_color "$ICyan" "Scan in progress, please be patient..."
+
 # Function
 for fs in "${filesystems[@]}"
 do
-    while read -d '' -r
+    while IFS= read -d '' -r
     do
-        arr+=( "$REPLY\n" )
+        arr+=( "$REPLY" )
     done < <(find "$fs" -mindepth 3 -size 0 -print0)
 done
+
 
 # TODO
 # Remove known 0-byte files
