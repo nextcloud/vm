ec004d3c67de62578fad83b29f84f0e653924a0f
enoch85 <github@hanssonit.se>
2021-06-18 09:30:16 +0200

    add setting
    Signed-off-by: enoch85 <github@hanssonit.se>

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -5,6 +5,18 @@
 # Prefer IPv4 for apt
 echo 'Acquire::ForceIPv4 "true";' >> /etc/apt/apt.conf.d/99force-ipv4
 
+# Fix fancy progress bar for apt-get
+# https://askubuntu.com/a/754653
+if [ -d /etc/apt-get/apt-get.conf.d ]
+then
+    if [ -f /etc/apt-get/apt-get.conf.d/99progressbar ]
+    then
+        echo 'Dpkg::Progress-Fancy "1";' > /etc/apt-get/apt-get.conf.d/99progressbar
+        echo 'APT::Color "1";' > /etc/apt-get/apt-get.conf.d/99progressbar
+        chmod 644 /etc/apt-get/apt-get.conf.d/99progressbar
+    fi
+fi
+
 # Install curl if not existing
 if [ "$(dpkg-query -W -f='${Status}' "curl" 2>/dev/null | grep -c "ok installed")" == "1" ]
 then
