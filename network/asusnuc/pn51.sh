42e88c8f031eabbff398eda8981c3536ee2d7bac
Daniel Hansson <github@hanssonit.se>
2021-07-19 19:37:11 +0200

    use dkms instead

--- network/asusnuc/pn51.sh
+++ network/asusnuc/pn51.sh
@@ -2,6 +2,8 @@
 
 # T&M Hansson IT AB © - 2021, https://www.hanssonit.se/
 
+# Implements this way of doing it: https://askubuntu.com/a/1281319
+
 true
 SCRIPT_NAME="PN51 Network Drivers"
 SCRIPT_EXPLAINER="This installs the correct drivers for the 2.5GB LAN card in the PN51 ASUS"
@@ -24,6 +26,7 @@ then
 fi
 
 INSTALLDIR="$SCRIPTS/PN51"
+RVERSION="9.005.06"
 
 mkdir -p $INSTALLDIR
 
@@ -31,29 +34,40 @@ mkdir -p $INSTALLDIR
 install_if_not build-essential
 
 # Download and extract
-if [ ! -f $INSTALLDIR/r8125-9.005.06.tar.bz2 ]
+if [ ! -f $INSTALLDIR"/r8125-$RVERSION".tar.bz2 ]
 then
     curl_to_dir https://github.com/nextcloud/vm/raw/master/network/asusnuc r8125-9.005.06.tar.bz2 "$INSTALLDIR"
 fi
 
-if [ ! -d "$INSTALLDIR/r8125-9.005.06" ]
+if [ ! -d "$INSTALLDIR"/r8125-"$RVERSION" ]
 then
     check_command cd "$INSTALLDIR"
-    check_command tar -xf r8125-9.005.06.tar.bz2
+    check_command tar -xf r8125-"$RVERSION".tar.bz2
 fi
 
 # Install
-if [ -d "$INSTALLDIR/r8125-9.005.06" ]
+if [ -d "$INSTALLDIR"/r8125-"RVERSION" ]
 then
-    check_command cd "$INSTALLDIR/r8125-9.005.06"
-    bash autorun.sh
+    cat <<-DKMSCONFIG > "$INSTALLDIR"/src"
+PACKAGE_NAME="r8125"
+PACKAGE_VERSION="$RVERSION"
+BUILT_MODULE_NAME[0]="$PACKAGE_NAME"
+DEST_MODULE_LOCATION[0]="/updates/dkms"
+AUTOINSTALL="YES"
+REMAKE_INITRD="YES"
+CLEAN="rm src/@PKGNAME@.ko src/*.o || true"
+DKMSCONFIG
+    check_command cp -R "$INSTALLDIR"/src /usr/src/r8125-"$RVERSION"
+    check_command dkms add -m r8125 -v "$RVERSION"
+    check_command dkms build -m r8125 -v "$RVERSION"
+    check_command dkms install -m r8125 -v "$RVERSION"
 else
-    msg_box "$INSTALLDIR/r8125-9.005.06 doesn't seem to exist, is the tar-package extracted?"
+    msg_box "$INSTALLDIR"/r8125-"$RVERSION" doesn't seem to exist, the script will now exit."
     exit 1
 fi
 
 # Remove the folder, keep the tar
-rm -rf "$INSTALLDIR/r8125-9.005.06"
+rm -rf "$INSTALLDIR"/r8125-"$RVERSION"
 
 # Add new interface in netplan
 cat <<-IPCONFIG > "$INTERFACES"
