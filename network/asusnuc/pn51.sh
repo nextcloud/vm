4834161a6f0fcc99cf0b73d7c65cbf24694166da
Daniel Hansson <github@hanssonit.se>
2021-07-19 12:37:26 +0200

    Rename network/pn51/install_driver.sh to network/asusnuc/pn51.sh (#2050)

--- network/asusnuc/pn51.sh
+++ network/asusnuc/pn51.sh
@@ -20,9 +20,24 @@ root_check
 
 install_if_not build-essentials
 
+# DOwnload and extract
 curl_to_dir https://github.com/nextcloud/vm/blob/pn51/network r8125-9.005.06.tar.bz2 /tmp
 cd /tmp || exit
 tar-xf r8125-9.005.06.tar.bz2
 
+# Install
 cd r8125-9.005.06
-./autorun
+bash /tmp/autorun
+
+# Add new interface in netplan
+cat <<-IPCONFIG > "$INTERFACES"
+network:
+   version: 2
+   ethernets:
+       enp2s0:
+         dhcp4: true
+         dhcp6: true
+IPCONFIG
+
+# Apply config
+netplan apply
