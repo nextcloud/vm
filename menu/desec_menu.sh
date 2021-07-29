924fe1825d86edfc628db91706b10a318a14580d
Daniel Hansson <github@hanssonit.se>
2021-07-29 13:46:35 +0200

    add deSEC menu (#2066)

---
+++ menu/desec_menu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2021, https://www.hanssonit.se/
+
+true
+SCRIPT_NAME="deSEC Menu"
+# shellcheck source=lib.sh
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+choice=$(whiptail --title "$TITLE" --menu \
+"Please choose one of the deSEC options below.
+$MENU_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Install deSEC" "(Setup deSEC fully automated: yourdomain.dedyn.io)" \
+"deSEC Subdomain" "(Add another subdomain to an existing account)" \
+"Remove deSEC" "(Remove your deSEC account - not yet active)" 3>&1 1>&2 2>&3)
+
+case "$choice" in
+    "Install deSEC")
+        print_text_in_color "$ICyan" "Downloading the deSEC install script..."
+        run_script ADDONS desec
+    ;;
+    "deSEC Subdomain")
+        print_text_in_color "$ICyan" "Downloading the deSEC subdomain script..."
+        run_script ADDONS desec_subdomain
+    ;;
+    "Remove deSEC")
+        #print_text_in_color "$ICyan" "Downloading the remove deSEC script..."
+        #run_script ADDONS remove_desec
+        msg_box "This option is not yet active. Please try again another day."
+    ;;
+    *)
+    ;;
+esac
+exit
