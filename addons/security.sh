e6d9133db785ca8fed14a952fbe7fb31b25cc831
Daniel Hansson <github@hanssonit.se>
2021-08-15 01:00:01 +0200

    fix typo + update spamhaus script (#2079)
    Fix #2078

--- addons/security.sh
+++ addons/security.sh
@@ -37,9 +37,11 @@ else
     rm -rf /var/log/apache2/evasive
     rm -f "$ENVASIVE"
     a2dismod reqtimeout
-    bash "$SCRIPTS"/spamhaus_cronjob.sh deletechain
-    rm -f "$SCRIPTS"/spamhaus_cronjob.sh
-    crontab -u root -l | grep -v "$SCRIPTS/spamhaus_crontab.sh 2>&1" | crontab -u root -
+    bash "$SCRIPTS"/spamhaus-drop -d
+    rm -f "$SCRIPTS"/spamhaus-drop
+    crontab -u root -l | grep -v "$SCRIPTS/spamhaus-drop" | crontab -u root -
+    rm -f "$SCRIPTS"/spamhaus_crontab.sh
+    crontab -u root -l | grep -v "$SCRIPTS/spamhaus_crontab.sh" | crontab -u root -
     restart_webserver
     # Show successful uninstall if applicable
     removal_popup "$SCRIPT_NAME"
@@ -68,98 +70,25 @@ fi
 #install_if_not libapache2-mod-qos
 a2enmod reqtimeout # http://httpd.apache.org/docs/2.4/mod/mod_reqtimeout.html
 
-cat << SPAMHAUS_ENABLE > "$SCRIPTS/spamhaus_cronjob.sh"
-#!/bin/bash
-
-## Source: https://raw.githubusercontent.com/wallyhall/spamhaus-drop/master/spamhaus-drop
-## Initially forked from cowgill, extended and improved for our mailserver needs.
-## Credit: https://github.com/cowgill/spamhaus/blob/master/spamhaus.sh
-
-# based off the following two scripts
-# http://www.theunsupported.com/2012/07/block-malicious-ip-addresses/
-# http://www.cyberciti.biz/tips/block-spamming-scanning-with-iptables.html
-
-# path to iptables
-IPTABLES="/sbin/iptables"
-
-# list of known spammers
-URLS="www.spamhaus.org/drop/drop.lasso www.spamhaus.org/drop/edrop.lasso"
-
-# save local copy here
-FILE="/tmp/drop.lasso"
-
-# iptables custom chain
-CHAIN="Spamhaus"
-
-# check to see if the chain already exists
-if \$IPTABLES -L "\$CHAIN" -n
-then
-    # flush the old rules
-    \$IPTABLES -D INPUT -j "\$CHAIN"
-    \$IPTABLES -D FORWARD -j "\$CHAIN"
-    \$IPTABLES -F "\$CHAIN"
-
-    if [ -n "\$1" ]
-    then
-        \$IPTABLES -X "\$CHAIN"
-        echo "\$CHAIN removed in iptables."
-        exit
-    else
-        echo "Flushed old rules. Applying updated Spamhaus list...."
-    fi
-else
-    # create a new chain set
-    \$IPTABLES -N "\$CHAIN"
-
-    # tie chain to input rules so it runs
-    \$IPTABLES -A INPUT -j "\$CHAIN"
-
-    # don't allow this traffic through
-    \$IPTABLES -A FORWARD -j "\$CHAIN"
-
-    echo "Chain not detected. Creating new chain and adding Spamhaus list...."
-fi;
-
-for URL in \$URLS; do
-    # get a copy of the spam list
-    echo "Fetching \$URL ..."
-    wget -qc "\$URL" -O "\$FILE"
-    tail "\$FILE"
-
-    # iterate through all known spamming hosts
-    for IP in \$( cat "\$FILE" | egrep -v '^;' | cut -d' ' -f1 ); do
-        # add the ip address log rule to the chain
-        \$IPTABLES -A "\$CHAIN" -p 0 -s "\$IP" -j LOG --log-prefix "[SPAMHAUS BLOCK]" -m limit --limit 3/min --limit-burst 10
-
-        # add the ip address to the chain
-        \$IPTABLES -A "\$CHAIN" -p 0 -s "\$IP" -j DROP
-
-        echo "\$IP"
-    done
-
-    # remove the spam list
-    unlink "\$FILE"
-done
-
-echo "Done!"
-SPAMHAUS_ENABLE
+# Download the spamhaud script
+curl_to_dir https://raw.githubusercontent.com/wallyhall/spamhaus-drop/master/ spamhaus-drop "$SCRIPTS"
 
 # Make the file executable
-chmod +x "$SCRIPTS"/spamhaus_cronjob.sh
+chmod +x "$SCRIPTS"/spamhaus-drop
 
 # Add it to crontab
-crontab -u root -l | grep -v "$SCRIPTS/spamhaus_crontab.sh 2>&1" | crontab -u root -
-crontab -u root -l | { cat; echo "10 2 * * * $SCRIPTS/spamhaus_crontab.sh 2>&1"; } | crontab -u root -
+crontab -u root -l | grep -v "$SCRIPTS/spamhaus-drop" | crontab -u root -
+crontab -u root -l | { cat; echo "10 2 * * * $SCRIPTS/spamhaus-drop -u 2>&1"; } | crontab -u root -
 
 # Run it for the first time
 msg_box "We will now add a number of bad IP-addresses to your IPtables block list, meaning that all IPs on that list will be blocked as they are known for doing bad stuff.
 
-The script will be run on a schelude to update the IP-addresses, and can be found in $SCRIPTS/spamhaus_cronjob.sh.
+The script will be run on a schelude to update the IP-addresses, and can be found in $SCRIPTS/spamhaus-drop.
 
 To disable it, please remove the crontab by executing 'crontab -e' and remove this:
-10 2 * * * $SCRIPTS/spamhaus_crontab.sh 2>&1"
+10 2 * * * $SCRIPTS/spamhaus-drop -u 2>&1"
 
-if check_command bash "$SCRIPTS"/spamhaus_cronjob.sh
+if check_command bash "$SCRIPTS"/spamhaus-drop -u
 then
     print_text_in_color "$IGreen" "Security added!"
     restart_webserver
