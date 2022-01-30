865cb6cc71b04d118ca6c9c11b4c60d8c31c33da
Daniel Hansson <github@hanssonit.se>
2022-01-30 17:39:42 +0100

    better check for LDAP (#2209)
    * better check for LDAP
    
    @szaimen
    
    * set a variable instead

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -960,7 +960,11 @@ fi
 ## TODO; remove in NC 23.0.2
 if is_app_installed user_ldap
 then
-    nextcloud_occ app:disable user_ldap
+    if is_app_enabled user_ldap
+    then
+        nextcloud_occ app:disable user_ldap
+        LDAP_ENABLED=yes-it-enabled
+    fi
 fi
 
 if [ -d $BACKUP/apps/ ]
@@ -1016,7 +1020,7 @@ fi
 
 # Enable LDAP again
 ## TODO; remove in NC 23.0.2
-if is_app_installed user_ldap
+if [ "$LDAP_ENABLED" = yes-it-enabled ]
 then
     nextcloud_occ app:enable user_ldap
 fi
