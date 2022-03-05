3f88e26ef3c8b6f87f92aabe657dd2b49f13a38d
Daniel Hansson <github@hanssonit.se>
2022-03-06 00:38:51 +0100

    fix prune for large disks (don't prune) (#2255)

--- disk/prune_zfs_snaphots.sh
+++ disk/prune_zfs_snaphots.sh
@@ -20,41 +20,47 @@ if [ -d $NCDATA ]
 then
     if is_this_installed zfs-auto-snapshot
     then
+        # Check if /mnt/ncdata is more than 70% full
         if [ "$(df -h $NCDATA | awk '{print $5}' | tail -1 | cut -d "%" -f1)" -gt 70 ]
         then
-            # Notify user
-            notify_admin_gui \
-            "Disk space almost full!" \
-            "The disk space for ncdata is almost full. We have automatically deleted \
+            # If it is, then check if there's more than 100 GB left. Large disks may have plenty of space left,
+            # and still being set for cleaning if this is not checked
+            if [ "$(df /mnt/ncdata/ | awk '{print $4}' | tail -1)" -lt 104857600 ]
+            then
+                # Notify user
+                notify_admin_gui \
+                "Disk space almost full!" \
+                "The disk space for ncdata is almost full. We have automatically deleted \
 ZFS snapshots older than 2 days and cleaned up your trashbin to free up some space \
 and avoid a fatal crash. Please check $VMLOGS/zfs_prune.log for the results."
-            # On screen information
-            msg_box "Your disk space is almost full (more than 70%).
+                # On screen information
+                msg_box "Your disk space is almost full (more than 70% or less than 100GB left).
 
 To solve that, we will now delete ZFS snapshots older than 4 weeks
 
 The script will also delete everything in trashbin for all users to free up some space."
-            countdown "To abort, please press CTRL+C within 10 seconds." 10
-            print_text_in_color "$IGreen" "Freeing some space... This might take a while, please don't abort."
-            # Get the latest prune script
-            if [ -f $SCRIPTS/zfs-prune-snapshots ]
-            then
-                rm -f "$SCRIPTS"/zfs-prune-snapshots
-                download_script DISK zfs-prune-snapshots
-            elif [ ! -f $SCRIPTS/zfs-prune-snapshots.sh ]
-            then
-                download_script DISK zfs-prune-snapshots
+                countdown "To abort, please press CTRL+C within 10 seconds." 10
+                print_text_in_color "$IGreen" "Freeing some space... This might take a while, please don't abort."
+                # Get the latest prune script
+                if [ -f $SCRIPTS/zfs-prune-snapshots ]
+                then
+                    rm -f "$SCRIPTS"/zfs-prune-snapshots
+                    download_script DISK zfs-prune-snapshots
+                elif [ ! -f $SCRIPTS/zfs-prune-snapshots.sh ]
+                then
+                    download_script DISK zfs-prune-snapshots
+                fi
+                check_command chmod +x "$SCRIPTS"/zfs-prune-snapshots.sh
+                # Prune!
+                cd "$SCRIPTS"
+                if [ ! -d "$VMLOGS" ]
+                then
+                    mkdir -p "$VMLOGS"
+                fi
+                touch $VMLOGS/zfs_prune.log
+                ./zfs-prune-snapshots.sh 2d ncdata >> $VMLOGS/zfs_prune.log
+                nextcloud_occ trashbin:cleanup --all-users >> $VMLOGS/zfs_prune.log
             fi
-            check_command chmod +x "$SCRIPTS"/zfs-prune-snapshots.sh
-            # Prune!
-            cd "$SCRIPTS"
-            if [ ! -d "$VMLOGS" ]
-            then
-                mkdir -p "$VMLOGS"
-            fi
-            touch $VMLOGS/zfs_prune.log
-            ./zfs-prune-snapshots.sh 2d ncdata >> $VMLOGS/zfs_prune.log
-            nextcloud_occ trashbin:cleanup --all-users >> $VMLOGS/zfs_prune.log
         fi
     fi
 fi
