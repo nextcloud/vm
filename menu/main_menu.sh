0584a03e8b6f450ea6989c56ffc1cc00913537ce
Daniel Hansson <mailto@danielhansson.nu>
2022-04-17 11:06:43 +0200

    always update minor before major version
    this improves stability of the update, and covers the cases when Nextcloud makes a X.**X**.XX release.

--- menu/main_menu.sh
+++ menu/main_menu.sh
@@ -7,6 +7,9 @@ SCRIPT_NAME="Main Menu"
 # shellcheck source=lib.sh
 source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
+# Needed for Update option
+nc_update
+
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
 # 0 = OFF
@@ -24,8 +27,7 @@ $MENU_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
 "Nextcloud Configuration" "(Choose between available Nextcloud configurations)" \
 "Startup Configuration" "(Choose between available startup configurations)" \
 "Server Configuration" "(Choose between available server configurations)" \
-"Update Nextcloud major" "(Update Nextcloud to the latest major release)" \
-"Update Nextcloud minor" "(Update Nextcloud to the latest minor release)" 3>&1 1>&2 2>&3)
+"Update Nextcloud" "(Update Nextcloud to the latest minor release)" 3>&1 1>&2 2>&3)
 
 case "$choice" in
     "Additional Apps")
@@ -44,57 +46,37 @@ case "$choice" in
         print_text_in_color "$ICyan" "Downloading the Server Configuration Menu..."
         run_script MENU server_configuration
     ;;
-    "Update Nextcloud major")
-        if [ -f "$SCRIPTS"/update.sh ]
+    "Update Nextcloud")
+do_the_update() {
+    chmod +x "$SCRIPTS"/update.sh
+    bash "$SCRIPTS"/update.sh minor
+    if yesno_box_yes "We will now run the update script a second time to update to the latest major version ($NCVERSION). Do you want to continue?"
+    then
+        bash "$SCRIPTS"/update.sh
+    fi
+}
+    if [ -f "$SCRIPTS"/update.sh ]
+    then
+        # Check if automated updates are set
+        REBOOT_SET=$(grep -r "shutdown -r" "$SCRIPTS"/update.sh)
+        # Check if it's older than 60 days (60 seconds * 60 minutes * 24 hours * 60 days)
+        if [ "$(stat --format=%Y "$SCRIPTS"/update.sh)" -le "$(( $(date +%s) - ((60*60*24*60)) ))" ]
         then
-	    # Check if automated updates are set
-            REBOOT_SET=$(grep -r "shutdown -r" "$SCRIPTS"/update.sh)
-            # Check if it's older than 60 days (60 seconds * 60 minutes * 24 hours * 60 days)
-            if [ "$(stat --format=%Y "$SCRIPTS"/update.sh)" -le "$(( $(date +%s) - ((60*60*24*60)) ))" ]
-            then
-	        print_text_in_color "$ICyan" "Downloading the latest update script..."
-                download_script STATIC update
-                if [ -n "$REBOOT_SET" ]
-                then
-                    sed -i "s|exit|/sbin/shutdown -r +1|g" "$SCRIPTS"/update.sh
-                fi
-                chmod +x "$SCRIPTS"/update.sh
-                bash "$SCRIPTS"/update.sh
-            else
-                bash "$SCRIPTS"/update.sh
-            fi
-        else
             print_text_in_color "$ICyan" "Downloading the latest update script..."
             download_script STATIC update
-            chmod +x "$SCRIPTS"/update.sh
-            bash "$SCRIPTS"/update.sh
-        fi
-    ;;
-    "Update Nextcloud minor")
-        if [ -f "$SCRIPTS"/update.sh ]
-        then
-	    # Check if automated updates are set
-            REBOOT_SET=$(grep -r "shutdown -r" "$SCRIPTS"/update.sh)
-            # Check if it's older than 60 days (60 seconds * 60 minutes * 24 hours * 60 days)
-            if [ "$(stat --format=%Y "$SCRIPTS"/update.sh)" -le "$(( $(date +%s) - ((60*60*24*60)) ))" ]
+            if [ -n "$REBOOT_SET" ]
             then
-	        print_text_in_color "$ICyan" "Downloading the latest update script..."
-                download_script STATIC update
-                if [ -n "$REBOOT_SET" ]
-                then
-                    sed -i "s|exit|/sbin/shutdown -r +1|g" "$SCRIPTS"/update.sh
-                fi
-                chmod +x "$SCRIPTS"/update.sh
-                bash "$SCRIPTS"/update.sh minor
-            else
-                bash "$SCRIPTS"/update.sh minor
+                sed -i "s|exit|/sbin/shutdown -r +1|g" "$SCRIPTS"/update.sh
             fi
+            do_the_update
         else
-            print_text_in_color "$ICyan" "Downloading the latest update script..."
-            download_script STATIC update
-            chmod +x "$SCRIPTS"/update.sh
-            bash "$SCRIPTS"/update.sh minor
+            do_the_update
         fi
+    else
+        print_text_in_color "$ICyan" "Downloading the latest update script..."
+        download_script STATIC update
+        do_the_update
+    fi
     ;;
     *)
     ;;
