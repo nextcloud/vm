0e17dde041215cec956cfe082c823a0e266796b3
enoch85 <github@hanssonit.se>
2022-04-24 16:24:44 +0200

    cron isn't included in minimal
    Signed-off-by: enoch85 <github@hanssonit.se>

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -227,6 +227,9 @@ install_if_not build-essential
 # Install a decent text editor
 install_if_not nano
 
+# Install package for crontab
+install_if_not cron
+
 # Make sure sudo exists (needed in adduser.sh)
 install_if_not sudo
 
