67a0718ed84a5258ba4f13aa7148c5495155e7cf
enoch85 <github@hanssonit.se>
2022-04-25 23:15:52 +0200

    better fix for mysql in update script
    Signed-off-by: enoch85 <github@hanssonit.se>

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -232,7 +232,7 @@ Holding PHP to avoid upgrading to a newer version without migration..."
 fi
 
 # Don't allow MySQL/MariaDB
-if ! grep -q pgsql /var/www/nextcloud/config/config.php || dpkg -l | grep -q mariadb-common
+if [[ $NCDBTYPE = mysql ]]
 then
     msg_box "MySQL/MariaDB is not supported in this script anymore. Please contact us to get support \
 for upgrading your server: https://shop.hanssonit.se/product/premium-support-per-30-minutes/"
