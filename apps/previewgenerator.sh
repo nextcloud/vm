e84262e98245bc243a75d73c86873465b0fdfd51
Daniel Hansson <mailto@danielhansson.nu>
2023-01-08 11:24:23 +0100

    128 MB shoulod be enough
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- apps/previewgenerator.sh
+++ apps/previewgenerator.sh
@@ -239,7 +239,7 @@ nextcloud_occ config:app:set previewgenerator heightSizes --value="256"
 nextcloud_occ config:system:set preview_max_x --value="2048"
 nextcloud_occ config:system:set preview_max_y --value="2048"
 nextcloud_occ config:system:set jpeg_quality --value="60"
-nextcloud_occ config:system:set preview_max_memory --value="256"
+nextcloud_occ config:system:set preview_max_memory --value="128"
 nextcloud_occ config:app:set preview jpeg_quality --value="60"
 
 # Add crontab for www-data
