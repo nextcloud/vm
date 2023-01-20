3c49dcda17d718520c19465ffc3b6f2ea4de9741
Daniel Hansson <mailto@danielhansson.nu>
2023-01-20 13:37:28 +0100

    improve the function also check distro
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- apps/face-recognition.sh
+++ apps/face-recognition.sh
@@ -100,7 +100,7 @@ then
 fi
 
 # Install requirements
-if [[ "$PHPVER" = "8.1" ]]
+if version 20.04 "$DISTRO" 20.04.10 && [[ "$PHPVER" = "8.1" ]]
 then
     # https://github.com/matiasdelellis/facerecognition/wiki/PDlib-Installation#ubuntu-focal
     add_trusted_key_and_repo "repo.gpg.key" \
@@ -108,8 +108,8 @@ then
     "https://repo.delellis.com.ar" \
     "focal focal" \
     "facerecognition-pdlib.list"
-    install_if_not php7.4-pdlib
-elif [[ "$PHPVER" = "8.1" ]]
+    install_if_not php8.1-pdlib
+elif version 22.04 "$DISTRO" 22.04.10 && [[ "$PHPVER" = "8.1" ]]
 then
     # https://github.com/matiasdelellis/facerecognition/wiki/PDlib-Installation#ubuntu-jammy
     add_trusted_key_and_repo "repo.gpg.key" \
@@ -118,6 +118,15 @@ then
     "$CODENAME $CODENAME" \
     "facerecognition-pdlib.list"
     install_if_not php8.1-pdlib
+elif version 20.04 "$DISTRO" 20.04.10 && [[ "$PHPVER" = "7.4" ]]
+then
+    # https://github.com/matiasdelellis/facerecognition/wiki/PDlib-Installation#ubuntu-focal
+    add_trusted_key_and_repo "repo.gpg.key" \
+    "https://repo.delellis.com.ar" \
+    "https://repo.delellis.com.ar" \
+    "focal focal" \
+    "facerecognition-pdlib.list"
+    install_if_not php7.4-pdlib
 fi
 
 # Install the app
