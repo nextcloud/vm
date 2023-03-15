b4d1f1bcf8aa4a8cfeb3e4c07047e156071414f4
Daniel Hansson <mailto@danielhansson.nu>
2023-03-15 17:47:35 +0100

    add temp script
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

---
+++ apps/face-recognition.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2023, https://www.hanssonit.se/
+
+true
+SCRIPT_NAME="Redirect to Recognize"
+# shellcheck source=lib.sh
+source /var/scripts/fetch_lib.sh
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+# This is because we move the old script to old, and replaced it with this instead.
+run_script APP recognize
+
+exit
