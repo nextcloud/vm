be512831d455d7f695734431b8b85f607e3e8c4a
Daniel Hansson <mailto@danielhansson.nu>
2023-03-31 13:30:39 +0200

    Change Updatenotification
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- static/updatenotification.sh
+++ static/updatenotification.sh
@@ -56,7 +56,7 @@ then
         notify_admin_gui \
         "New minor Nextcloud Update!" \
         "Nextcloud $NCMIN just became available. Since you are running Automatic \
-Updates on Saturdays at $AUT_UPDATES_TIME:00, you don't need to bother about updating \
+Updates every month on Saturdays at $AUT_UPDATES_TIME:00, you don't need to bother about updating \
 the server to minor Nextcloud versions manually, as that's already taken care of."
     else
         notify_admin_gui \
@@ -91,7 +91,7 @@ then
     notify_admin_gui \
     "New major Nextcloud Update!" \
     "Nextcloud $NCVERSION just became available. Please run 'sudo bash \
-/var/scripts/update.sh' from your CLI to update your server to Nextcloud $NCVERSION. \
+/var/scripts/menu.sh' --> Update Nextcloud from your CLI to update your server to Nextcloud $NCVERSION. \
 Before updating though, you should visit https://your-nc-domain/settings/admin/overview \
 and make sure that all apps are compatible with the new version. And please never forget to \
 create a backup and/or snapshot before updating!"
