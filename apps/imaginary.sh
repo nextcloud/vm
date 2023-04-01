a796c74c9eae15f79fb3d221dad8d63b6f10b934
Daniel Hansson <mailto@danielhansson.nu>
2023-04-01 21:43:54 +0200

    only look for previews
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- apps/imaginary.sh
+++ apps/imaginary.sh
@@ -75,7 +75,7 @@ This will most likely clear a lot of space! Also, pre-generated previews are not
             rm -rfv "$NCDATA"/appdata_*/preview
             print_text_in_color "$ICyan" "Scanning Nextclouds appdata directory after removing all previews. \
 This can take a while..."
-            nextcloud_occ files:scan-app-data -vvv
+            nextcloud_occ files:scan-app-data preview -vvv
             msg_box "All previews were successfully removed."
         fi
         # Remove log
