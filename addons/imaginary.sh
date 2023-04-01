b07171e4b4a35aa4cca5de6963eaf964603b84ad
Daniel Hansson <mailto@danielhansson.nu>
2023-04-01 03:18:01 +0200

    add previewproviders
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- addons/imaginary.sh
+++ addons/imaginary.sh
@@ -76,11 +76,7 @@ fi
 # Start the container
 docker compose -p imaginary -f "$SCRIPTS"/imaginary-docker/docker-compose.yml up -d
 
-# Replace imagick
-nextcloud_occ config:system:set enabledPreviewProviders 0 --value="OC\\Preview\\Imaginary"
-nextcloud_occ config:system:set preview_imaginary_url --value="http://127.0.0.1:9000"
-
-### Test if it's working
+# Test if imaginary is working
 if curl -O "http://127.0.0.1:9000/crop?width=500&height=400&url=https://raw.githubusercontent.com/h2non/imaginary/master/testdata/large.jpg"
 then
     print_text_in_color "$IGreen" "Imaginary seems to be working OK!"
@@ -89,9 +85,61 @@ else
     exit
 fi
 
+# Install preview generator just in case
+install_and_enable_app previewgenerator
+
+# check if the previewgenerator is installed and enabled
+if is_app_enabled previewgenerator
+then
+    # enable previews
+    nextcloud_occ config:system:set enable_previews --value=true --type=boolean
+
+    # install needed dependency for movies
+    install_if_not ffmpeg
+else
+    exit
+fi
+
+# Set providers
+nextcloud_occ config:system:set enabledPreviewProviders 0 --value="OC\\Preview\\JPEG"
+nextcloud_occ config:system:set enabledPreviewProviders 1 --value="OC\\Preview\\PNG"
+nextcloud_occ config:system:set enabledPreviewProviders 2 --value="OC\\Preview\\WEBP"
+nextcloud_occ config:system:set enabledPreviewProviders 3 --value="OC\\Preview\\HEIF"
+nextcloud_occ config:system:set enabledPreviewProviders 4 --value="OC\\Preview\\TIFF"
+nextcloud_occ config:system:set enabledPreviewProviders 5 --value="OC\\Preview\\PDF"
+nextcloud_occ config:system:set enabledPreviewProviders 6 --value="OC\\Preview\\GIF"
+nextcloud_occ config:system:set enabledPreviewProviders 7 --value="OC\\Preview\\SVG"
+nextcloud_occ config:system:set enabledPreviewProviders 8 --value="OC\\Preview\\MP3"
+nextcloud_occ config:system:set enabledPreviewProviders 9 --value="OC\\Preview\\TXT"
+nextcloud_occ config:system:set enabledPreviewProviders 10 --value="OC\\Preview\\MarkDown"
+nextcloud_occ config:system:set enabledPreviewProviders 11 --value="OC\\Preview\\OpenDocument"
+nextcloud_occ config:system:set enabledPreviewProviders 12 --value="OC\\Preview\\Krita"
+nextcloud_occ config:system:set enabledPreviewProviders 13 --value="OC\\Preview\\BMP"
+nextcloud_occ config:system:set enabledPreviewProviders 14 --value="OC\\Preview\\Movie"
+nextcloud_occ config:system:set enabledPreviewProviders 15 --value="OC\\Preview\\Imaginary"
+nextcloud_occ config:system:set preview_imaginary_url --value="http://127.0.0.1:9000"
+
+# Set values
+nextcloud_occ config:app:set previewgenerator squareSizes --value="32 256"
+nextcloud_occ config:app:set previewgenerator widthSizes  --value="256 384"
+nextcloud_occ config:app:set previewgenerator heightSizes --value="256"
+nextcloud_occ config:system:set preview_max_x --value="2048"
+nextcloud_occ config:system:set preview_max_y --value="2048"
+nextcloud_occ config:system:set jpeg_quality --value="60"
+nextcloud_occ config:system:set preview_max_memory --value="128"
+nextcloud_occ config:app:set preview jpeg_quality --value="60"
+
+# Add logs
+touch "$VMLOGS"/previewgenerator.log
+chown www-data:www-data "$VMLOGS"/previewgenerator.log
+
 # Rebuild
-nextcloud_occ files:scan-app-data
-nextcloud_occ preview:generate-all --verbose
+print_text_in_color "$ICyan" "Scanning Nextclouds appdata directory to rebuild all previews all previews. This will take a while..."
+nextcloud_occ files:scan-app-data -vvv
+nextcloud_occ preview:generate-all --verbose >> "$VMLOGS"/previewgenerator.log
 
 # Uninstall Preview Generator, not needed anymore (?)
 # nextcloud_occ app:remove previewgenerator
+# reset the cronjob
+# crontab -u www-data -l | grep -v 'preview:pre-generate'  | crontab -u www-data -
+# rm -f "$VMLOGS"/previewgenerator.log
