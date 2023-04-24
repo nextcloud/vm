c85be42d9efe5931d4de9e895c0503986364332e
Daniel Hansson <mailto@danielhansson.nu>
2023-04-24 17:55:16 +0200

    Update trusted.sh
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- network/trusted.sh
+++ network/trusted.sh
@@ -4,6 +4,8 @@ SCRIPT_NAME="Set trusted domain"
 # shellcheck source=lib.sh
 source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
 
+# Removed in NC 26.0.0.
+
 nextcloud occ config:system:set trusted_domains 3 --value="${ADDRESS[@]}" "$(hostname)" "$(hostname --fqdn)"
 nextcloud occ config:system:set overwrite.cli.url --value="https://$(hostname --fqdn)"
 nextcloud_occ maintenance:update:htaccess
