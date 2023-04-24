f7c54cd2b1603cee804d502b868a0bc07a4c4138
Daniel Hansson <mailto@danielhansson.nu>
2023-04-24 18:35:53 +0200

    Update nextcloud-startup-script.sh
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- nextcloud-startup-script.sh
+++ nextcloud-startup-script.sh
@@ -554,7 +554,10 @@ mesg n
 ROOTNEWPROFILE
 
 # Set trusted domain in config.php
-nextcloud occ config:system:set trusted_domains 3 --value="${ADDRESS[@]}" "$(hostname)" "$(hostname --fqdn)"
+nnextcloud occ config:system:set trusted_domains 0 --value="localhost"
+nextcloud occ config:system:set trusted_domains 0 --value="${ADDRESS[@]}"
+nextcloud occ config:system:set trusted_domains 1 --value="$(hostname)"
+nextcloud occ config:system:set trusted_domains 2 --value="$(hostname --fqdn)"
 nextcloud occ config:system:set overwrite.cli.url --value="https://$(hostname --fqdn)"
 nextcloud_occ maintenance:update:htaccess
 
