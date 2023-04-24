a424553670bca5a898594f77eb4d37454ffae04d
Daniel Hansson <mailto@danielhansson.nu>
2023-04-24 18:37:14 +0200

    Update test-new-config.sh
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- lets-encrypt/test-new-config.sh
+++ lets-encrypt/test-new-config.sh
@@ -36,9 +36,9 @@ fi
 
 # Set trusted domains
 nextcloud occ config:system:set trusted_domains 0 --value="localhost"
-nextcloud occ config:system:set trusted_domains 0 --value="${ADDRESS[@]}"
-nextcloud occ config:system:set trusted_domains 1 --value="$(hostname)"
-nextcloud occ config:system:set trusted_domains 2 --value="$(hostname --fqdn)"
+nextcloud occ config:system:set trusted_domains 1 --value="${ADDRESS[@]}"
+nextcloud occ config:system:set trusted_domains 2 --value="$(hostname)"
+nextcloud occ config:system:set trusted_domains 3 --value="$(hostname --fqdn)"
 nextcloud occ config:system:set overwrite.cli.url --value="https://$(hostname --fqdn)"
 nextcloud_occ maintenance:update:htaccess
 
