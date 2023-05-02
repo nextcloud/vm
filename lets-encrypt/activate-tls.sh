8b8d6c920663ec2dbb8dd87007136f87e87ad120
Daniel Hansson <mailto@danielhansson.nu>
2023-05-02 18:11:17 +0200

    Update activate-tls.sh
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- lets-encrypt/activate-tls.sh
+++ lets-encrypt/activate-tls.sh
@@ -171,18 +171,30 @@ then
     <Directory $NCPATH>
     Options Indexes FollowSymLinks
     AllowOverride None
-    ### include all .htaccess
-    Include $NCPATH/.htaccess
-    Include $NCPATH/config/.htaccess
-    Include $NCDATA/.htaccess
-    ###
     Require all granted
     Satisfy Any
+    # This is to include all the Nextcloud rules due to that we use PHP-FPM and .htaccess aren't read
+    Include $NCPATH/.htaccess
+    </Directory>
+
+    # Deny access to your data directory
+    <Directory $NCDATA>
+    Require all denied
+    </Directory>
+
+    # Deny access to the Nextcloud config folder
+    <Directory "$NCPATH"/config/>
+    Require all denied
     </Directory>
 
     <IfModule mod_dav.c>
     Dav off
     </IfModule>
+
+    # The following lines prevent .htaccess and .htpasswd files from being viewed by Web clients.
+    <Files ".ht*">
+    Require all denied
+    </Files>
 
     SetEnv HOME $NCPATH
     SetEnv HTTP_HOME $NCPATH
