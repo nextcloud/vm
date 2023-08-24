fd92b6ed249f28406db62835ba30f403974553a7
Daniel Hansson <mailto@danielhansson.nu>
2023-08-24 09:15:54 +0200

    SC
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -1100,9 +1100,9 @@ then
     if [ "${CURRENTVERSION%%.*}" -ge "23" ]
     then
         # Update opcache.interned_strings_buffer
-        if ! grep -r opcache.interned_strings_buffer="$opcache_interned_strings_buffer_value" $PHP_INI
+        if ! grep -r opcache.interned_strings_buffer="$opcache_interned_strings_buffer_value" "$PHP_INI"
         then
-            sed -i "s|opcache.interned_strings_buffer=.*|opcache.interned_strings_buffer=$opcache_interned_strings_buffer_value|g" $PHP_INI
+            sed -i "s|opcache.interned_strings_buffer=.*|opcache.interned_strings_buffer=$opcache_interned_strings_buffer_value|g" "$PHP_INI"
             restart_webserver
         fi
     fi
