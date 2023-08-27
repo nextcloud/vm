764f541d96f3616b11b26b0f75603b60c50d7d7e
Daniel Hansson <mailto@danielhansson.nu>
2023-08-27 21:27:46 +0200

    improve the https check (#2555)

--- apps/notify_push.sh
+++ apps/notify_push.sh
@@ -41,8 +41,13 @@ fi
 # Check TLS
 check_nextcloud_https "Notify Push"
 
+# Get the NCDOMAIN variable
+if [ -z $NCDOMAIN ]
+then
+    ncdomain
+fi
+
 # Check apache conf
-NCDOMAIN=$(nextcloud_occ_no_check config:system:get overwrite.cli.url | sed 's|https://||;s|/||')
 if ! [ -f "$SITES_AVAILABLE/$NCDOMAIN.conf" ]
 then
     msg_box "It seems like you haven't used the built-in 'Activate TLS' script to enable 'Let's Encrypt!' \
