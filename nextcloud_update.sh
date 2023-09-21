98051df1530789d783caad926e4a3b7665f47051
Daniel Hansson <mailto@danielhansson.nu>
2023-09-21 12:05:52 +0200

    update to latest
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- nextcloud_update.sh
+++ nextcloud_update.sh
@@ -661,7 +661,8 @@ $DOCKER_RUN_OUTPUT"
             msg_box "Please consider reinstalling FullTextSearch since you seem to have the old (and not working) implemantation by issuing the uninstall script: sudo bash $SCRIPTS/menu.sh --> Additional Apps --> FullTextSearch"
         elif [ -d "$FULLTEXTSEARCH_DIR" ]
         then
-            print_text_in_color "$ICyan" "Full Text Search is version based, to upgrade it, please change the version in $FULLTEXTSEARCH_DIR and run 'docker compose pull && docker compose up -d'. Latest tags are here: https://www.docker.elastic.co/r/elasticsearch and release notes here: https://www.elastic.co/guide/en/elasticsearch/reference/current/release-highlights.html"
+            docker-compose_update 'es01' 'Full Text Search' "$FULLTEXTSEARCH_DIR"
+            print_text_in_color "$ICyan" "Latest tags are here: https://www.docker.elastic.co/r/elasticsearch and release notes here: https://www.elastic.co/guide/en/elasticsearch/reference/current/release-highlights.html"
         fi
     fi
     # Talk Recording
