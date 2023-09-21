b1fbb5197d134cb0b014ce528e8b77d3c2a724f5
Daniel Hansson <mailto@danielhansson.nu>
2023-09-21 14:37:19 +0200

    update function variable
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- lib.sh
+++ lib.sh
@@ -177,7 +177,8 @@ fulltextsearch_install() {
     ELASTIC_USER_PASSWORD=$(gen_passwd "$SHUF" '[:lower:]')
     FULLTEXTSEARCH_IMAGE_NAME=fulltextsearch_es01
     FULLTEXTSEARCH_SERVICE=nextcloud-fulltext-elasticsearch-worker.service
-    FULLTEXTSEARCH_IMAGE_NAME_LATEST_TAG="$(curl -s -m 900 https://www.docker.elastic.co/r/elasticsearch | grep -Eo [[:digit:]]\\.[[:digit:]]\\.[[:digit:]] | sort --version-sort | tail -1)"
+    # Supports 0-9.0-99.0-9. If the new major becomes 10.X.X, then another [[:digit:]] is needed in the first tab before the dot.
+    FULLTEXTSEARCH_IMAGE_NAME_LATEST_TAG="$(curl -s -m 900 https://www.docker.elastic.co/r/elasticsearch | grep -Eo "[[:digit:]]\\.[[:digit:]][[:digit:]]\\.[[:digit:]]" | sort --version-sort | tail -1)"
     # Legacy, changed 2023-09-21
     DOCKER_IMAGE_NAME=es01
     # Legacy, not used at all
