bc1cf865404608514871e4af1e6c9b5021f58a74
Daniel Hansson <mailto@danielhansson.nu>
2024-05-07 20:18:58 +0200

    add 10 minutes instead
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- addons/automatic_updates.sh
+++ addons/automatic_updates.sh
@@ -42,7 +42,7 @@ mkdir -p "$VMLOGS"/updates
 crontab -u root -l | { cat; echo "0 $AUT_UPDATES_TIME * 1-12 6 $SCRIPTS/update.sh minor >> $VMLOGS/updates/update-\$(date +\%Y-\%m-\%d_\%H:\%M).log 2>&1"; } | crontab -u root -
 if yesno_box_yes "Do you want to reboot your server after every update? *recommended*"
 then
-    sed -i "s|exit|/sbin/shutdown -r +1|g" "$SCRIPTS"/update.sh
+    sed -i "s|exit|/sbin/shutdown -r +10|g" "$SCRIPTS"/update.sh
     echo "exit" >> "$SCRIPTS"/update.sh
 fi
 
