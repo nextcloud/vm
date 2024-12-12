fdd03a9275602b0108bb9ace3e0eaf8540d7f479
Daniel Hansson <mailto@danielhansson.nu>
2024-12-12 11:11:04 +0100

    make it cleaner
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 true
-SCRIPT_NAME="Install NcVM with Vagrant"
 
 # Clone this repo
 git clone https://github.com/nextcloud/vm.git
 
 # We need a check here due to Shellcheck
-if [ -d vm ]
-then
-    cd vm || exit
-else
-    echo "Sorry, but the 'cd' dir doesn't exist, please report this issue to https://github.com/nextcloud/vm/"
-    exit
-fi
+cd vm || exit
 
 # Do the installation
 sudo bash nextcloud_install_production.sh --provisioning
