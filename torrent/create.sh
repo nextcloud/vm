4ed5f4d6321be2dba1929f4c5218a4e304f7b95c
Daniel Hansson <mailto@danielhansson.nu>
2025-02-27 12:52:59 +0100

    cosmetic fixes to torrent script (#2729)
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- torrent/create.sh
+++ torrent/create.sh
@@ -26,21 +26,23 @@ root_check
 install_if_not transmission-cli
 install_if_not transmission-daemon
 
-TRANSMISSION_DL_DIR=/var/lib/transmission-daemon/downloads
+TRANSMISSION_DL_DIR="/var/lib/transmission-daemon/downloads"
+NC_ZIP="NextcloudVM.zip"
 
 # Modify transmission service file to fix https://github.com/transmission/transmission/issues/6991
 sed -i 's/Type=notify/Type=simple/' /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
 systemctl daemon-reload
 
 # Check if NextcloudVM.zip already exists
-if [ ! -f "$TRANSMISSION_DL_DIR/NextcloudVM.zip" ]; then
+if [ ! -f "$TRANSMISSION_DL_DIR"/"$NC_ZIP" ]
+then
     # Download the VM only if it doesn't exist
-    curl -fSLO --retry 3 https://download.kafit.se/s/dnkWptz8AK4JZDM/download -o $TRANSMISSION_DL_DIR/NextcloudVM.zip
+    curl_to_dir https://download.kafit.se/s/dnkWptz8AK4JZDM download "$TRANSMISSION_DL_DIR"
+    mv "$TRANSMISSION_DL_DIR"/download "$TRANSMISSION_DL_DIR"/"$NC_ZIP"
 else
-    echo "NextcloudVM.zip already exists in transmission default downloads directory, skipping download"
+    echo "$NC_ZIP already exists in transmission default downloads directory, skipping download"
 fi
 
-# I dont think these are necessary in 2025?
 # Set more memory to sysctl
 #echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
 #echo "net.core.wmem_max = 4194304" >> /etc/sysctl.conf
@@ -48,15 +50,15 @@ fi
 
 # Create torrent
 curl_to_dir "$GITHUB_REPO"/torrent trackers.txt /tmp
-transmission-create -o $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent -c "https://www.hanssonit.se/nextcloud-vm" -t $(cat /tmp/trackers.txt) $TRANSMISSION_DL_DIR/NextcloudVM.zip
+transmission-create -o $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent -c "https://www.hanssonit.se/nextcloud-vm" -t "$(cat /tmp/trackers.txt)" "$TRANSMISSION_DL_DIR"/"$NC_ZIP"
 
 # Seed it!
-transmission-remote -n 'transmission:transmission' --torrent=$TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent -a $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent --start --verify
+transmission-remote -n 'transmission:transmission' --torrent="$TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent" -a "$TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent" --start --verify
 
 # Copy it to local NC account
 install_if_not rsync
 nextclouduser="$(input_box_flow "Please enter the Nextcloud user that you want to move the finished torrent file to:")"
-rsync -av $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent /mnt/ncdata/"$nextclouduser"/files/
+rsync -av "$TRANSMISSION_DL_DIR"/nextcloudvmhanssonit.torrent /mnt/ncdata/"$nextclouduser"/files/
 chown www-data:www-data /mnt/ncdata/"$nextclouduser"/files/nextcloudvmhanssonit.torrent
 nextcloud_occ files:scan "$nextclouduser"
 unset nextclouduser
