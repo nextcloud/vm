58854431122e1ff8b11524de14ff684f4f669ed1
Sami Nieminen <imas@nenimein.fi>
2025-02-27 12:57:26 +0200

    Update & Fix torrent creation (#2728)
    * Update torrent script to work with current transmission CLI.
    
    Signed-off-by: Sami Nieminen <imas@nenimein.fi>
    
    * Add logic to not download the file every time script is ran to save bandwidth.
    Refine curl file to download straight to correct directory.
    Add script to fix transmission default systemd file per https://github.com/transmission/transmission/issues/6991
    
    Signed-off-by: Sami Nieminen <imas@nenimein.fi>
    
    * Make Transmission directory a variable.
    Fix torrent creation missing the source file.
    
    Signed-off-by: Sami Nieminen <imas@nenimein.fi>
    
    * Update trackers file to reduce amount of dead trackers while creating the torrent.
    
    Signed-off-by: Sami Nieminen <imas@nenimein.fi>
    
    ---------
    
    Signed-off-by: Sami Nieminen <imas@nenimein.fi>

--- torrent/create.sh
+++ torrent/create.sh
@@ -6,7 +6,7 @@
 
 #########
 
-## This doesn't seem to work in current state.
+## This script will install Transmission, download the latest version of the VM, create a torrent of the file and seed it using Transmission 
 ## Help is welcome!
 
 # shellcheck source=lib.sh
@@ -26,27 +26,37 @@ root_check
 install_if_not transmission-cli
 install_if_not transmission-daemon
 
-# Download the VM
-curl -fSLO --retry 3 https://download.kafit.se/s/dnkWptz8AK4JZDM/download
-mv download NextcloudVM.zip
-chown debian-transmission:debian-transmission NextcloudVM.zip
+TRANSMISSION_DL_DIR=/var/lib/transmission-daemon/downloads
 
+# Modify transmission service file to fix https://github.com/transmission/transmission/issues/6991
+sed -i 's/Type=notify/Type=simple/' /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
+systemctl daemon-reload
+
+# Check if NextcloudVM.zip already exists
+if [ ! -f "$TRANSMISSION_DL_DIR/NextcloudVM.zip" ]; then
+    # Download the VM only if it doesn't exist
+    curl -fSLO --retry 3 https://download.kafit.se/s/dnkWptz8AK4JZDM/download -o $TRANSMISSION_DL_DIR/NextcloudVM.zip
+else
+    echo "NextcloudVM.zip already exists in transmission default downloads directory, skipping download"
+fi
+
+# I dont think these are necessary in 2025?
 # Set more memory to sysctl
-echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
-echo "net.core.wmem_max = 4194304" >> /etc/sysctl.conf
-sysctl -p
+#echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+#echo "net.core.wmem_max = 4194304" >> /etc/sysctl.conf
+#sysctl -p
 
 # Create torrent
 curl_to_dir "$GITHUB_REPO"/torrent trackers.txt /tmp
-transmission-create -o nextcloudvmhanssonit.torrent -c "https://www.hanssonit.se/nextcloud-vm" -t $(cat /tmp/trackers.txt) NextcloudVM.zip
+transmission-create -o $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent -c "https://www.hanssonit.se/nextcloud-vm" -t $(cat /tmp/trackers.txt) $TRANSMISSION_DL_DIR/NextcloudVM.zip
 
 # Seed it!
-transmission-remote -n 'transmission:transmission' -a nextcloudvmhanssonit.torrent
+transmission-remote -n 'transmission:transmission' --torrent=$TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent -a $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent --start --verify
 
 # Copy it to local NC account
 install_if_not rsync
 nextclouduser="$(input_box_flow "Please enter the Nextcloud user that you want to move the finished torrent file to:")"
-rsync -av nextcloudvmhanssonit.torrent /mnt/ncdata/"$nextclouduser"/files/
+rsync -av $TRANSMISSION_DL_DIR/nextcloudvmhanssonit.torrent /mnt/ncdata/"$nextclouduser"/files/
 chown www-data:www-data /mnt/ncdata/"$nextclouduser"/files/nextcloudvmhanssonit.torrent
 nextcloud_occ files:scan "$nextclouduser"
 unset nextclouduser
