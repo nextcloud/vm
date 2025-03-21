de519803fe0db626e5205cdb4da72721efb0f91d
Daniel Hansson <mailto@danielhansson.nu>
2025-03-21 23:18:00 +0100

    change version
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- torrent/create.sh
+++ torrent/create.sh
@@ -28,7 +28,7 @@ install_if_not transmission-daemon
 
 TRANSMISSION_DL_DIR="/var/lib/transmission-daemon/downloads"
 NC_OVA="100GB_Nextcloud-VM_www.hanssonit.se.ova"
-VERSION_TAG=30.0.1
+VERSION_TAG=31.0.2
 VERSION_HUB=9
 
 # Modify transmission service file to fix https://github.com/transmission/transmission/issues/6991
